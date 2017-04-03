f592f9ba8223ddf7199442787ec9d3f21c5fbf8c
Matt Macy <mmacy@nextbsd.org>
2017-04-02 22:22:26 -0700

    add vector download and vector preprocessing progress indicator

---
+++ torchtext/utils.py
@@ -0,0 +1,19 @@
+
+def reporthook(t):
+    """https://github.com/tqdm/tqdm"""
+    last_b = [0]
+
+    def inner(b=1, bsize=1, tsize=None):
+        """
+        b: int, optionala
+        Number of blocks just transferred [default: 1].
+        bsize: int, optional
+        Size of each block (in tqdm units) [default: 1].
+        tsize: int, optional
+        Total size (in tqdm units). If [default: None] remains unchanged.
+        """
+        if tsize is not None:
+            t.total = tsize
+        t.update((b - last_b[0]) * bsize)
+        last_b[0] = b
+    return inner
