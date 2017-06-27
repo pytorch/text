028c69497bdfb1ad0a862fc2b20f011695020a36
Nelson Liu <nelson.liu.2009@gmail.com>
2017-06-26 23:07:12 -0700

    Add simple test for Field.preprocess

---
+++ test/test_data.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+import torchtext.data as data
+
+
+class TestData(TestCase):
+    def test_field_preprocess(self):
+        field = data.Field()
+        assert field.preprocess("Test string.") == ["Test", "string."]
+
+        field_lower = data.Field(lower=True)
+        assert field_lower.preprocess("Test string.") == ["test", "string."]
+
+        preprocess_pipeline = data.Pipeline(lambda x: x + "!")
+        field_preprocessing = data.Field(preprocessing=preprocess_pipeline,
+                                         lower=True)
+        assert field_preprocessing.preprocess("Test string.") == ["test!", "string.!"]
+
+        field_not_sequential = data.Field(sequential=False, lower=True,
+                                          preprocessing=preprocess_pipeline)
+        assert field_not_sequential.preprocess("Test string.") == "test string.!"
