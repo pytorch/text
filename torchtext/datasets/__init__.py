99099d680c774c0cafcff50d37a1d1d62b4847ae
Nelson Liu <nelson.liu.2009@gmail.com>
2017-06-26 22:12:35 -0700

    Add __all__ to datasets __init__.py

--- torchtext/datasets/__init__.py
+++ torchtext/datasets/__init__.py
@@ -1,5 +1,11 @@
-from .language_modeling import LanguageModelingDataset
-from .language_modeling import WikiText2
+from .language_modeling import LanguageModelingDataset, WikiText2
 from .snli import SNLI
 from .sentiment import SST
 from .translation import TranslationDataset
+
+
+__all__ = ['LanguageModelingDataset',
+           'SNLI',
+           'SST',
+           'TranslationDataset',
+           'WikiText2']
