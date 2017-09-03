dde75e78f59c37fc74615b0c690ec25c1b361edb
Nelson Liu <nfliu@cs.washington.edu>
2017-09-03 02:48:19 -0700

    Generalize csv tabular test to csv and tsv

--- test/common/torchtext_test_case.py
+++ test/common/torchtext_test_case.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 class TorchtextTestCase(TestCase):
     # Directory where everything temporary and test-related is written
     test_dir = tempfile.mkdtemp()
-    csv_dataset_path = os.path.join(test_dir, "csv_dataset.csv")
+    sv_dataset_path = os.path.join(test_dir, "sv_dataset")
 
     def setUp(self):
         logging.basicConfig(format=('%(asctime)s - %(levelname)s - '
@@ -25,12 +25,18 @@ class TorchtextTestCase(TestCase):
         except:
             subprocess.call(["rm", "-rf", self.test_dir])
 
-    def write_csv_dataset(self):
-        with open(self.csv_dataset_path, "w") as csv_dataset_file:
-            csv_dataset_file.write("0,When do you use シ instead of し?,"
-                                   "When do you use \"&\" instead of \"and\"?,"
-                                   "0\n")
-            csv_dataset_file.write("1,Where was Lincoln born?,"
-                                   "Which location was Abraham Lincoln born?,"
-                                   "1\n")
-            csv_dataset_file.write("2,What is 2+2,2+2=?,1")
+    def write_sv_dataset(self, data_format="csv"):
+        if data_format == "csv":
+            delim = ","
+        elif data_format == "tsv":
+            delim = "\t"
+        else:
+            raise ValueError("Invalid format {}".format(data_format))
+        with open(self.sv_dataset_path, "w") as sv_dataset_file:
+            sv_dataset_file.write("0{}When do you use シ instead of し?{}"
+                                  "When do you use \"&\" instead of \"and\"?{}"
+                                  "0\n".format(delim, delim, delim))
+            sv_dataset_file.write("1{}Where was Lincoln born?{}"
+                                  "Which location was Abraham Lincoln born?{}"
+                                  "1\n".format(delim, delim, delim))
+            sv_dataset_file.write("2{}What is 2+2{}2+2=?{}1".format(delim, delim, delim))
