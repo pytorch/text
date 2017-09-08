cc9084c744b3ba3525f464adeaa7edaea64abf41
Nelson Liu <nfliu@cs.washington.edu>
2017-09-07 23:47:22 -0700

    Fix bug in Pipeline call where we run the whole Pipeline on list input

--- torchtext/data/pipeline.py
+++ torchtext/data/pipeline.py
@@ -18,7 +18,7 @@ class Pipeline(object):
 
     def call(self, x, *args):
         if isinstance(x, list):
-            return [self(tok, *args) for tok in x]
+            return [self.convert_token(tok, *args) for tok in x]
         return self.convert_token(x, *args)
 
     def add_before(self, pipeline):
