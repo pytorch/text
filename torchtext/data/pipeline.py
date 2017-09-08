d0e91f7ed042acb8bfb1b0d1dab4383bc99763d7
Nelson Liu <nfliu@cs.washington.edu>
2017-09-07 23:49:02 -0700

    Add Pipeline docstrings

--- torchtext/data/pipeline.py
+++ torchtext/data/pipeline.py
@@ -1,7 +1,22 @@
 class Pipeline(object):
-    """Defines a pipeline for transforming sequence data."""
+    """Defines a pipeline for transforming sequence data.
+
+    The input is assumed to be utf-8 encoded `str` (Python 3) or
+    `unicode` (Python 2).
+
+    Attributes:
+        convert_token: The function to apply to input sequence data.
+        pipes: The Pipelines that will be applid to input sequence
+            data in order.
+    """
 
     def __init__(self, convert_token=None):
+        """Create a pipeline.
+
+        Arguments:
+            convert_token: The function to apply to input sequence data.
+                If None, the identity function is used. Default: None
+        """
         if convert_token is None:
             self.convert_token = lambda x: x
         elif callable(convert_token):
@@ -12,24 +27,51 @@ class Pipeline(object):
         self.pipes = [self]
 
     def __call__(self, x, *args):
+        """Apply the the current Pipeline(s) to an input.
+
+        Arguments:
+            x: The input to process with the Pipeline(s).
+            Positional arguments: Forwarded to the `call` function
+                of the Pipeline(s).
+        """
         for pipe in self.pipes:
             x = pipe.call(x, *args)
         return x
 
     def call(self, x, *args):
+        """Apply _only_ the convert_token function of the current pipeline
+        to the input. If the input is a list, a list with the results of
+        applying the `convert_token` function to all input elements is
+        returned.
+
+        Arguments:
+            x: The input to apply the convert_token function to.
+            Positional arguments: Forwarded to the `convert_token` function
+                of the current Pipeline.
+        """
         if isinstance(x, list):
             return [self.convert_token(tok, *args) for tok in x]
         return self.convert_token(x, *args)
 
     def add_before(self, pipeline):
-        """Add `pipeline` before this processing pipeline."""
+        """Add a Pipeline to be applied before this processing pipeline.
+
+        Arguments:
+            pipeline: The Pipeline or callable to apply before this
+                Pipeline.
+        """
         if not isinstance(pipeline, Pipeline):
             pipeline = Pipeline(pipeline)
         self.pipes = pipeline.pipes[:] + self.pipes[:]
         return self
 
     def add_after(self, pipeline):
-        """Add `pipeline` after this processing pipeline."""
+        """Add a Pipeline to be applied after this processing pipeline.
+
+        Arguments:
+            pipeline: The Pipeline or callable to apply after this
+                Pipeline.
+        """
         if not isinstance(pipeline, Pipeline):
             pipeline = Pipeline(pipeline)
         self.pipes = self.pipes[:] + pipeline.pipes[:]
