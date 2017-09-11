93189bd0a98b0f24ee22988e424d24622aa74c4c
Bryan Marcus McCann <Bryan.McCann.is@gmail.com>
2017-09-11 10:27:47 +0000

    adding Multi30k wrapper

--- test/translation.py
+++ test/translation.py
@@ -21,9 +21,35 @@ def tokenize_en(text):
 DE = data.Field(tokenize=tokenize_de)
 EN = data.Field(tokenize=tokenize_en)
 
+train, val, test = datasets.Multi30k.splits(exts=('.de', '.en'), fields=(DE, EN))
+
+print(train.fields)
+print(len(train))
+print(vars(train[0]))
+print(vars(train[100]))
+
+DE.build_vocab(train.src, min_freq=3)
+EN.build_vocab(train.trg, max_size=50000)
+
+train_iter, val_iter = data.BucketIterator.splits(
+    (train, val), batch_size=3, device=0)
+
+print(DE.vocab.freqs.most_common(10))
+print(len(DE.vocab))
+print(EN.vocab.freqs.most_common(10))
+print(len(EN.vocab))
+
+batch = next(iter(train_iter))
+print(batch.src)
+print(batch.trg)
+
+
+DE = data.Field(tokenize=tokenize_de)
+EN = data.Field(tokenize=tokenize_en)
+
 train, val = datasets.TranslationDataset.splits(
-    path='~/iwslt2016/de-en/', train='train.tags.de-en',
-    validation='IWSLT16.TED.tst2013.de-en', exts=('.de', '.en'),
+    path='./multi30k/', train='train',
+    validation='val', exts=('.de', '.en'),
     fields=(DE, EN))
 
 print(train.fields)
@@ -38,9 +64,9 @@ train_iter, val_iter = data.BucketIterator.splits(
     (train, val), batch_size=3, device=0)
 
 print(DE.vocab.freqs.most_common(10))
-print(DE.vocab.size)
+print(len(DE.vocab))
 print(EN.vocab.freqs.most_common(10))
-print(EN.vocab.size)
+print(len(EN.vocab))
 
 batch = next(iter(train_iter))
 print(batch.src)
--- torchtext/datasets/translation.py
+++ torchtext/datasets/translation.py
@@ -1,4 +1,7 @@
 import os
+import tarfile
+from six.moves import urllib
+
 
 from .. import data
 
@@ -35,3 +38,56 @@ class TranslationDataset(data.Dataset):
                         [src_line, trg_line], fields))
 
         super(TranslationDataset, self).__init__(examples, fields, **kwargs)
+
+
+class Multi30k(TranslationDataset):
+    """Defines a dataset for the multi-modal WMT 2017 task"""
+
+    train_url = 'http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/training.tar.gz'
+    val_url = 'http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/validation.tar.gz'
+    test_url = 'https://staff.fnwi.uva.nl/d.elliott/wmt16/mmt16_task1_test.tgz'
+    dirname = 'multi30k'
+
+    @classmethod
+    def download(cls, root):
+        path = os.path.join(root, cls.dirname)
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        for url in [cls.train_url, cls.val_url, cls.test_url]:
+            gz_name = os.path.basename(url)
+            fpath = os.path.join(path, gz_name)
+            if not os.path.isfile(fpath):
+                print('downloading {}'.format(gz_name))
+                urllib.request.urlretrieve(url, fpath)
+            with tarfile.open(fpath, 'r:gz') as tar:
+                dirs = [member for member in tar.getmembers()]
+                tar.extractall(path=path, members=dirs)
+        return os.path.join(path, '')
+
+    @classmethod
+    def splits(cls, exts, fields, root='.',
+               train='train', val='val', test='test', **kwargs):
+        """Create dataset objects for splits of the Multi30k dataset.
+
+        Arguments:
+
+            root: directory containing Multi30k data
+            exts: A tuple containing the extension to path for each language.
+            fields: A tuple containing the fields that will be used for data
+                in each language.
+            train: The prefix of the train data. Default: 'train'.
+            validation: The prefix of the validation data. Default: 'val'.
+            test: The prefix of the test data. Default: 'test'.
+            Remaining keyword arguments: Passed to the splits method of
+                Dataset.
+        """
+        path = cls.download(root)
+
+        train_data = None if train is None else cls(
+            os.path.join(path, train), exts, fields, **kwargs)
+        val_data = None if val is None else cls(
+            os.path.join(path, val), exts, fields, **kwargs)
+        test_data = None if test is None else cls(
+            os.path.join(path, test), exts, fields, **kwargs)
+        return tuple(d for d in (train_data, val_data, test_data)
+                     if d is not None)
