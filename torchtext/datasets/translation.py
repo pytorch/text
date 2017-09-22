0c1f0e9977994c277ce60844356943105bc9c521
Bryan McCann <bryan.mccann.is@gmail.com>
2017-09-21 17:26:09 -0700

    iwslt (#126)

--- torchtext/datasets/translation.py
+++ torchtext/datasets/translation.py
@@ -1,4 +1,6 @@
 import os
+import xml.etree.ElementTree as ET
+import glob
 
 from .. import data
 
@@ -38,7 +40,7 @@ class TranslationDataset(data.Dataset):
 
 
 class Multi30k(TranslationDataset, data.Dataset):
-    """Defines a dataset for the multi-modal WMT 2017 task"""
+    """Defines a dataset for the multi-modal WMT 2016 task"""
 
     urls = ['http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/training.tar.gz',
             'http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/validation.tar.gz',
@@ -73,3 +75,70 @@ class Multi30k(TranslationDataset, data.Dataset):
             os.path.join(path, test), exts, fields, **kwargs)
         return tuple(d for d in (train_data, val_data, test_data)
                      if d is not None)
+
+
+class IWSLT(TranslationDataset, data.Dataset):
+    """Defines a dataset for the IWSLT 2016 task"""
+
+    urls = ['https://wit3.fbk.eu/archive/2016-01//texts/{}/{}/{}.tgz']
+    name = 'iwslt'
+    dirname = None
+
+    @classmethod
+    def splits(cls, exts, fields, root='.data',
+               train='train', val='IWSLT16.TED.tst2013',
+               test='IWSLT16.TED.tst2014', **kwargs):
+        """Create dataset objects for splits of the IWSLT dataset.
+
+        Arguments:
+
+            root: directory containing Multi30k data
+            exts: A tuple containing the extension to path for each language.
+            fields: A tuple containing the fields that will be used for data
+                in each language.
+            train: The prefix of the train data. Default: 'train'.
+            validation: The prefix of the validation data. Default: 'val'.
+            test: The prefix of the test data. Default: 'test'.
+            Remaining keyword arguments: Passed to the splits method of
+                Dataset.
+        """
+        cls.dirname = '{}-{}'.format(exts[0][1:], exts[1][1:])
+        cls.urls[0] = cls.urls[0].format(exts[0][1:], exts[1][1:], cls.dirname)
+        path = cls.download(root)
+
+        train = '.'.join([train, cls.dirname])
+        val = '.'.join([val, cls.dirname])
+        test = '.'.join([test, cls.dirname])
+
+        if not os.path.exists(os.path.join(path, train) + exts[0]):
+            cls.clean(path)
+
+        train_data = None if train is None else cls(
+            os.path.join(path, train), exts, fields, **kwargs)
+        val_data = None if val is None else cls(
+            os.path.join(path, val), exts, fields, **kwargs)
+        test_data = None if test is None else cls(
+            os.path.join(path, test), exts, fields, **kwargs)
+        return tuple(d for d in (train_data, val_data, test_data)
+                     if d is not None)
+
+    @staticmethod
+    def clean(path):
+        for f_xml in glob.iglob(os.path.join(path, '*.xml')):
+            print(f_xml)
+            f_txt = os.path.splitext(f_xml)[0]
+            with open(f_txt, 'w') as fd_txt:
+                root = ET.parse(f_xml).getroot()[0]
+                for doc in root.findall('doc'):
+                    for e in doc.findall('seg'):
+                        fd_txt.write(e.text.strip() + '\n')
+
+        xml_tags = ['<url', '<keywords', '<talkid', '<description',
+                    '<reviewer', '<translator', '<title', '<speaker']
+        for f_orig in glob.iglob(os.path.join(path, 'train.tags*')):
+            print(f_orig)
+            f_txt = f_orig.replace('.tags', '')
+            with open(f_txt, 'w') as fd_txt, open(f_orig) as fd_orig:
+                for l in fd_orig:
+                    if not any(tag in l for tag in xml_tags):
+                        fd_txt.write(l.strip() + '\n')
