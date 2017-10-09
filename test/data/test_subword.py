47edd423b48479bf5c5a65eed9ce42af038a43c7
James Bradbury <jekbradbury@gmail.com>
2017-10-08 20:21:10 -0700

    use CPU device for subword test

--- test/data/test_subword.py
+++ test/data/test_subword.py
@@ -15,7 +15,7 @@ class TestSubword(unittest.TestCase):
         TEXT.build_vocab(cooked, max_size=100)
         TEXT.segment(cooked)
         print(cooked[0].text)
-        batch = next(iter(data.Iterator(cooked, 1, shuffle=False)))
+        batch = next(iter(data.Iterator(cooked, 1, shuffle=False, device=-1)))
         self.assertEqual(TEXT.reverse(batch.text.data)[0], raw[0].text)
 
 
