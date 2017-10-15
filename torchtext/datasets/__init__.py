fe322c8206a204b7eb81d972b11d0859e1e2a088
jekbradbury <jekbradbury@gmail.com>
2017-10-15 16:57:47 -0700

    work around flake8 issue

--- torchtext/datasets/__init__.py
+++ torchtext/datasets/__init__.py
@@ -1,7 +1,7 @@
-from .language_modeling import LanguageModelingDataset, WikiText2
+from .language_modeling import LanguageModelingDataset, WikiText2 #NOQA
 from .snli import SNLI
 from .sst import SST
-from .translation import TranslationDataset, Multi30k, IWSLT, WMT14
+from .translation import TranslationDataset, Multi30k, IWSLT, WMT14 #NOQA
 from .trec import TREC
 from .imdb import IMDB
 
