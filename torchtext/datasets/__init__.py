e066e31a5f06912ab8033de304b08d566a80d468
jekbradbury <jekbradbury@gmail.com>
2017-10-15 17:16:53 -0700

    aaah

--- torchtext/datasets/__init__.py
+++ torchtext/datasets/__init__.py
@@ -1,7 +1,7 @@
-from .language_modeling import LanguageModelingDataset, WikiText2 # NOQA
+from .language_modeling import LanguageModelingDataset, WikiText2  # NOQA
 from .snli import SNLI
 from .sst import SST
-from .translation import TranslationDataset, Multi30k, IWSLT, WMT14 # NOQA
+from .translation import TranslationDataset, Multi30k, IWSLT, WMT14  # NOQA
 from .trec import TREC
 from .imdb import IMDB
 
