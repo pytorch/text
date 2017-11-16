5003ffb56017768c1dbafae81587321c5652729b
keon <kwk236@gmail.com>
2017-11-16 03:15:39 -0500

    allow __version__ to be used

--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
+import os
+import io
+import re
+import shutil
+import sys
 from setuptools import setup, find_packages
 
-VERSION = '0.2.0'
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+VERSION = find_version('torchtext', '__init__.py')
 
 long_description = '''torch-text provides text and NLP data utilities
 and datasets for torch'''
