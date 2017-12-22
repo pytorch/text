4f2643590d43cbf57dc08d6c52f09e5c1ee6bbbb
jekbradbury <jekbradbury@gmail.com>
2017-12-22 02:32:20 -0500

    Update iterator.py

--- torchtext/data/iterator.py
+++ torchtext/data/iterator.py
@@ -268,8 +268,9 @@ class BucketIterator(Iterator):
                                 random_shuffler=self.random_shuffler)
 
 
-def batch(data, batch_size, batch_size_fn=lambda new, count, sofar: count):
+def batch(data, batch_size, batch_size_fn=None):
     """Yield elements from data in chunks of batch_size."""
+    if batch_size_fn is None: batch_size_fn = lambda new, count, sofar: count
     minibatch, size_so_far = [], 0
     for ex in data:
         minibatch.append(ex)
