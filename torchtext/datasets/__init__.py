28722590cdeaa0a796c22b16e9826d44023beabf
jekbradbury <jekbradbury@gmail.com>
2017-12-22 03:29:02 -0500

    Update __init__.py

--- torchtext/datasets/__init__.py
+++ torchtext/datasets/__init__.py
@@ -2,7 +2,7 @@ from .language_modeling import LanguageModelingDataset, WikiText2  # NOQA
 from .snli import SNLI
 from .sst import SST
 from .translation import TranslationDataset, Multi30k, IWSLT, WMT14  # NOQA
-from .sequence_labeling import SequenceLabelingDataset
+from .sequence_tagging import SequenceTaggingDataset
 from .trec import TREC
 from .imdb import IMDB
 
@@ -17,4 +17,4 @@ __all__ = ['LanguageModelingDataset',
            'WikiText2',
            'TREC',
            'IMDB',
-           'SequenceLabelingDataset']
+           'SequenceTaggingDataset']
