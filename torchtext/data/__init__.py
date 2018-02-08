f5e68272f142d5d67770b84de058479bef91cd72
mttk <martin.tutek@gmail.com>
2018-02-08 14:52:35 +0100

    label field

--- torchtext/data/__init__.py
+++ torchtext/data/__init__.py
@@ -1,7 +1,7 @@
 from .batch import Batch
 from .dataset import Dataset, TabularDataset
 from .example import Example
-from .field import RawField, Field, ReversibleField, SubwordField, NestedField
+from .field import RawField, Field, ReversibleField, SubwordField, NestedField, LabelField
 from .iterator import (batch, BucketIterator, Iterator, BPTTIterator,
                        pool)
 from .pipeline import Pipeline
@@ -11,6 +11,7 @@ __all__ = ["Batch",
            "Dataset", "TabularDataset",
            "Example",
            "RawField", "Field", "ReversibleField", "SubwordField", "NestedField",
+           "LabelField",
            "batch", "BucketIterator", "Iterator", "BPTTIterator",
            "pool",
            "Pipeline",
--- torchtext/data/field.py
+++ torchtext/data/field.py
@@ -604,3 +604,18 @@ class NestedField(Field):
                 arr, device=device, train=train)
             numericalized.append(numericalized_ex)
         return torch.stack(numericalized)
+
+
+class LabelField(Field):
+    """A Label field.
+
+    A label field is a shallow wrapper around a standard field designed to hold labels
+    for a classification task. Its only use is to set the unk_token and sequential to
+    `None` by default.
+    """
+    def __init__(self, **kwargs):
+        # whichever value is set for sequential and unk_token will be overwritten
+        kwargs['sequential'] = False
+        kwargs['unk_token'] = None
+
+        super(LabelField, self).__init__(**kwargs)
