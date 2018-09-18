13f5e3b35918e96395f2055adca075203da9f454
Prasetya Utama <p.ajie.utama@gmail.com>
2018-09-18 09:00:43 +0200

    fixed ParseTextField postprocessing, added genre field to MultiNLI (#386)
    * fixed ParseTextField postprocessing function, added option to whether reverse ParseTextField, added genre field multinli
    
    * fix code lines length
    
    * remove lambda expression

--- test/nli.py
+++ test/nli.py
@@ -2,58 +2,65 @@ from torchtext import data
 from torchtext import datasets
 
 # Testing SNLI
-TEXT = data.Field()
-LABEL = data.Field(sequential=False)
+print("Run test on SNLI...")
+TEXT = datasets.nli.ParsedTextField()
+LABEL = data.LabelField()
+TREE = datasets.nli.ShiftReduceField()
 
-train, val, test = datasets.SNLI.splits(TEXT, LABEL)
+train, val, test = datasets.SNLI.splits(TEXT, LABEL, TREE)
 
-print(train.fields)
-print(len(train))
-print(vars(train[0]))
+print("Fields:", train.fields)
+print("Number of examples:\n", len(train))
+print("First Example instance:\n", vars(train[0]))
 
 TEXT.build_vocab(train)
 LABEL.build_vocab(train)
 
-train_iter, val_iter, test_iter = data.BucketIterator.splits(
-    (train, val, test), batch_size=3)
+train_iter, val_iter, test_iter = data.Iterator.splits((train, val, test), batch_size=3)
 
 batch = next(iter(train_iter))
-print(batch.premise)
-print(batch.hypothesis)
-print(batch.label)
+print("Numericalize premises:\n", batch.premise)
+print("Numericalize hypotheses:\n", batch.hypothesis)
+print("Entailment labels:\n", batch.label)
 
-train_iter, val_iter, test_iter = datasets.SNLI.iters(batch_size=4)
+print("Test iters function")
+train_iter, val_iter, test_iter = datasets.SNLI.iters(batch_size=4, trees=True)
 
 batch = next(iter(train_iter))
-print(batch.premise)
-print(batch.hypothesis)
-print(batch.label)
+print("Numericalize premises:\n", batch.premise)
+print("Numericalize hypotheses:\n", batch.hypothesis)
+print("Entailment labels:\n", batch.label)
 
 
 # Testing MultiNLI
-TEXT = data.Field()
-LABEL = data.Field(sequential=False)
+print("Run test on MultiNLI...")
+TEXT = datasets.nli.ParsedTextField()
+LABEL = data.LabelField()
+GENRE = data.LabelField()
+TREE = datasets.nli.ShiftReduceField()
 
-train, val, test = datasets.MultiNLI.splits(TEXT, LABEL)
+train, val, test = datasets.MultiNLI.splits(TEXT, LABEL, TREE, GENRE)
 
-print(train.fields)
-print(len(train))
-print(vars(train[0]))
+print("Fields:", train.fields)
+print("Number of examples:\n", len(train))
+print("First Example instance:\n", vars(train[0]))
 
 TEXT.build_vocab(train)
 LABEL.build_vocab(train)
+GENRE.build_vocab(train, val, test)
 
-train_iter, val_iter, test_iter = data.BucketIterator.splits(
-    (train, val, test), batch_size=3)
-
-batch = next(iter(train_iter))
-print(batch.premise)
-print(batch.hypothesis)
-print(batch.label)
-
-train_iter, val_iter, test_iter = datasets.MultiNLI.iters(batch_size=4)
+train_iter, val_iter, test_iter = data.Iterator.splits((train, val, test), batch_size=3)
 
 batch = next(iter(train_iter))
-print(batch.premise)
-print(batch.hypothesis)
-print(batch.label)
+print("Numericalize premises:\n", batch.premise)
+print("Numericalize hypotheses:\n", batch.hypothesis)
+print("Entailment labels:\n", batch.label)
+print("Genre categories:\n", batch.genre)
+
+print("Test iters function")
+train_iter, val_iter, test_iter = datasets.MultiNLI.iters(batch_size=4, trees=True)
+
+batch = next(iter(train_iter))
+print("Numericalize premises:\n", batch.premise)
+print("Numericalize hypotheses:\n", batch.hypothesis)
+print("Entailment labels:\n", batch.label)
