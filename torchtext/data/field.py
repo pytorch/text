8a2fb6d19c7ffa59f84bde098729b48610ce32e6
keitakurita <keita.kurita@gmail.com>
2018-09-21 08:00:48 -0400

    Feature to ignore tokens (#397)
    * Add stopword filtering to fields

--- torchtext/data/field.py
+++ torchtext/data/field.py
@@ -106,9 +106,9 @@ class Field(RawField):
         unk_token: The string token used to represent OOV words. Default: "<unk>".
         pad_first: Do the padding of the sequence at the beginning. Default: False.
         truncate_first: Do the truncating of the sequence at the beginning. Default: False
+        stop_words: Tokens to discard during the preprocessing step. Default: None
         is_target: Whether this field is a target variable.
             Affects iteration over batches. Default: False
-        truncate_first: Do the truncating of the sequence at the beginning. Default: False
     """
 
     vocab_cls = Vocab
@@ -137,7 +137,8 @@ class Field(RawField):
                  preprocessing=None, postprocessing=None, lower=False,
                  tokenize=(lambda s: s.split()), include_lengths=False,
                  batch_first=False, pad_token="<pad>", unk_token="<unk>",
-                 pad_first=False, truncate_first=False, is_target=False):
+                 pad_first=False, truncate_first=False, stop_words=None,
+                 is_target=False):
         self.sequential = sequential
         self.use_vocab = use_vocab
         self.init_token = init_token
@@ -154,6 +155,14 @@ class Field(RawField):
         self.pad_token = pad_token if self.sequential else None
         self.pad_first = pad_first
         self.truncate_first = truncate_first
+        if stop_words is not None:
+            try:
+                self.stop_words = set(stop_words)
+            except:
+                raise ValueError("Stop words must be convertible to a set")
+        else:
+            self.stop_words = stop_words
+        self.stop_words = stop_words
         self.is_target = is_target
 
     def preprocess(self, x):
@@ -170,6 +179,8 @@ class Field(RawField):
             x = self.tokenize(x.rstrip('\n'))
         if self.lower:
             x = Pipeline(six.text_type.lower)(x)
+        if self.sequential and self.use_vocab and self.stop_words is not None:
+            x = [w for w in x if w not in self.stop_words]
         if self.preprocessing is not None:
             return self.preprocessing(x)
         else:
