fd9eda4be5513f34e6ffab8aaa4ca287e528d83c
mttk <martin.tutek@gmail.com>
2018-09-21 22:14:15 +0200

    one line

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import torchtext
+# import torchtext
 import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
