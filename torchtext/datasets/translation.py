a92a2601a3dfb6e2485b21b4aba490bfbbaa943a
Martin Tutek <martin.tutek@gmail.com>
2018-09-24 17:19:06 +0200

    Fix clean method in iwsl (#408)

--- torchtext/datasets/translation.py
+++ torchtext/datasets/translation.py
@@ -2,6 +2,7 @@ import os
 import xml.etree.ElementTree as ET
 import glob
 import io
+import codecs
 
 from .. import data
 
@@ -163,7 +164,7 @@ class IWSLT(TranslationDataset):
         for f_xml in glob.iglob(os.path.join(path, '*.xml')):
             print(f_xml)
             f_txt = os.path.splitext(f_xml)[0]
-            with io.open(f_txt, mode='w', encoding='utf-8') as fd_txt:
+            with codecs.open(f_txt, mode='w', encoding='utf-8') as fd_txt:
                 root = ET.parse(f_xml).getroot()[0]
                 for doc in root.findall('doc'):
                     for e in doc.findall('seg'):
@@ -174,7 +175,7 @@ class IWSLT(TranslationDataset):
         for f_orig in glob.iglob(os.path.join(path, 'train.tags*')):
             print(f_orig)
             f_txt = f_orig.replace('.tags', '')
-            with io.open(f_txt, mode='w', encoding='utf-8') as fd_txt, \
+            with codecs.open(f_txt, mode='w', encoding='utf-8') as fd_txt, \
                     io.open(f_orig, mode='r', encoding='utf-8') as fd_orig:
                 for l in fd_orig:
                     if not any(tag in l for tag in xml_tags):
