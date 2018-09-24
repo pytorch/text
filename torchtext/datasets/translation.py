aea228b19cf3742ccb5af4eacbab2cf22669b35f
mttk <martin.tutek@gmail.com>
2018-09-24 16:31:02 +0200

    Swap io.open to codecs.open

--- torchtext/datasets/translation.py
+++ torchtext/datasets/translation.py
@@ -1,4 +1,5 @@
 import os
+import codecs
 import xml.etree.ElementTree as ET
 import glob
 import io
@@ -165,7 +166,7 @@ class IWSLT(TranslationDataset):
         for f_xml in glob.iglob(os.path.join(path, '*.xml')):
             print(f_xml)
             f_txt = os.path.splitext(f_xml)[0]
-            with io.open(f_txt, mode='w', encoding='utf-8') as fd_txt:
+            with codecs.open(f_txt, mode='w', encoding='utf-8') as fd_txt:
                 root = ET.parse(f_xml).getroot()[0]
                 for doc in root.findall('doc'):
                     for e in doc.findall('seg'):
@@ -176,7 +177,7 @@ class IWSLT(TranslationDataset):
         for f_orig in glob.iglob(os.path.join(path, 'train.tags*')):
             print(f_orig)
             f_txt = f_orig.replace('.tags', '')
-            with io.open(f_txt, mode='w', encoding='utf-8') as fd_txt, \
+            with codecs.open(f_txt, mode='w', encoding='utf-8') as fd_txt, \
                     io.open(f_orig, mode='r', encoding='utf-8') as fd_orig:
                 for l in fd_orig:
                     if not any(tag in l for tag in xml_tags):
