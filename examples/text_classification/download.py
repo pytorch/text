9ce7c68b10f0a46efec65b5b6893c4ff7649179d
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-07-26 14:50:53 -0700

    update download.py

--- examples/text_classification/download.py
+++ examples/text_classification/download.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Download and extract a given dataset')
     parser.add_argument('dataset', choices=text_classification.DATASETS)
-    parser.add_argument('root')
+    parser.add_argument('--data', default='.data')
     parser.add_argument('--logging-level', default='WARNING')
     args = parser.parse_args()
 
     logging.basicConfig(level=getattr(logging, args.logging_level))
 
-    tar_file = download_from_url(URLS[args.dataset], args.root)
-    extracted_files = extract_archive(tar_file, args.root)
+    tar_file = download_from_url(URLS[args.dataset], args.data)
+    extracted_files = extract_archive(tar_file, args.data)
     print("Downloaded and extracted files:")
     for extracted_file in extracted_files:
         print(extracted_file)
