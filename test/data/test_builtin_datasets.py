b8e00dea7fc381c33dd7e003501fec5a98f6faf0
KK <bebound@gmail.com>
2019-07-28 00:13:38 +0800

    Add ATIS test

--- test/data/test_builtin_datasets.py
+++ test/data/test_builtin_datasets.py
@@ -1,7 +1,6 @@
 import os
 import torchtext.data as data
-from torchtext.datasets import WikiText2, PennTreebank
-from torchtext.datasets import AG_NEWS
+from torchtext.datasets import WikiText2, PennTreebank, AG_NEWS, ATIS
 
 from ..common.test_markers import slow
 from ..common.torchtext_test_case import TorchtextTestCase
@@ -60,4 +59,26 @@ class TestDataset(TorchtextTestCase):
         # Delete the dataset after we're done to save disk space on CI
         if os.environ.get("TRAVIS") == "true":
             datafile = os.path.join(self.project_root, ".data", "AG_NEWS")
+            conditional_remove(datafile)
+
+    def test_atis(self):
+        # smoke test to ensure ATIS works properly
+        TEXT = data.Field(lower=True, batch_first=True)
+        SLOT = data.Field(batch_first=True, unk_token=None)
+        INTENT = data.Field(batch_first=True, unk_token=None)
+
+        # make splits for data
+        train, val, test = ATIS.splits(
+            fields=(('text', TEXT), ('slot', SLOT), ('intent', INTENT)))
+
+        TEXT.build_vocab(train)
+        SLOT.build_vocab(train, val, test)
+        INTENT.build_vocab(train, val, test)
+
+        train_iter, val_iter, test_iter = data.BucketIterator.splits(
+            (train, val, test), batch_sizes=(32, 256, 256))
+
+        # Delete the dataset after we're done to save disk space on CI
+        if os.environ.get("TRAVIS") == "true":
+            datafile = os.path.join(self.project_root, ".data", "atis")
             conditional_remove(datafile)
