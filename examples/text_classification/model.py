e93f8b572c72bbdb62dcfcc2b323861b6a4ce7eb
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-07-30 09:00:08 -0700

    Docs to model.py.

--- examples/text_classification/model.py
+++ examples/text_classification/model.py
@@ -1,5 +1,15 @@
 import torch.nn as nn
 
+r'''
+The model is composed of the embeddingbag layer and the linear layer.
+nn.EmbeddingBag computes the mean of 'bags' of embeddings. Since it
+doesn't instantiate the intermediate embeddings, nn.EmbeddingBag can
+enhance the performance and memory efficiency to process a sequence
+of tensors. Additionally, the text entries here have different lengths.
+nn.EmbeddingBag requires no padding here so this method is much faster
+than the original one with TorchText Iterator and Batch.
+
+'''
 
 class TextSentiment(nn.Module):
     def __init__(self, vocab_size, embed_dim, num_class):
@@ -15,4 +25,10 @@ class TextSentiment(nn.Module):
         self.fc.bias.data.zero_()
 
     def forward(self, text, offsets):
+        r'''
+        Arguments:
+            text: a bag of text tensors
+            offsets: a list of offsets
+
+        '''
         return self.fc(self.embedding(text, offsets))
