67c8720f2163047014e6954b1eda336ee3c68858
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-07-30 09:22:21 -0700

    Comments to predict.

--- examples/text_classification/predict.py
+++ examples/text_classification/predict.py
@@ -6,6 +6,18 @@ from torchtext.datasets.text_classification import text_normalize
 
 
 def predict(text, model, dictionary):
+    r'''
+    The predict() function here is used to test the model on a sample text.
+    The input text is numericalized with the dictionary and then sent to
+    the model for inference.
+
+    Arguments:
+        text: a sample text string
+        model: the trained model
+        dictionary: a vocab object for the information of string-to-index
+
+    '''
+
     with torch.no_grad():
         text = torch.tensor([dictionary.get(token, dictionary['<unk>'])
                              for token in text_normalize(text)])
