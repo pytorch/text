95ffe821175bc266c667f489477d5183fb102ac6
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-07-30 17:13:51 -0700

    Minor edit.

--- examples/text_classification/train.py
+++ examples/text_classification/train.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from model import TextSentiment
 
 r"""
-This file shows the training process of the text sentiment model.
+This file shows the training process of the text classification model.
 """
 
 
