d95838ce7e36faafa878ca31bc136a5e91603e62
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-07-31 11:34:07 -0700

    Add ngrams to predict.py

--- examples/text_classification/predict.py
+++ examples/text_classification/predict.py
@@ -2,10 +2,11 @@ import torch
 import sys
 import argparse
 from torchtext.data.utils import get_tokenizer
+from torchtext.data.utils import ngrams_iterator
 
 
-def predict(text, model, dictionary):
-    r'''
+def predict(text, model, dictionary, ngrams):
+    r"""
     The predict() function here is used to test the model on a sample text.
     The input text is numericalized with the vocab and then sent to
     the model for inference.
@@ -14,11 +15,12 @@ def predict(text, model, dictionary):
         text: a sample text string
         model: the trained model
         dictionary: a vocab object for the information of string-to-index
-
-    '''
+        ngrams: the number of ngrams.
+    """
     tokenizer = get_tokenizer("basic_english")
     with torch.no_grad():
-        text = torch.tensor([dictionary[token] for token in tokenizer(text)])
+        text = torch.tensor([dictionary[token]
+                            for token in ngrams_iterator(tokenizer(text), ngrams)])
         output = model(text, torch.tensor([0]))
         return output.argmax(1).item() + 1
 
@@ -28,9 +30,10 @@ if __name__ == "__main__":
         description='Predict text from stdin given model and dictionary')
     parser.add_argument('model')
     parser.add_argument('dictionary')
+    parser.add_argument('--ngrams', type=int, default=2)
     args = parser.parse_args()
 
     model = torch.load(args.model)
     dictionary = torch.load(args.dictionary)
     for line in sys.stdin:
-        print(predict(line, model, dictionary))
+        print(predict(line, model, dictionary, args.ngrams))
