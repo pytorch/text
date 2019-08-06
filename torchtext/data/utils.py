40a2be44d617fa19eae056189b6912dd8285cc69
ShahriarSS <shahriar25.ss@gmail.com>
2019-08-07 00:28:02 +0430

    Replaced the function

--- torchtext/data/utils.py
+++ torchtext/data/utils.py
@@ -1,4 +1,5 @@
 import random
+from torchtext import _C
 from contextlib import contextmanager
 from copy import deepcopy
 import re
@@ -43,34 +44,6 @@ _replacements = [' \'  ',
 _patterns_dict = list((re.compile(p), r) for p, r in zip(_patterns, _replacements))
 
 
-def _basic_english_normalize(line):
-    r"""
-    Basic normalization for a line of text.
-    Normalization includes
-    - lowercasing
-    - complete some basic text normalization for English words as follows:
-        add spaces before and after '\''
-        remove '\"',
-        add spaces before and after '.'
-        replace '<br \/>'with single space
-        add spaces before and after ','
-        add spaces before and after '('
-        add spaces before and after ')'
-        add spaces before and after '!'
-        add spaces before and after '?'
-        replace ';' with single space
-        replace ':' with single space
-        replace multiple spaces with single space
-
-    Returns a list of tokens after splitting on whitespace.
-    """
-
-    line = line.lower()
-    for pattern_re, replaced_str in _patterns_dict:
-        line = pattern_re.sub(replaced_str, line)
-    return line.split()
-
-
 def get_tokenizer(tokenizer, language='en'):
     r"""
     Generate tokenizer function for a string sentence.
@@ -102,7 +75,7 @@ def get_tokenizer(tokenizer, language='en'):
     if tokenizer == "basic_english":
         if language != 'en':
             raise ValueError("Basic normalization is only available for Enlish(en)")
-        return _basic_english_normalize
+        return _C.basic_english_normalize
 
     # simply return if a function is passed
     if callable(tokenizer):
