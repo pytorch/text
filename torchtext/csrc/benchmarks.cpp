b65cfd714a25c9122d8b765051f8bda6ecd7ce96
ShahriarSS <shahriar25.ss@gmail.com>
2019-08-06 16:43:31 +0430

    Fixed a few stuff

--- torchtext/csrc/benchmarks.cpp
+++ torchtext/csrc/benchmarks.cpp
@@ -3,12 +3,11 @@
 #include "text.h"
 
 static void benchmark_basic_english_normalize(benchmark::State& state) {
-  std::ifstream stream("assets/benchmakr-article.txt");
+  std::ifstream stream("benchmark-article.txt");
   auto it = std::istreambuf_iterator<std::string::value_type>(stream);
   auto lines = torch::text::core::impl::split(std::string(it, {}), '\n');
 
   for (auto _ : state) {
-    std::cout << "Hi" << std::endl;
     std::vector<std::string> words;
     for (const auto& line : lines) {
       auto list = torch::text::core::impl::basic_english_normalize(line);
