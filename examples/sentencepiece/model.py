5f4ee686c36bc6b7841e641fb57115d715f6b8e7
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-09-10 13:19:16 -0700

    Remove vocab.

--- examples/sentencepiece/model.py
+++ examples/sentencepiece/model.py
@@ -264,7 +264,7 @@ def test(data_):
 import time
 from torch.utils.data.dataset import random_split
 N_EPOCHS = 5
-N_EPOCHS = 4
+N_EPOCHS = 8
 min_valid_loss = float('inf')
 
 criterion = torch.nn.CrossEntropyLoss().to(device)
@@ -378,7 +378,7 @@ ag_news_label = {1 : "World",
                  3 : "Business",
                  4 : "Sci/Tec"}
 
-def predict(text, model, vocab, ngrams):
+def predict(text, model):
 #    tokenizer = get_tokenizer("basic_english")
 #    tokenizer = get_tokenizer("sentencepiece", spm_name="m_user.model")
     with torch.no_grad():
@@ -395,10 +395,9 @@ def predict(text, model, vocab, ngrams):
 
 ex_text_str = "MEMPHIS, Tenn. – Four days ago, Jon Rahm was     enduring the season’s worst weather conditions on Sunday at The     Open on his way to a closing 75 at Royal Portrush, which     considering the wind and the rain was a respectable showing.     Thursday’s first round at the WGC-FedEx St. Jude Invitational     was another story. With temperatures in the mid-80s and hardly any     wind, the Spaniard was 13 strokes better in a flawless round.     Thanks to his best putting performance on the PGA Tour, Rahm     finished with an 8-under 62 for a three-stroke lead, which     was even more impressive considering he’d never played the     front nine at TPC Southwind."
 
-vocab = train_dataset.get_vocab()
 model = model.to("cpu")
 
-print("This is a %s news" %ag_news_label[predict(ex_text_str, model, vocab, 2)])
+print("This is a %s news" %ag_news_label[predict(ex_text_str, model)])
 
 
 # This is a Sports news
