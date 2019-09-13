5fd36de6330ebb9eb30055cdc0bef4fe53846350
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-09-13 07:23:53 -0700

    byte string in Python2 and Unicode string in Python3 respectively

--- test/data/test_utils.py
+++ test/data/test_utils.py
@@ -77,10 +77,18 @@ class TestUtils(TorchtextTestCase):
         model_path = 'test/asset/spm_example.model'
         tokenizer = data.get_tokenizer("sentencepiece", spm_name=model_path)
 
-        ref_results = ['\u2581Sent', 'ence', 'P', 'ie', 'ce', '\u2581is',
-                       '\u2581an', '\u2581un', 'super', 'vis', 'ed', '\u2581text',
-                       '\u2581to', 'ken', 'izer', '\u2581and',
-                       '\u2581de', 'to', 'ken', 'izer']
+        import sys
+        # Handle byte string in Python2 and Unicode string in Python3, respectively
+        if sys.version_info < (3, 0):
+            ref_results = ['\xe2\x96\x81Sent', 'ence', 'P', 'ie', 'ce', '\xe2\x96\x81is',
+                           '\xe2\x96\x81an', '\xe2\x96\x81un', 'super', 'vis', 'ed',
+                           '\xe2\x96\x81text', '\xe2\x96\x81to', 'ken', 'izer',
+                           '\xe2\x96\x81and', '\xe2\x96\x81de', 'to', 'ken', 'izer']
+        else:
+            ref_results = ['\u2581Sent', 'ence', 'P', 'ie', 'ce', '\u2581is',
+                           '\u2581an', '\u2581un', 'super', 'vis', 'ed', '\u2581text',
+                           '\u2581to', 'ken', 'izer', '\u2581and',
+                           '\u2581de', 'to', 'ken', 'izer']
 
         self.assertEqual(tokenizer(test_sample), ref_results)
 
