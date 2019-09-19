ac2a8ee3446bd3bf7a23170b93715f164a6ff91c
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-09-19 08:30:20 -0700

    Switch to torch.nn.Module.

--- torchtext/data/transforms.py
+++ torchtext/data/transforms.py
@@ -1,10 +1,13 @@
+import torch
+import torch.nn as nn
 import sentencepiece as spm
 
 
-class SentencePieceTransform(object):
+class SentencePieceTransform(nn.Module):
     def __init__(self, spm_filename):
+        super(SentencePieceTransform, self).__init__()
         self.sp_model = spm.SentencePieceProcessor()
         self.sp_model.Load(spm_filename)
 
-    def __call__(self, txt_str):
+    def forward(self, txt_str):
         return self.sp_model.EncodeAsIds(txt_str)
