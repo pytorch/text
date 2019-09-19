d5e159d7c9e269cc51f1faf3ba64759968d90014
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-09-19 07:08:55 -0700

    Update test result.

--- test/data/test_transforms.py
+++ test/data/test_transforms.py
@@ -8,18 +8,6 @@ class TestUtils(TorchtextTestCase):
         test_sample = 'SentencePiece is an unsupervised text tokenizer and detokenizer'
         model_path = 'test/asset/spm_example.model'
         sp_transform = SentencePieceTransform(model_path)
-
-        import sys
-        # Handle byte string in Python2 and Unicode string in Python3, respectively
-        if sys.version_info < (3, 0):
-            ref_results = ['\xe2\x96\x81Sent', 'ence', 'P', 'ie', 'ce', '\xe2\x96\x81is',
-                           '\xe2\x96\x81an', '\xe2\x96\x81un', 'super', 'vis', 'ed',
-                           '\xe2\x96\x81text', '\xe2\x96\x81to', 'ken', 'izer',
-                           '\xe2\x96\x81and', '\xe2\x96\x81de', 'to', 'ken', 'izer']
-        else:
-            ref_results = ['\u2581Sent', 'ence', 'P', 'ie', 'ce', '\u2581is',
-                           '\u2581an', '\u2581un', 'super', 'vis', 'ed', '\u2581text',
-                           '\u2581to', 'ken', 'izer', '\u2581and',
-                           '\u2581de', 'to', 'ken', 'izer']
-
+        ref_results = [15340, 4286, 981, 1207, 1681, 17, 84, 684, 8896, 5366,
+                       144, 3689, 9, 5602, 12114, 6, 560, 649, 5602, 12114]
         self.assertEqual(sp_transform(test_sample), ref_results)
