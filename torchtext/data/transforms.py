2f859f49b5dc17d6ceac21ddefe56525f496ae3a
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-09-19 08:50:37 -0700

    Add docs for SentencePieceTransform.

--- torchtext/data/transforms.py
+++ torchtext/data/transforms.py
@@ -4,10 +4,30 @@ import sentencepiece as spm
 
 
 class SentencePieceTransform(nn.Module):
-    def __init__(self, spm_filename):
+    """A tokenizer transform based on SentencePiece model.
+
+    Arguments:
+        filename: the data file for saving SentencePiece model.
+
+    Examples:
+        >>> from torchtext.data.transforms import SentencePieceTransform
+        >>> sp_transform = SentencePieceTransform('m_user.model')
+        >>> sp_transform('SentencePiece is an unsupervised text tokenizer')
+    """
+
+    def __init__(self, filename):
         super(SentencePieceTransform, self).__init__()
         self.sp_model = spm.SentencePieceProcessor()
-        self.sp_model.Load(spm_filename)
+        self.sp_model.Load(filename)
 
     def forward(self, txt_str):
+        """Transform a sentence text into a list of integer.
+
+        Arguments:
+            txt_str: input sentence text.
+
+        Outputs:
+            output: a list of integers based on SentencePiece model.
+        """
+
         return self.sp_model.EncodeAsIds(txt_str)
