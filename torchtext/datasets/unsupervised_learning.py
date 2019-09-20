4faa7399c1ef7ca66fccda5e27ead7746e50addb
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-09-20 12:14:41 -0700

    Finalize the txt classification.

--- torchtext/datasets/unsupervised_learning.py
+++ torchtext/datasets/unsupervised_learning.py
@@ -3,18 +3,18 @@ time1 = time.time()
 import re
 
 byte_list = []
-#with open("normalized_enwik9_short.txt") as f:
 with open("enwik9_short.txt") as f:
     for line in range(100):
         byte_list.append(f.tell())
         f.readline()
 
+
 def getLines(begin_line, num_lines):
-    #with open("normalized_enwik9_short.txt") as f:
     with open("enwik9_short.txt") as f:
         f.seek(byte_list[begin_line])
         for i in range(num_lines):
             print(f.readline())
+
 
 _patterns = [(r'<.*>', ''),
              (r'&amp;', '&'),
@@ -45,48 +45,12 @@ _patterns = [(r'<.*>', ''),
              (r'3', ' three '), (r'4', ' four '), (r'5', ' five '),
              (r'6', ' six '), (r'7', ' seven '), (r'8', ' eight '),
              (r'9', ' nine '),
-             (r'[^0-9a-zA-Z\n]+', ' '),
+             (r'[^a-z\n]+', ' '),
              (r'\n ', ''),
-             (r'\n\s*\n', '\n'),
-             (r'\s+', ' ')]
+             (r'\s+', ' '),
+             (r'\n\s*\n', r'\n')
+             ]
 
-_r1 = ['', '&', '<', '>',
-       '', '',
-       '[', '', '', '', '',
-       '', '[[$1]]',
-       '', '[[', '',
-       '', '', '', ' ',
-       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-       'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-       ' zero ', ' one ', ' two ', ' three ', ' four ', ' five ',
-       ' six ', ' seven ', ' eight ', ' nine ',
-       ' ', '', '\n']
-
-_pattern = [r'\'',
-            r'\"',
-            r'\.',
-            r'<br \/>',
-            r',',
-            r'\(',
-            r'\)',
-            r'\!',
-            r'\?',
-            r'\;',
-            r'\:',
-            r'\s+']
-
-_replacement = [' \'  ',
-                '',
-                ' . ',
-                ' ',
-                ' , ',
-                ' ( ',
-                ' ) ',
-                ' ! ',
-                ' ? ',
-                ' ',
-                ' ',
-                ' ']
 
 _patterns_dict = list((re.compile(p), r) for (p, r) in _patterns)
 
@@ -94,57 +58,24 @@ _patterns_dict = list((re.compile(p), r) for (p, r) in _patterns)
 def enwik9_normalize(line):
     r"""
     Basic normalization for a line of text.
-    Normalization includes
-    - lowercasing
-    - complete some basic text normalization for English words as follows:
-        add spaces before and after '\''
-        remove '\"',
-        add spaces before and after '.'
-        replace '<br \/>'with single space
-        add spaces before and after ','
-        add spaces before and after '('
-        add spaces before and after ')'
-        add spaces before and after '!'
-        add spaces before and after '?'
-        replace ';' with single space
-        replace ':' with single space
-        replace multiple spaces with single space
     Returns a list of tokens after splitting on whitespace.
     """
 
-#    line = line.lower()
     for pattern_re, replaced_str in _patterns_dict:
         line = pattern_re.sub(replaced_str, line)
     return line
 
 
-#getLines(20, 5)
 buffer_lines = []
-#with open("enwik9", 'r') as f1:
-#    with open("NORMAL_enwik9.txt", 'w') as f2:
-with open("enwik9_80000.txt", 'r') as f1:
-    with open("NORMAL_enwik9_80000.txt", 'w') as f2:
-#        t1, t2, t3, t4, t5 = 0.0, 0.0, 0.0, 0.0, 0.0
+with open("enwik9_8000.txt", 'r') as f1:
+    with open("NORMAL_enwik9_8000.txt", 'w') as f2:
         while True:
-#        for _i in range(80000):
-#            t1 = time.time()
             line = f1.readline()
-#            t2 = time.time()
             if not line:
                 break
-#            t3 = time.time()
             line = enwik9_normalize(line)
-#            t4 = time.time()
-#            print(_i, line, len(line), line == ' ', line == '')
             if line != ' ' and line != '':
                 if line[0] == ' ':
                     line = line[1:]
-
-                buffer_lines.append(line + '\n')
-#                f2.writelines(line + '\n')
-
-            if len(buffer_lines) % 5000 == 0:
-                f2.writelines(buffer_lines)
-#            t5 = time.time()
-#            print(t2 - t1, t3 - t2, t4 - t3, t5 - t4)
+                f2.writelines(line + '\n')
 print("total time: ", time.time() - time1)
