353869f8eaf89786602b2904aceebe0f4e8d3b59
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-10-03 11:07:09 -0700

    Minor change.

--- torchtext/data/__init__.py
+++ torchtext/data/__init__.py
@@ -9,7 +9,7 @@ from .utils import get_tokenizer, interleave_keys
 from .functional import generate_sp_model, \
     load_sp_model, \
     sentencepiece_numericalizer, \
-    sentencepiece_tokenizer, simple_space_split
+    sentencepiece_tokenizer, custom_replace, simple_space_split
 
 __all__ = ["Batch",
            "Dataset", "TabularDataset",
@@ -22,4 +22,4 @@ __all__ = ["Batch",
            "get_tokenizer", "interleave_keys",
            "generate_sp_model", "load_sp_model",
            "sentencepiece_numericalizer", "sentencepiece_tokenizer",
-           "simple_space_split"]
+           "custom_replace", "simple_space_split"]
