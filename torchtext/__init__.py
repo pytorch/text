45d53de2ad8ffa20f27b28bee593247cdff670ba
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-10-23 14:01:40 -0700

    Move PennTreebank, WikiText103, WikiText2 to torchtext.legacy

--- torchtext/__init__.py
+++ torchtext/__init__.py
@@ -2,10 +2,12 @@ from . import data
 from . import datasets
 from . import utils
 from . import vocab
+from . import legacy
 
 __version__ = '0.4.0'
 
 __all__ = ['data',
            'datasets',
            'utils',
-           'vocab']
+           'vocab',
+           'legacy']
---
+++ torchtext/legacy/__init__.py
@@ -0,0 +1,5 @@
+from . import datasets
+
+__version__ = '0.4.0'
+
+__all__ = ['datasets']
