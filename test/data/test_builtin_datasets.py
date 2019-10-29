cc127dec11c174533ca9a3f4d376220101e4293e
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-10-29 11:07:45 -0700

    Update tests.

--- test/data/test_builtin_datasets.py
+++ test/data/test_builtin_datasets.py
@@ -1,6 +1,5 @@
 import os
 import torchtext.data as data
-from torchtext.datasets import WikiText2, PennTreebank
 from torchtext.datasets import AG_NEWS
 
 from ..common.test_markers import slow
@@ -14,7 +13,8 @@ def conditional_remove(f):
 
 class TestDataset(TorchtextTestCase):
     @slow
-    def test_wikitext2(self):
+    def test_wikitext2_legacy(self):
+        from torchtext.legacy.datasets import WikiText2
         # smoke test to ensure wikitext2 works properly
         ds = WikiText2
         TEXT = data.Field(lower=True, batch_first=True)
@@ -31,8 +31,26 @@ class TestDataset(TorchtextTestCase):
             datafile = os.path.join(self.project_root, ".data", "wikitext-2")
             conditional_remove(datafile)
 
+    def test_wikitext2(self):
+        from torchtext.datasets import WikiText2
+        # smoke test to ensure wikitext2 works properly
+        train_dataset, test_dataset, valid_dataset = WikiText2()
+        self.assertEqual(len(train_dataset), 1947375)
+        self.assertEqual(len(test_dataset), 230357)
+        self.assertEqual(len(valid_dataset), 203947)
+
+        vocab = train_dataset.get_vocab()
+        tokens_ids = [vocab[token] for token in 'the player characters rest'.split()]
+        self.assertEqual(tokens_ids, [2, 285, 502, 699])
+
+        # Delete the dataset after we're done to save disk space on CI
+        if os.environ.get("TRAVIS") == "true":
+            datafile = os.path.join(self.project_root, ".data", "wikitext-2")
+            conditional_remove(datafile)
+
     @slow
-    def test_penntreebank(self):
+    def test_penntreebank_legacy(self):
+        from torchtext.legacy.datasets import PennTreebank
         # smoke test to ensure penn treebank works properly
         TEXT = data.Field(lower=True, batch_first=True)
         ds = PennTreebank
@@ -47,6 +65,27 @@ class TestDataset(TorchtextTestCase):
         # Delete the dataset after we're done to save disk space on CI
         if os.environ.get("TRAVIS") == "true":
             datafile = os.path.join(self.project_root, ".data", "penn-treebank")
+            conditional_remove(datafile)
+
+    def test_penntreebank(self):
+        from torchtext.datasets import PennTreebank
+        # smoke test to ensure wikitext2 works properly
+        train_dataset, test_dataset, valid_dataset = PennTreebank()
+        self.assertEqual(len(train_dataset), 924412)
+        self.assertEqual(len(test_dataset), 82114)
+        self.assertEqual(len(valid_dataset), 73339)
+
+        vocab = train_dataset.get_vocab()
+        tokens_ids = [vocab[token] for token in 'the player characters rest'.split()]
+        self.assertEqual(tokens_ids, [2, 2550, 3344, 1125])
+
+        # Delete the dataset after we're done to save disk space on CI
+        if os.environ.get("TRAVIS") == "true":
+            datafile = os.path.join(self.project_root, ".data", 'ptb.train.txt')
+            conditional_remove(datafile)
+            datafile = os.path.join(self.project_root, ".data", 'ptb.ttest.txt')
+            conditional_remove(datafile)
+            datafile = os.path.join(self.project_root, ".data", 'ptb.valid.txt')
             conditional_remove(datafile)
 
     def test_text_classification(self):
