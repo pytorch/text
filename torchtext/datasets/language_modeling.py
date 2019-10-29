97af9d0db57a6750586f5958c1abd90cc1659f5d
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-10-29 10:30:55 -0700

    Re-write three datasets.

--- torchtext/datasets/language_modeling.py
+++ torchtext/datasets/language_modeling.py
@@ -1,7 +1,7 @@
 import torch
 import logging
-from .. import data
 import io
+import os
 from torchtext.utils import download_from_url, extract_archive, unicode_csv_reader
 from torchtext.vocab import build_vocab_from_iterator
 from torchtext.data.utils import get_tokenizer
@@ -15,37 +15,35 @@ URLS = {
         'https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip',
     'PennTreebank':
         ['https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.train.txt',
-         'https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.valid.txt',
-         'https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.test.txt']
+         'https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.test.txt',
+         'https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.valid.txt']
 }
 
+
 def _read_text_iterator(data_path, tokenizer):
-    tokenizer = get_tokenizer("basic_english")
     with io.open(data_path, encoding="utf8") as f:
         reader = unicode_csv_reader(f)
         for row in reader:
-            tokens = ' '.join(row[1:])
-            tokens = tokenizer(tokens)
+            tokens = tokenizer(' '.join(row))
             yield tokens
 
 
 def _create_data_from_iterator(vocab, iterator, include_unk):
-    data = []
-    labels = []
+    _data = []
     with tqdm(unit_scale=0, unit='lines') as t:
-        for cls, tokens in iterator:
+        for tokens in iterator:
             if include_unk:
-                tokens = torch.tensor([vocab[token] for token in tokens])
+                tokens = [vocab[token] for token in tokens]
             else:
                 token_ids = list(filter(lambda x: x is not Vocab.UNK, [vocab[token]
                                         for token in tokens]))
-                tokens = torch.tensor(token_ids)
+                tokens = token_ids
             if len(tokens) == 0:
                 logging.info('Row contains no tokens.')
-            data.append((cls, tokens))
-            labels.append(cls)
+            _data += tokens
             t.update(1)
-    return data, set(labels)
+    return torch.Tensor(_data).long()
+
 
 class LanguageModelingDataset(torch.utils.data.Dataset):
     """Defines a dataset for language modeling."""
@@ -73,18 +71,23 @@ class LanguageModelingDataset(torch.utils.data.Dataset):
     def get_vocab(self):
         return self._vocab
 
-def _setup_datasets(dataset_name, tokenizer=get_tokenizer("basic_english"),
-                    root='.data', ngrams=2, vocab=None, include_unk=False):
-    dataset_tar = download_from_url(URLS[dataset_name], root=root)
-    extracted_files = extract_archive(dataset_tar)
 
-    for fname in extracted_files:
-        if 'train' in fname:
-            train_path = fname
-        if 'test' in fname:
-            test_path = fname
-        if 'valid' in fname:
-            valid_path = fname
+def _setup_datasets(dataset_name, tokenizer=get_tokenizer("basic_english"),
+                    root='.data', vocab=None, include_unk=False):
+    if dataset_name == 'PennTreebank':
+        train_path = download_from_url(URLS['PennTreebank'][0], root=root)
+        test_path = download_from_url(URLS['PennTreebank'][1], root=root)
+        valid_path = download_from_url(URLS['PennTreebank'][2], root=root)
+    else:
+        dataset_tar = download_from_url(URLS[dataset_name], root=root)
+        extracted_files = extract_archive(dataset_tar)
+        for fname in extracted_files:
+            if 'train' in fname:
+                train_path = os.path.join(root, fname)
+            if 'test' in fname:
+                test_path = os.path.join(root, fname)
+            if 'valid' in fname:
+                valid_path = os.path.join(root, fname)
 
     if vocab is None:
         logging.info('Building Vocab based on {}'.format(train_path))
@@ -107,188 +110,13 @@ def _setup_datasets(dataset_name, tokenizer=get_tokenizer("basic_english"),
             LanguageModelingDataset(valid_data, vocab))
 
 
-class WikiText2(LanguageModelingDataset):
-
-    urls = ['https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip']
-    name = 'wikitext-2'
-    dirname = 'wikitext-2'
-
-    @classmethod
-    def splits(cls, text_field, root='.data', train='wiki.train.tokens',
-               validation='wiki.valid.tokens', test='wiki.test.tokens',
-               **kwargs):
-        """Create dataset objects for splits of the WikiText-2 dataset.
-
-        This is the most flexible way to use the dataset.
-
-        Arguments:
-            text_field: The field that will be used for text data.
-            root: The root directory that the dataset's zip archive will be
-                expanded into; therefore the directory in whose wikitext-2
-                subdirectory the data files will be stored.
-            train: The filename of the train data. Default: 'wiki.train.tokens'.
-            validation: The filename of the validation data, or None to not
-                load the validation set. Default: 'wiki.valid.tokens'.
-            test: The filename of the test data, or None to not load the test
-                set. Default: 'wiki.test.tokens'.
-        """
-        return super(WikiText2, cls).splits(
-            root=root, train=train, validation=validation, test=test,
-            text_field=text_field, **kwargs)
-
-    @classmethod
-    def iters(cls, batch_size=32, bptt_len=35, device=0, root='.data',
-              vectors=None, **kwargs):
-        """Create iterator objects for splits of the WikiText-2 dataset.
-
-        This is the simplest way to use the dataset, and assumes common
-        defaults for field, vocabulary, and iterator parameters.
-
-        Arguments:
-            batch_size: Batch size.
-            bptt_len: Length of sequences for backpropagation through time.
-            device: Device to create batches on. Use -1 for CPU and None for
-                the currently active GPU device.
-            root: The root directory that the dataset's zip archive will be
-                expanded into; therefore the directory in whose wikitext-2
-                subdirectory the data files will be stored.
-            wv_dir, wv_type, wv_dim: Passed to the Vocab constructor for the
-                text field. The word vectors are accessible as
-                train.dataset.fields['text'].vocab.vectors.
-            Remaining keyword arguments: Passed to the splits method.
-        """
-        TEXT = data.Field()
-
-        train, val, test = cls.splits(TEXT, root=root, **kwargs)
-
-        TEXT.build_vocab(train, vectors=vectors)
-
-        return data.BPTTIterator.splits(
-            (train, val, test), batch_size=batch_size, bptt_len=bptt_len,
-            device=device)
+def WikiText2(*args, **kwargs):
+    return _setup_datasets(*(("WikiText2",) + args), **kwargs)
 
 
-class WikiText103(LanguageModelingDataset):
-
-    urls = ['https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip']
-    name = 'wikitext-103'
-    dirname = 'wikitext-103'
-
-    @classmethod
-    def splits(cls, text_field, root='.data', train='wiki.train.tokens',
-               validation='wiki.valid.tokens', test='wiki.test.tokens',
-               **kwargs):
-        """Create dataset objects for splits of the WikiText-103 dataset.
-
-        This is the most flexible way to use the dataset.
-
-        Arguments:
-            text_field: The field that will be used for text data.
-            root: The root directory that the dataset's zip archive will be
-                expanded into; therefore the directory in whose wikitext-103
-                subdirectory the data files will be stored.
-            train: The filename of the train data. Default: 'wiki.train.tokens'.
-            validation: The filename of the validation data, or None to not
-                load the validation set. Default: 'wiki.valid.tokens'.
-            test: The filename of the test data, or None to not load the test
-                set. Default: 'wiki.test.tokens'.
-        """
-        return super(WikiText103, cls).splits(
-            root=root, train=train, validation=validation, test=test,
-            text_field=text_field, **kwargs)
-
-    @classmethod
-    def iters(cls, batch_size=32, bptt_len=35, device=0, root='.data',
-              vectors=None, **kwargs):
-        """Create iterator objects for splits of the WikiText-103 dataset.
-
-        This is the simplest way to use the dataset, and assumes common
-        defaults for field, vocabulary, and iterator parameters.
-
-        Arguments:
-            batch_size: Batch size.
-            bptt_len: Length of sequences for backpropagation through time.
-            device: Device to create batches on. Use -1 for CPU and None for
-                the currently active GPU device.
-            root: The root directory that the dataset's zip archive will be
-                expanded into; therefore the directory in whose wikitext-2
-                subdirectory the data files will be stored.
-            wv_dir, wv_type, wv_dim: Passed to the Vocab constructor for the
-                text field. The word vectors are accessible as
-                train.dataset.fields['text'].vocab.vectors.
-            Remaining keyword arguments: Passed to the splits method.
-        """
-        TEXT = data.Field()
-
-        train, val, test = cls.splits(TEXT, root=root, **kwargs)
-
-        TEXT.build_vocab(train, vectors=vectors)
-
-        return data.BPTTIterator.splits(
-            (train, val, test), batch_size=batch_size, bptt_len=bptt_len,
-            device=device)
+def WikiText103(*args, **kwargs):
+    return _setup_datasets(*(("WikiText103",) + args), **kwargs)
 
 
-class PennTreebank(LanguageModelingDataset):
-    """The Penn Treebank dataset.
-    A relatively small dataset originally created for POS tagging.
-
-    References
-    ----------
-    Marcus, Mitchell P., Marcinkiewicz, Mary Ann & Santorini, Beatrice (1993).
-    Building a Large Annotated Corpus of English: The Penn Treebank
-    """
-
-    urls = ['https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.train.txt',
-            'https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.valid.txt',
-            'https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.test.txt']
-    name = 'penn-treebank'
-    dirname = ''
-
-    @classmethod
-    def splits(cls, text_field, root='.data', train='ptb.train.txt',
-               validation='ptb.valid.txt', test='ptb.test.txt',
-               **kwargs):
-        """Create dataset objects for splits of the Penn Treebank dataset.
-
-        Arguments:
-            text_field: The field that will be used for text data.
-            root: The root directory where the data files will be stored.
-            train: The filename of the train data. Default: 'ptb.train.txt'.
-            validation: The filename of the validation data, or None to not
-                load the validation set. Default: 'ptb.valid.txt'.
-            test: The filename of the test data, or None to not load the test
-                set. Default: 'ptb.test.txt'.
-        """
-        return super(PennTreebank, cls).splits(
-            root=root, train=train, validation=validation, test=test,
-            text_field=text_field, **kwargs)
-
-    @classmethod
-    def iters(cls, batch_size=32, bptt_len=35, device=0, root='.data',
-              vectors=None, **kwargs):
-        """Create iterator objects for splits of the Penn Treebank dataset.
-
-        This is the simplest way to use the dataset, and assumes common
-        defaults for field, vocabulary, and iterator parameters.
-
-        Arguments:
-            batch_size: Batch size.
-            bptt_len: Length of sequences for backpropagation through time.
-            device: Device to create batches on. Use -1 for CPU and None for
-                the currently active GPU device.
-            root: The root directory where the data files will be stored.
-            wv_dir, wv_type, wv_dim: Passed to the Vocab constructor for the
-                text field. The word vectors are accessible as
-                train.dataset.fields['text'].vocab.vectors.
-            Remaining keyword arguments: Passed to the splits method.
-        """
-        TEXT = data.Field()
-
-        train, val, test = cls.splits(TEXT, root=root, **kwargs)
-
-        TEXT.build_vocab(train, vectors=vectors)
-
-        return data.BPTTIterator.splits(
-            (train, val, test), batch_size=batch_size, bptt_len=bptt_len,
-            device=device)
+def PennTreebank(*args, **kwargs):
+    return _setup_datasets(*(("PennTreebank",) + args), **kwargs)
