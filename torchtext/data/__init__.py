f765e517d6cc456b876715f1cb9c843bec8b14ed
sluks <56857675+sluks@users.noreply.github.com>
2019-10-31 19:07:18 -0400

    Add BLEU score metric (#627)

--- torchtext/data/__init__.py
+++ torchtext/data/__init__.py
@@ -4,6 +4,7 @@ from .example import Example
 from .field import RawField, Field, ReversibleField, SubwordField, NestedField, LabelField
 from .iterator import (batch, BucketIterator, Iterator, BPTTIterator,
                        pool)
+from .metrics import bleu_score
 from .pipeline import Pipeline
 from .utils import get_tokenizer, interleave_keys
 from .functional import generate_sp_model, \
@@ -18,6 +19,7 @@ __all__ = ["Batch",
            "LabelField",
            "batch", "BucketIterator", "Iterator", "BPTTIterator",
            "pool",
+           "bleu_score",
            "Pipeline",
            "get_tokenizer", "interleave_keys",
            "generate_sp_model", "load_sp_model",
