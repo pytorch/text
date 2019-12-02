adbda154e105c71141d1f4f096edbedb45640b3a
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-12-02 12:44:26 -0800

    add prototype to __init__ file

--- torchtext/__init__.py
+++ torchtext/__init__.py
@@ -3,6 +3,7 @@ from . import datasets
 from . import utils
 from . import vocab
 from . import legacy
+from . import prototype
 
 __version__ = '0.4.0'
 
@@ -10,4 +11,4 @@ __all__ = ['data',
            'datasets',
            'utils',
            'vocab',
-           'legacy']
+           'prototype']
