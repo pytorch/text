41f60f7e452bb1f02a67c59d286c0b5e7f972f6b
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-12-04 12:09:45 -0800

    Move language modeling datasets to experimental.

--- torchtext/experimental/datasets/__init__.py
+++ torchtext/experimental/datasets/__init__.py
@@ -1,3 +1,8 @@
+from .language_modeling import LanguageModelingDataset, WikiText2, WikiText103, PennTreebank  # NOQA
 from .text_classification import IMDB
 
-__all__ = ['IMDB']
+__all__ = ['LanguageModelingDataset',
+           'WikiText2',
+           'WikiText103',
+           'PennTreebank',
+           'IMDB']
