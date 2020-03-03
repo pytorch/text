6611a097236684c8283df646845bfa1b3ef34c4b
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-03-03 09:12:55 -0800

    switch transforms to torch.nn.Module

--- torchtext/experimental/transforms.py
+++ torchtext/experimental/transforms.py
@@ -2,33 +2,36 @@ import torch
 from torchtext.data.utils import get_tokenizer
 
 
-class TokenizerTransform(object):
+class TokenizerTransform(torch.nn.Module):
 
     def __init__(self, tokenizer_name):
+        super(TokenizerTransform, self).__init__()
         self.tokenizer = get_tokenizer(tokenizer_name)
 
-    def __call__(self, str_input):
+    def forward(self, str_input):
         return self.tokenizer(str_input)
 
 
-class VocabTransform(object):
+class VocabTransform(torch.nn.Module):
     def __init__(self, vocab):
+        super(VocabTransform, self).__init__()
         self.vocab = vocab
 
-    def __call__(self, tok_iter):
+    def forward(self, tok_iter):
         return [self.vocab[tok] for tok in tok_iter]
 
 
-class ToTensor(object):
+class ToTensor(torch.nn.Module):
     def __init__(self, dtype=torch.long):
+        super(ToTensor, self).__init__()
         self.dtype = dtype
 
-    def __call__(self, ids_list):
+    def forward(self, ids_list):
         return torch.tensor(ids_list).to(self.dtype)
 
 
 # Fork from torchvision
-class Compose(object):
+class Compose(torch.nn.Module):
     """Composes several transforms together.
     Args:
         transforms (list of ``Transform`` objects): list of transforms to compose.
@@ -40,9 +43,10 @@ class Compose(object):
     """
 
     def __init__(self, transforms):
+        super(Compose, self).__init__()
         self.transforms = transforms
 
-    def __call__(self, img):
+    def forward(self, img):
         for t in self.transforms:
             img = t(img)
         return img
@@ -56,9 +60,10 @@ class Compose(object):
         return format_string
 
 
-class ListTransform(object):
+class ListTransform(torch.nn.Module):
     def __init__(self, transforms):
+        super(ListTransform, self).__init__()
         self.transforms = transforms
 
-    def __call__(self, corpus_iter):
+    def forward(self, corpus_iter):
         return [self.transforms(item) for item in corpus_iter]
