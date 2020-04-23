adca988cfd38c5f64516a36ebde011d7c26d6961
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-04-23 08:16:11 -0700

    Add explicit test results to text classification datasets

--- test/data/test_builtin_datasets.py
+++ test/data/test_builtin_datasets.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import torchtext.data as data
 from torchtext.datasets import AG_NEWS
-
+from torch.testing import assert_allclose
 from ..common.test_markers import slow
 from ..common.torchtext_test_case import TorchtextTestCase
 
@@ -98,6 +98,10 @@ class TestDataset(TorchtextTestCase):
         ag_news_train, ag_news_test = AG_NEWS(root=datadir, ngrams=3)
         self.assertEqual(len(ag_news_train), 120000)
         self.assertEqual(len(ag_news_test), 7600)
+        assert_allclose(ag_news_train[-1][1][:10],
+                        torch.tensor([3525, 319, 4053, 34, 5407, 3607, 70, 6798, 10599, 4053]).long())
+        assert_allclose(ag_news_test[-1][1][:10],
+                        torch.tensor(tensor([2351, 758, 96, 38581, 2351, 220, 5, 396, 3, 14786])).long())
 
         # Delete the dataset after we're done to save disk space on CI
         datafile = os.path.join(self.project_root, ".data", "ag_news_csv")
@@ -113,6 +117,14 @@ class TestDataset(TorchtextTestCase):
         train_dataset, test_dataset = IMDB()
         self.assertEqual(len(train_dataset), 25000)
         self.assertEqual(len(test_dataset), 25000)
+        assert_allclose(train_dataset[0][1][:10],
+                        torch.tensor([13, 1568, 13, 246, 35468, 43, 64, 398, 1135, 92]).long())
+        assert_allclose(train_dataset[-1][1][:10],
+                        torch.tensor([2, 71, 4555, 194, 3328, 15144, 42, 227, 148, 8]).long())
+        assert_allclose(test_dataset[0][1][:10],
+                        torch.tensor([13, 125, 1051, 5, 246, 1652, 8, 277, 66, 20]).long())
+        assert_allclose(test_dataset[-1][1][:10],
+                        torch.tensor([13, 1035, 14, 21, 28, 2, 1051, 1275, 1008, 3]).long())
 
         # Test API with a vocab input object
         old_vocab = train_dataset.get_vocab()
