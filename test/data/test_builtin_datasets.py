1359d4b6576b15cec1859bc783742a859e134f20
akurniawan <akurniawan@traveloka.com>
2020-05-02 14:02:13 +0700

    add unit test for all datasets and updating behaviour for conditional_remove for removing multiple files at once

--- test/data/test_builtin_datasets.py
+++ test/data/test_builtin_datasets.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import shutil
 import torchtext.data as data
 from torchtext.datasets import AG_NEWS
@@ -9,10 +10,11 @@ from ..common.torchtext_test_case import TorchtextTestCase
 
 
 def conditional_remove(f):
-    if os.path.isfile(f):
-        os.remove(f)
-    elif os.path.isdir(f):
-        shutil.rmtree(f)
+    for path in glob.glob(f):
+        if os.path.isfile(path):
+            os.remove(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
 
 
 class TestDataset(TorchtextTestCase):
@@ -27,8 +29,7 @@ class TestDataset(TorchtextTestCase):
         train_iter, valid_iter, test_iter = data.BPTTIterator.splits(
             (train, valid, test), batch_size=3, bptt_len=30)
 
-        train_iter, valid_iter, test_iter = ds.iters(batch_size=4,
-                                                     bptt_len=30)
+        train_iter, valid_iter, test_iter = ds.iters(batch_size=4, bptt_len=30)
 
         # Delete the dataset after we're done to save disk space on CI
         datafile = os.path.join(self.project_root, ".data", "wikitext-2")
@@ -43,13 +44,16 @@ class TestDataset(TorchtextTestCase):
         self.assertEqual(len(valid_dataset), 214417)
 
         vocab = train_dataset.get_vocab()
-        tokens_ids = [vocab[token] for token in 'the player characters rest'.split()]
+        tokens_ids = [
+            vocab[token] for token in 'the player characters rest'.split()
+        ]
         self.assertEqual(tokens_ids, [2, 286, 503, 700])
 
         # Delete the dataset after we're done to save disk space on CI
         datafile = os.path.join(self.project_root, ".data", "wikitext-2")
         conditional_remove(datafile)
-        datafile = os.path.join(self.project_root, ".data", "wikitext-2-v1.zip")
+        datafile = os.path.join(self.project_root, ".data",
+                                "wikitext-2-v1.zip")
         conditional_remove(datafile)
 
     @slow
@@ -63,8 +67,7 @@ class TestDataset(TorchtextTestCase):
         train_iter, valid_iter, test_iter = data.BPTTIterator.splits(
             (train, valid, test), batch_size=3, bptt_len=30)
 
-        train_iter, valid_iter, test_iter = ds.iters(batch_size=4,
-                                                     bptt_len=30)
+        train_iter, valid_iter, test_iter = ds.iters(batch_size=4, bptt_len=30)
 
         # Delete the dataset after we're done to save disk space on CI
         datafile = os.path.join(self.project_root, ".data", "penn-treebank")
@@ -79,7 +82,9 @@ class TestDataset(TorchtextTestCase):
         self.assertEqual(len(valid_dataset), 73339)
 
         vocab = train_dataset.get_vocab()
-        tokens_ids = [vocab[token] for token in 'the player characters rest'.split()]
+        tokens_ids = [
+            vocab[token] for token in 'the player characters rest'.split()
+        ]
         self.assertEqual(tokens_ids, [2, 2550, 3344, 1125])
 
         # Delete the dataset after we're done to save disk space on CI
@@ -99,15 +104,21 @@ class TestDataset(TorchtextTestCase):
         ag_news_train, ag_news_test = AG_NEWS(root=datadir, ngrams=3)
         self.assertEqual(len(ag_news_train), 120000)
         self.assertEqual(len(ag_news_test), 7600)
-        assert_allclose(ag_news_train[-1][1][:10],
-                        torch.tensor([3525, 319, 4053, 34, 5407, 3607, 70, 6798, 10599, 4053]).long())
-        assert_allclose(ag_news_test[-1][1][:10],
-                        torch.tensor([2351, 758, 96, 38581, 2351, 220, 5, 396, 3, 14786]).long())
+        assert_allclose(
+            ag_news_train[-1][1][:10],
+            torch.tensor(
+                [3525, 319, 4053, 34, 5407, 3607, 70, 6798, 10599,
+                 4053]).long())
+        assert_allclose(
+            ag_news_test[-1][1][:10],
+            torch.tensor([2351, 758, 96, 38581, 2351, 220, 5, 396, 3,
+                          14786]).long())
 
         # Delete the dataset after we're done to save disk space on CI
         datafile = os.path.join(self.project_root, ".data", "ag_news_csv")
         conditional_remove(datafile)
-        datafile = os.path.join(self.project_root, ".data", "ag_news_csv.tar.gz")
+        datafile = os.path.join(self.project_root, ".data",
+                                "ag_news_csv.tar.gz")
         conditional_remove(datafile)
 
     @slow
@@ -118,14 +129,21 @@ class TestDataset(TorchtextTestCase):
         train_dataset, test_dataset = IMDB()
         self.assertEqual(len(train_dataset), 25000)
         self.assertEqual(len(test_dataset), 25000)
-        assert_allclose(train_dataset[0][1][:10],
-                        torch.tensor([13, 1568, 13, 246, 35468, 43, 64, 398, 1135, 92]).long())
-        assert_allclose(train_dataset[-1][1][:10],
-                        torch.tensor([2, 71, 4555, 194, 3328, 15144, 42, 227, 148, 8]).long())
-        assert_allclose(test_dataset[0][1][:10],
-                        torch.tensor([13, 125, 1051, 5, 246, 1652, 8, 277, 66, 20]).long())
-        assert_allclose(test_dataset[-1][1][:10],
-                        torch.tensor([13, 1035, 14, 21, 28, 2, 1051, 1275, 1008, 3]).long())
+        assert_allclose(
+            train_dataset[0][1][:10],
+            torch.tensor([13, 1568, 13, 246, 35468, 43, 64, 398, 1135,
+                          92]).long())
+        assert_allclose(
+            train_dataset[-1][1][:10],
+            torch.tensor([2, 71, 4555, 194, 3328, 15144, 42, 227, 148,
+                          8]).long())
+        assert_allclose(
+            test_dataset[0][1][:10],
+            torch.tensor([13, 125, 1051, 5, 246, 1652, 8, 277, 66, 20]).long())
+        assert_allclose(
+            test_dataset[-1][1][:10],
+            torch.tensor([13, 1035, 14, 21, 28, 2, 1051, 1275, 1008,
+                          3]).long())
 
         # Test API with a vocab input object
         old_vocab = train_dataset.get_vocab()
@@ -137,5 +155,106 @@ class TestDataset(TorchtextTestCase):
         conditional_remove(datafile)
         datafile = os.path.join(self.project_root, ".data", "aclImdb")
         conditional_remove(datafile)
-        datafile = os.path.join(self.project_root, ".data", "aclImdb_v1.tar.gz")
+        datafile = os.path.join(self.project_root, ".data",
+                                "aclImdb_v1.tar.gz")
+        conditional_remove(datafile)
+
+    def test_multi30k(self):
+        from torchtext.experimental.datasets.translation import Multi30k
+        # smoke test to ensure multi30k works properly
+        train_dataset, test_dataset, valid_dataset = Multi30k()
+        self.assertEqual(len(train_dataset), 29001)
+        self.assertEqual(len(test_dataset), 1015)
+        self.assertEqual(len(valid_dataset), 1000)
+
+        de_vocab, en_vocab = train_dataset.get_vocab()
+        de_tokens_ids = [
+            de_vocab[token] for token in
+            'Zwei Männer verpacken Donuts in Kunststofffolie'.split()
+        ]
+        self.assertEqual(de_tokens_ids, [19, 29, 18703, 4448, 5, 6240])
+
+        en_tokens_ids = [
+            en_vocab[token] for token in
+            'Two young White males are outside near many bushes'.split()
+        ]
+        self.assertEqual(en_tokens_ids,
+                         [17, 23, 1167, 806, 15, 55, 82, 334, 1337])
+
+        datafile = os.path.join(self.project_root, ".data", "train*")
+        conditional_remove(datafile)
+        datafile = os.path.join(self.project_root, ".data", "val*")
+        conditional_remove(datafile)
+        datafile = os.path.join(self.project_root, ".data", "test*")
+        conditional_remove(datafile)
+        datafile = os.path.join(self.project_root, ".data",
+                                "mmt_task1_test2016.tar.gz")
+        conditional_remove(datafile)
+
+    @slow
+    def test_wmt14(self):
+        from torchtext.experimental.datasets.translation import WMT14
+        # smoke test to ensure wmt14 works properly
+        train_dataset, test_dataset, valid_dataset = WMT14()
+        self.assertEqual(len(train_dataset), 4500966)
+        self.assertEqual(len(test_dataset), 3000)
+        self.assertEqual(len(valid_dataset), 3003)
+
+        de_vocab, en_vocab = train_dataset.get_vocab()
+        de_tokens_ids = [
+            de_vocab[token] for token in
+            '( Das Parlament erhebt sich zu einer Schwei@@ ge@@ minute . )'.
+            split()
+        ]
+        self.assertEqual(
+            de_tokens_ids,
+            [36, 52, 131, 13450, 28, 8, 46, 5281, 146, 16143, 3, 37])
+
+        en_tokens_ids = [
+            en_vocab[token] for token in
+            'I would like your advice about Rule 14@@ 3 concerning in@@ ad@@ mis@@ sibility .'
+            .split()
+        ]
+        self.assertEqual(en_tokens_ids,
+                         [17, 23, 1167, 806, 15, 55, 82, 334, 1337])
+
+        datafile = os.path.join(self.project_root, ".data", "newstest*")
+        conditional_remove(datafile)
+        datafile = os.path.join(self.project_root, ".data", "bpe.32000")
+        conditional_remove(datafile)
+        datafile = os.path.join(self.project_root, ".data", "vocab.bpe.32000")
+        conditional_remove(datafile)
+        datafile = os.path.join(self.project_root, ".data", "train.tok.clean*")
+        conditional_remove(datafile)
+        datafile = os.path.join(self.project_root, ".data",
+                                "wmt16_en_de.tar.gz")
+        conditional_remove(datafile)
+
+    def test_iwslt(self):
+        from torchtext.experimental.datasets.translation import IWSLT
+        # smoke test to ensure IWSLT works properly
+        train_dataset, test_dataset, valid_dataset = IWSLT()
+        self.assertEqual(len(train_dataset), 173939)
+        self.assertEqual(len(test_dataset), 823)
+        self.assertEqual(len(valid_dataset), 1096)
+
+        de_vocab, en_vocab = train_dataset.get_vocab()
+        de_tokens_ids = [
+            de_vocab[token] for token in
+            'Es hat diese Fischköder an der Unterseite. Die bewegen sich auf und ab'
+            .split()
+        ]
+        self.assertEqual(
+            de_tokens_ids,
+            [40, 53, 42, 33887, 35, 6, 0, 64, 529, 27, 26, 5, 337])
+
+        en_tokens_ids = [
+            en_vocab[token]
+            for token in 'The average depth is about two miles'.split()
+        ]
+        self.assertEqual(en_tokens_ids, [63, 717, 3480, 12, 36, 118, 736])
+
+        datafile = os.path.join(self.project_root, ".data", "de-en")
+        conditional_remove(datafile)
+        datafile = os.path.join(self.project_root, ".data", "de-en.tgz")
         conditional_remove(datafile)
