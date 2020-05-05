6c097a4ecd7138a61eb87584973dbc89c108af64
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-05-05 13:04:51 -0700

    add dataloader to mlm_task

--- examples/BERT/mlm_task.py
+++ examples/BERT/mlm_task.py
@@ -7,41 +7,31 @@ from model import MLMTask
 from utils import setup, cleanup, run_demo, print_loss_log
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
-import os
+from torch.utils.data import DataLoader
 
 
-def batchify(txt_data, bsz, args):
+def collate_batch(batch_data, args, mask_id, cls_id):
+    batch_data = torch.tensor(batch_data).long().view(args.batch_size, -1).t().contiguous()
+    # Generate masks with args.mask_frac
+    data_len = batch_data.size(0)
+    ones_num = int(data_len * args.mask_frac)
+    zeros_num = data_len - ones_num
+    lm_mask = torch.cat([torch.zeros(zeros_num), torch.ones(ones_num)])
+    lm_mask = lm_mask[torch.randperm(data_len)]
+    # Add <'cls'> token id to the beginning of seq across batches
+    batch_data = torch.cat((torch.tensor([[cls_id] * batch_data.size(1)]).long(), batch_data))
+    lm_mask = torch.cat((torch.tensor([0.0]), lm_mask))
 
+    targets = torch.stack([batch_data[i] for i in range(lm_mask.size(0)) if lm_mask[i]]).view(-1)
+    batch_data = batch_data.masked_fill(lm_mask.bool().unsqueeze(1), mask_id)
+    return batch_data, lm_mask, targets
+
+
+def process_raw_data(raw_data, args):
     # Cut the data to bptt and bsz
-    _num = len(txt_data) // (bsz * args.bptt)
-    txt_data = txt_data[:(_num * bsz * args.bptt)]
-    # Divide the dataset into bsz parts.
-    nbatch = txt_data.size(0) // bsz
-    # Trim off any extra elements that wouldn't cleanly fit (remainders).
-    txt_data = txt_data.narrow(0, 0, nbatch * bsz)
-    # Evenly divide the data across the bsz batches.
-    txt_data = txt_data.view(bsz, -1).t().contiguous()
-    return txt_data
-
-
-###############################################################################
-# Training code
-###############################################################################
-
-# get_batch subdivides the source data into chunks of length args.bptt.
-# If source is equal to the example output of the batchify function, with
-# a bptt-limit of 2, we'd get the following two Variables for i = 0:
-# ┌ a g m s ┐ ┌ b h n t ┐
-# └ b h n t ┘ └ c i o u ┘
-# Note that despite the name of the function, the subdivison of data is not
-# done along the batch dimension (i.e. dimension 1), since that was handled
-# by the batchify function. The chunks are along dimension 0, corresponding
-# to the seq_len dimension in the LSTM.
-
-def get_batch(source, i, args):
-    seq_len = min(args.bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len]
-    return data
+    _num = raw_data.size(0) // (args.batch_size * args.bptt)
+    raw_data = raw_data[:(_num * args.batch_size * args.bptt)]
+    return raw_data
 
 
 def evaluate(data_source, model, vocab, ntokens, criterion, args, device):
@@ -50,32 +40,16 @@ def evaluate(data_source, model, vocab, ntokens, criterion, args, device):
     total_loss = 0.
     mask_id = vocab.stoi['<MASK>']
     cls_id = vocab.stoi['<cls>']
+    dataloader = DataLoader(data_source, batch_size=args.batch_size * args.bptt,
+                            shuffle=False, collate_fn=lambda b: collate_batch(b, args, mask_id, cls_id))
     with torch.no_grad():
-        for i in range(0, data_source.size(0) - 1, args.bptt):
-
-            if i + args.bptt * args.world_size > len(data_source):
-                continue
-            data = get_batch(data_source, i, args)
-            # Generate masks with args.mask_frac
-            data_len = data.size(0)
-            ones_num = int(data_len * args.mask_frac)
-            zeros_num = data_len - ones_num
-            lm_mask = torch.cat([torch.zeros(zeros_num), torch.ones(ones_num)])
-            lm_mask = lm_mask[torch.randperm(data_len)]
-
-            # Add <'cls'> token id to the beginning of seq across batches
-            data = torch.cat((torch.tensor([[cls_id] * data.size(1)]).long(), data))
-            lm_mask = torch.cat((torch.tensor([0.0]), lm_mask))
-
-            targets = torch.stack([data[i] for i in range(lm_mask.size(0)) if lm_mask[i]]).view(-1)
-            data = data.masked_fill(lm_mask.bool().unsqueeze(1), mask_id)
-
+        for batch, (data, lm_mask, targets) in enumerate(dataloader):
             data = data.transpose(0, 1)  # Wrap up by nn.DataParallel
             output = model(data)
             output = torch.stack([output[i] for i in range(lm_mask.size(0)) if lm_mask[i]])
             output_flat = output.view(-1, ntokens)
             total_loss += criterion(output_flat, targets.to(device[0])).item()
-    return total_loss / ((len(data_source) - 1) / args.bptt)
+    return total_loss / ((len(data_source) - 1) / args.bptt / args.batch_size)
 
 
 def train(model, vocab, train_loss_log, train_data,
@@ -87,48 +61,31 @@ def train(model, vocab, train_loss_log, train_data,
     mask_id = vocab.stoi['<MASK>']
     cls_id = vocab.stoi['<cls>']
     train_loss_log.append(0.0)
-    for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
+    dataloader = DataLoader(train_data, batch_size=args.batch_size * args.bptt,
+                            shuffle=False, collate_fn=lambda b: collate_batch(b, args, mask_id, cls_id))
 
-        data = get_batch(train_data, i, args)
-
-        # Generate masks with args.mask_frac
-        data_len = data.size(0)
-        ones_num = int(data_len * args.mask_frac)
-        zeros_num = data_len - ones_num
-        lm_mask = torch.cat([torch.zeros(zeros_num), torch.ones(ones_num)])
-        lm_mask = lm_mask[torch.randperm(data_len)]
-
-        # Add <'cls'> token id to the beginning of seq across batches
-        data = torch.cat((torch.tensor([[cls_id] * data.size(1)]).long(), data))
-        lm_mask = torch.cat((torch.tensor([0.0]), lm_mask))
-
-        targets = torch.stack([data[i] for i in range(lm_mask.size(0)) if lm_mask[i]]).view(-1)
-        data = data.masked_fill(lm_mask.bool().unsqueeze(1), mask_id)
-
-        # Starting each batch, we detach the hidden state from how it was previously produced.
-        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+    for batch, (data, lm_mask, targets) in enumerate(dataloader):
         optimizer.zero_grad()
         data = data.transpose(0, 1)  # Wrap up by nn.DataParallel
         output = model(data)
         output = torch.stack([output[i] for i in range(lm_mask.size(0)) if lm_mask[i]])
         loss = criterion(output.view(-1, ntokens), targets.to(device[0]))
         loss.backward()
-
         # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
         torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
-
         optimizer.step()
         total_loss += loss.item()
-
         if batch % args.log_interval == 0 and batch > 0:
             cur_loss = total_loss / args.log_interval
             elapsed = time.time() - start_time
             if (rank is None) or rank == 0:
                 train_loss_log[-1] = cur_loss
                 print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:05.5f} | ms/batch {:5.2f} | '
-                      'loss {:5.2f} | ppl {:8.2f}'.format(
-                      epoch, batch, len(train_data) // args.bptt, scheduler.get_last_lr()[0],
-                      elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+                      'loss {:5.2f} | ppl {:8.2f}'.format(epoch, batch,
+                                                          len(train_data) // (args.bptt * args.batch_size),
+                                                          scheduler.get_last_lr()[0],
+                                                          elapsed * 1000 / args.log_interval,
+                                                          cur_loss, math.exp(cur_loss)))
             total_loss = 0
             start_time = time.time()
 
@@ -178,7 +135,8 @@ def run_main(args, rank=None):
     if args.dataset == 'WikiText103' or args.dataset == 'WikiText2':
         train_dataset, test_dataset, valid_dataset = WLMDataset(vocab=vocab)
     elif args.dataset == 'WMTNewsCrawl':
-        test_dataset, valid_dataset = torchtext.experimental.datasets.WikiText2(vocab=vocab, data_select=('test', 'valid'))
+        from torchtext.experimental.datasets import WikiText2
+        test_dataset, valid_dataset = WikiText2(vocab=vocab, data_select=('test', 'valid'))
         train_dataset, = WLMDataset(vocab=vocab, data_select='train')
     elif args.dataset == 'EnWik9':
         enwik9 = EnWik9()
@@ -196,15 +154,15 @@ def run_main(args, rank=None):
     elif args.dataset == 'BookCorpus':
         train_dataset, test_dataset, valid_dataset = BookCorpus(vocab)
 
-    train_data = batchify(train_dataset.data, args.batch_size, args)
+    train_data = process_raw_data(train_dataset.data, args)
 
     if rank is not None:
         # Chunk training data by rank for different gpus
         chunk_len = len(train_data) // args.world_size
-        train_data = train_data[(rank*chunk_len):((rank + 1)*chunk_len)]
+        train_data = train_data[(rank * chunk_len):((rank + 1) * chunk_len)]
 
-    val_data = batchify(valid_dataset.data, args.batch_size, args)
-    test_data = batchify(test_dataset.data, args.batch_size, args)
+    val_data = process_raw_data(valid_dataset.data, args)
+    test_data = process_raw_data(test_dataset.data, args)
 
     ###############################################################################
     # Build the model
@@ -252,7 +210,7 @@ def run_main(args, rank=None):
                 with open(args.save, 'wb') as f:
                     torch.save(model, f)
             elif rank == 0:
-                with open(os.environ['SLURM_JOB_ID'] + '_' + args.save, 'wb') as f:
+                with open(args.save, 'wb') as f:
                     torch.save(model.state_dict(), f)
             best_val_loss = val_loss
         else:
@@ -268,84 +226,75 @@ def run_main(args, rank=None):
         device_pairs = zip(rank0_devices, device)
         map_location = {'cuda:%d' % x: 'cuda:%d' % y for x, y in device_pairs}
         model.load_state_dict(
-            torch.load(os.environ['SLURM_JOB_ID'] + '_' + args.save, map_location=map_location))
+            torch.load(args.save, map_location=map_location))
 
         ###############################################################################
         # Run on test data.
         ###############################################################################
         test_loss = evaluate(test_data, model, train_dataset.vocab, ntokens, criterion, args, device)
         if rank == 0:
-            print('=' * 89)
-            print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
-                  test_loss, math.exp(test_loss)))
-            print('=' * 89)
-            print_loss_log(os.environ['SLURM_JOB_ID'] + '_mlm_loss.txt', train_loss_log, val_loss_log, test_loss, args)
-
-            ###############################################################################
-            # Save the bert model layer
-            ###############################################################################
-            with open(os.environ['SLURM_JOB_ID'] + '_' + args.save, 'wb') as f:
-                torch.save(model.module.bert_model, f)
-            with open(os.environ['SLURM_JOB_ID'] + '_mlm_model.pt', 'wb') as f:
-                torch.save(model.module, f)
+            wrap_up(train_loss_log, val_loss_log, test_loss, args, model)
     else:
         with open(args.save, 'rb') as f:
             model = torch.load(f)
         test_loss = evaluate(test_data, model, train_dataset.vocab, ntokens, criterion, args, device)
-        print('=' * 89)
-        print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
-              test_loss, math.exp(test_loss)))
-        print('=' * 89)
-        print_loss_log('mlm_loss.txt', train_loss_log, val_loss_log, test_loss, args)
+        wrap_up(train_loss_log, val_loss_log, test_loss, args, model)
 
-        ###############################################################################
-        # Save the bert model layer
-        ###############################################################################
-        with open(args.save, 'wb') as f:
-            torch.save(model.module.bert_model, f)
-        with open('mlm_model.pt', 'wb') as f:
-            torch.save(model.module, f)
+
+def wrap_up(train_loss_log, val_loss_log, test_loss, args, model):
+    print('=' * 89)
+    print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+          test_loss, math.exp(test_loss)))
+    print('=' * 89)
+    print_loss_log('mlm_loss.txt', train_loss_log, val_loss_log, test_loss, args)
+    ###############################################################################
+    # Save the bert model layer
+    ###############################################################################
+    with open(args.save, 'wb') as f:
+        torch.save(model.module.bert_model, f)
+    with open('mlm_model.pt', 'wb') as f:
+        torch.save(model.module, f)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 Transformer Language Model')
-    parser.add_argument('--emsize', type=int, default=32,
+    parser.add_argument('--emsize', type=int, default=768,
                         help='size of word embeddings')
-    parser.add_argument('--nhid', type=int, default=64,
+    parser.add_argument('--nhid', type=int, default=3072,
                         help='number of hidden units per layer')
-    parser.add_argument('--nlayers', type=int, default=4,
+    parser.add_argument('--nlayers', type=int, default=12,
                         help='number of layers')
-    parser.add_argument('--nhead', type=int, default=8,
+    parser.add_argument('--nhead', type=int, default=12,
                         help='the number of heads in the encoder/decoder of the transformer model')
-    parser.add_argument('--lr', type=float, default=5,
+    parser.add_argument('--lr', type=float, default=6,
                         help='initial learning rate')
-    parser.add_argument('--clip', type=float, default=0.25,
+    parser.add_argument('--clip', type=float, default=0.1,
                         help='gradient clipping')
-    parser.add_argument('--epochs', type=int, default=2,
+    parser.add_argument('--epochs', type=int, default=10,
                         help='upper epoch limit')
-    parser.add_argument('--batch_size', type=int, default=16, metavar='N',
+    parser.add_argument('--batch_size', type=int, default=32, metavar='N',
                         help='batch size')
-    parser.add_argument('--bptt', type=int, default=35,
+    parser.add_argument('--bptt', type=int, default=128,
                         help='sequence length')
     parser.add_argument('--dropout', type=float, default=0.2,
                         help='dropout applied to layers (0 = no dropout)')
-    parser.add_argument('--seed', type=int, default=1111,
+    parser.add_argument('--seed', type=int, default=5431916812,
                         help='random seed')
-    parser.add_argument('--log-interval', type=int, default=1000, metavar='N',
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='report interval')
     parser.add_argument('--checkpoint', type=str, default='None',
                         help='path to load the checkpoint')
     parser.add_argument('--save', type=str, default='bert_model.pt',
                         help='path to save the final model')
-    parser.add_argument('--save-vocab', type=str, default='vocab.pt',
+    parser.add_argument('--save-vocab', type=str, default='squad_30k_vocab_cls_sep.pt',
                         help='path to save the vocab')
     parser.add_argument('--mask_frac', type=float, default=0.15,
                         help='the fraction of masked tokens')
     parser.add_argument('--dataset', type=str, default='WikiText2',
                         help='dataset used for MLM task')
-    parser.add_argument('--parallel', type=str, default='None',
+    parser.add_argument('--parallel', type=str, default='DDP',
                         help='Use DataParallel to train model')
-    parser.add_argument('--world_size', type=int, default=1,
+    parser.add_argument('--world_size', type=int, default=8,
                         help='the world size to initiate DPP')
     args = parser.parse_args()
 
