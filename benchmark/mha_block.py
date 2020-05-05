f02073f7b5b20883b86e66beeaf60b68011b4ba5
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-05-05 13:20:54 -0700

    add self-attention in the benchmark

--- benchmark/mha_block.py
+++ benchmark/mha_block.py
@@ -6,7 +6,7 @@ import time
 
 def benchmark_mha_block():
 
-    def _run_benchmark(embed_dim, nhead, tgt_len, src_len, bsz, device):
+    def _run_benchmark(embed_dim, nhead, bsz, device, tgt_len, src_len=None):
         # Build torchtext MultiheadAttention module
         in_proj_container = InProjContainer(torch.nn.Linear(embed_dim, embed_dim),
                                             torch.nn.Linear(embed_dim, embed_dim),
@@ -16,7 +16,11 @@ def benchmark_mha_block():
                                           torch.nn.Linear(embed_dim, embed_dim)).to(device)
 
         query = torch.rand((tgt_len, bsz, embed_dim)).to(device)
-        key = value = torch.rand((src_len, bsz, embed_dim)).to(device)
+        if src_len is None:
+            key = value = query
+            src_len = tgt_len
+        else:
+            key = value = torch.rand((src_len, bsz, embed_dim)).to(device)
         attn_mask_2D = torch.randint(0, 2, (tgt_len, src_len)).to(torch.bool).to(device)
         bias_k = bias_v = torch.rand((1, 1, embed_dim)).to(device)
         print("starting torchtext.modules.MultiheadAttentionContainer")
@@ -48,19 +52,23 @@ def benchmark_mha_block():
 
     print("*" * 80)
     print("test case GPU with embed_dim, nhead, tgt_len, src_len, bsz:", 768, 12, 128, 128, 72)
-    _run_benchmark(768, 12, 128, 128, 72, torch.device("cuda"))
+    _run_benchmark(768, 12, 72, torch.device("cuda"), 128, 128)
+
+    print("*" * 80)
+    print("self-attention test case GPU with embed_dim, nhead, tgt_len, src_len, bsz:", 256, 8, 1000, 1000, 2)
+    _run_benchmark(256, 8, 2, torch.device("cuda"), 1000)
 
     print("*" * 80)
     print("test case GPU with embed_dim, nhead, tgt_len, src_len, bsz:", 64, 2, 10, 10, 8)
-    _run_benchmark(64, 2, 10, 10, 8, torch.device("cuda"))
+    _run_benchmark(64, 2, 8, torch.device("cuda"), 10, 10)
 
     print("*" * 80)
     print("test case CPU with embed_dim, nhead, tgt_len, src_len, bsz:", 768, 12, 128, 128, 72)
-    _run_benchmark(768, 12, 128, 128, 72, torch.device("cpu"))
+    _run_benchmark(768, 12, 72, torch.device("cpu"), 128, 128)
 
     print("*" * 80)
     print("test case CPU with embed_dim, nhead, tgt_len, src_len, bsz:", 64, 2, 10, 10, 8)
-    _run_benchmark(64, 2, 10, 10, 8, torch.device("cpu"))
+    _run_benchmark(64, 2, 8, torch.device("cpu"), 10, 10)
 
 
 if __name__ == "__main__":
