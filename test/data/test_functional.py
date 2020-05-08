8ee4753c4db6c45719b8c6e699b0134547c21b89
moto <855818+mthrok@users.noreply.github.com>
2020-05-08 16:50:07 -0400

    [BC breaking] Add Sentencepiece torchscript Extension (#755)
    This CC adds `torchscript` extension `_torchtext.so`, which contains simple interface to `SentencePiece`.
    
     - SentencePiece `v0.1.86` is used.
     - `libsentencepiece.a` is built right before `_torchtext.so` is compiled.
       The logic for triggering this build from `setuptools` can be found under `build_tools/setup_helpers`.
     - `_torchtext.so` provides interface to train a SentencePiece model and load a model from file.
    
    Breaking change:
    
    Previously `torchtext.data.functional.load_sp_model` returned `sentencepiece.SentencePieceProcessor` object, which supported the following methods, in addition to `__len__` and `__getitem__`
    
    ```
    $ grep '$self->' third_party/sentencepiece/python/sentencepiece.i
        return $self->Load(filename);
        return $self->LoadFromSerializedProto(filename);
        return $self->SetEncodeExtraOptions(extra_option);
        return $self->SetDecodeExtraOptions(extra_option);
        return $self->SetVocabulary(valid_vocab);
        return $self->ResetVocabulary();
        return $self->LoadVocabulary(filename, threshold);
        return $self->EncodeAsPieces(input);
        return $self->EncodeAsIds(input);
        return $self->NBestEncodeAsPieces(input, nbest_size);
        return $self->NBestEncodeAsIds(input, nbest_size);
        return $self->SampleEncodeAsPieces(input, nbest_size, alpha);
        return $self->SampleEncodeAsIds(input, nbest_size, alpha);
        return $self->DecodePieces(input);
        return $self->DecodeIds(input);
        return $self->EncodeAsSerializedProto(input);
        return $self->SampleEncodeAsSerializedProto(input, nbest_size, alpha);
        return $self->NBestEncodeAsSerializedProto(input, nbest_size);
        return $self->DecodePiecesAsSerializedProto(pieces);
        return $self->DecodeIdsAsSerializedProto(ids);
        return $self->GetPieceSize();
        return $self->PieceToId(piece);
        return $self->IdToPiece(id);
        return $self->GetScore(id);
        return $self->IsUnused(id);
        return $self->IsControl(id);
        return $self->IsUnused(id);
        return $self->GetPieceSize();
        return $self->PieceToId(key);
    ```
    
    The new C++ Extension provides the following methods
    
    ```
    Encode(input)
    EncodeAsIds(input)
    EncodeAsPieces(input)
    ```

--- test/data/test_functional.py
+++ test/data/test_functional.py
@@ -1,9 +1,19 @@
-from ..common.torchtext_test_case import TorchtextTestCase
-import sentencepiece as spm
-from torchtext.data.functional import generate_sp_model, load_sp_model, \
-    sentencepiece_numericalizer, sentencepiece_tokenizer, \
-    custom_replace, simple_space_split
 import os
+import unittest
+import tempfile
+
+import sentencepiece as spm
+import torch
+from torchtext.data.functional import (
+    generate_sp_model,
+    load_sp_model,
+    sentencepiece_numericalizer,
+    sentencepiece_tokenizer,
+    custom_replace,
+    simple_space_split,
+)
+
+from ..common.torchtext_test_case import TorchtextTestCase
 
 
 class TestFunctional(TorchtextTestCase):
@@ -29,7 +39,7 @@ class TestFunctional(TorchtextTestCase):
         test_sample = 'SentencePiece is an unsupervised text tokenizer and detokenizer'
         model_path = 'test/asset/spm_example.model'
         sp_model = load_sp_model(model_path)
-        self.assertEqual(len(sp_model), 20000)
+        self.assertEqual(sp_model.GetPieceSize(), 20000)
         spm_generator = sentencepiece_numericalizer(sp_model)
 
         ref_results = [15340, 4286, 981, 1207, 1681, 17, 84, 684, 8896, 5366,
@@ -43,7 +53,7 @@ class TestFunctional(TorchtextTestCase):
         test_sample = 'SentencePiece is an unsupervised text tokenizer and detokenizer'
         model_path = 'test/asset/spm_example.model'
         sp_model = load_sp_model(model_path)
-        self.assertEqual(len(sp_model), 20000)
+        self.assertEqual(sp_model.GetPieceSize(), 20000)
         spm_generator = sentencepiece_tokenizer(sp_model)
 
         ref_results = ['\u2581Sent', 'ence', 'P', 'ie', 'ce', '\u2581is',
@@ -66,3 +76,59 @@ class TestFunctional(TorchtextTestCase):
         ref_results = ['test', 'simple', 'space', 'split', 'function']
         self.assertEqual(list(simple_space_split(test_sample))[0],
                          ref_results)
+
+
+class ScriptableSP(torch.jit.ScriptModule):
+    def __init__(self, model_path):
+        super().__init__()
+        self.spm = load_sp_model(model_path)
+
+    @torch.jit.script_method
+    def encode(self, input: str):
+        return self.spm.Encode(input)
+
+    @torch.jit.script_method
+    def encode_as_ids(self, input: str):
+        return self.spm.EncodeAsIds(input)
+
+    @torch.jit.script_method
+    def encode_as_pieces(self, input: str):
+        return self.spm.EncodeAsPieces(input)
+
+
+class TestScriptableSP(unittest.TestCase):
+    def setUp(self):
+        model_path = 'test/asset/spm_example.model'
+        with tempfile.NamedTemporaryFile() as file:
+            torch.jit.script(ScriptableSP(model_path)).save(file.name)
+            self.model = torch.jit.load(file.name)
+
+    def test_encode(self):
+        input = 'SentencePiece is an unsupervised text tokenizer and detokenizer'
+        expected = [
+            '▁Sent', 'ence', 'P', 'ie', 'ce', '▁is',
+            '▁an', '▁un', 'super', 'vis', 'ed', '▁text',
+            '▁to', 'ken', 'izer', '▁and',
+            '▁de', 'to', 'ken', 'izer',
+        ]
+        output = self.model.encode(input)
+        self.assertEqual(expected, output)
+
+    def test_encode_as_ids(self):
+        input = 'SentencePiece is an unsupervised text tokenizer and detokenizer'
+        expected = [
+            15340, 4286, 981, 1207, 1681, 17, 84, 684, 8896, 5366,
+            144, 3689, 9, 5602, 12114, 6, 560, 649, 5602, 12114]
+        output = self.model.encode_as_ids(input)
+        self.assertEqual(expected, output)
+
+    def test_encode_as_pieces(self):
+        input = 'SentencePiece is an unsupervised text tokenizer and detokenizer'
+        expected = [
+            '\u2581Sent', 'ence', 'P', 'ie', 'ce', '\u2581is',
+            '\u2581an', '\u2581un', 'super', 'vis', 'ed', '\u2581text',
+            '\u2581to', 'ken', 'izer', '\u2581and',
+            '\u2581de', 'to', 'ken', 'izer',
+        ]
+        output = self.model.encode_as_pieces(input)
+        self.assertEqual(expected, output)
