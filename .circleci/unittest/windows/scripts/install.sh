a0e5b44037568080430e7be96890a52635119992
peter <peter_jiachen@163.com>
2020-05-14 14:52:51 +0800

    Activate vc environment

--- .circleci/unittest/windows/scripts/install.sh
+++ .circleci/unittest/windows/scripts/install.sh
@@ -7,6 +7,11 @@ unset PYTORCH_VERSION
 
 set -e
 
+this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+root_dir="$(git rev-parse --show-toplevel)"
+
+cd "${root_dir}"
+
 eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
 conda activate ./env
 
@@ -15,4 +20,4 @@ conda install -y -c pytorch-nightly pytorch cpuonly
 
 printf "* Installing torchtext\n"
 git submodule update --init --recursive
-python setup.py develop
+"$this_dir/install.bat"
