8a0213ae4bd4b55d4dd9ef3b8a51be929ad481a3
moto <855818+mthrok@users.noreply.github.com>
2020-05-19 19:00:11 +0000

    move test_vocab_download_custom_vectors (test.test_vocab.TestVocab)

--- test/test_build.py
+++ test/test_build.py
@@ -161,3 +161,31 @@ class TestVocab(TorchtextTestCase):
 
             torch.testing.assert_allclose(vectors[v.stoi['<unk>']], np.zeros(100))
             torch.testing.assert_allclose(vectors[v.stoi['OOV token']], np.zeros(100))
+
+    def test_vocab_download_custom_vectors(self):
+        c = Counter({'hello': 4, 'world': 3, 'ᑌᑎIᑕOᗪᕮ_Tᕮ᙭T': 5, 'freq_too_low': 2})
+        # Build a vocab and get vectors twice to test caching.
+        for _ in range(2):
+            v = torchtext.vocab.Vocab(
+                c, min_freq=3, specials=['<unk>', '<pad>', '<bos>'],
+                vectors=torchtext.vocab.Vectors(
+                    'wiki.simple.vec',
+                    url=torchtext.vocab.FastText.url_base.format('simple')
+                )
+            )
+
+            self.assertEqual(v.itos, ['<unk>', '<pad>', '<bos>',
+                                      'ᑌᑎIᑕOᗪᕮ_Tᕮ᙭T', 'hello', 'world'])
+            vectors = v.vectors.numpy()
+
+            # The first 5 entries in each vector.
+            expected_fasttext_simple_en = {
+                'hello': [0.39567, 0.21454, -0.035389, -0.24299, -0.095645],
+                'world': [0.10444, -0.10858, 0.27212, 0.13299, -0.33165],
+            }
+
+            for word in expected_fasttext_simple_en:
+                torch.testing.assert_allclose(
+                    vectors[v.stoi[word], :5], expected_fasttext_simple_en[word])
+
+            torch.testing.assert_allclose(vectors[v.stoi['<unk>']], np.zeros(300))
