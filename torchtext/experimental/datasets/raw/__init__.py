d9e03245323c453e72fd7e56b6a09417c4cba1af
akurniawan <akurniawan@traveloka.com>
2020-05-20 11:59:51 +0700

    add translation dataset to __init__

--- torchtext/experimental/datasets/raw/__init__.py
+++ torchtext/experimental/datasets/raw/__init__.py
@@ -1,6 +1,7 @@
 from .text_classification import AG_NEWS, SogouNews, DBpedia, YelpReviewPolarity, \
     YelpReviewFull, YahooAnswers, \
     AmazonReviewPolarity, AmazonReviewFull, IMDB
+from .translation import Multi30k, IWSLT, WMT14
 
 __all__ = ['IMDB',
            'AG_NEWS',
@@ -10,4 +11,7 @@ __all__ = ['IMDB',
            'YelpReviewFull',
            'YahooAnswers',
            'AmazonReviewPolarity',
-           'AmazonReviewFull']
+           'AmazonReviewFull',
+           'Multi30k',
+           'IWSLT',
+           'WMT14']
