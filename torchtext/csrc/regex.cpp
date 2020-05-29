5b295cff5ec27d151916e0d4b31ae27fc911aa92
Nayef Ahmed <nayef211@devfair0197.h2.fair>
2020-05-29 15:05:35 -0700

    Removed local re library. Added custom regex class.

---
+++ torchtext/csrc/regex.cpp
@@ -0,0 +1,42 @@
+#include <torch/script.h>
+#include <regex>
+#include <string>
+
+namespace torchtext {
+namespace {
+
+struct Regex : torch::CustomClassHolder {
+private:
+  std::regex re_;
+public:
+  // re_str_ holds the serialized regex string passed at the initialization.
+  // We need this because we need to be able to serialize the model so that we can save the scripted object. pickle will get the
+  // serialized model from this re_str_ member, thus it needs to be public.
+  const std::string re_str_;
+
+  Regex(const std::string &re_str) : re_str_(re_str) {
+      self.re_ = std::regex(re_str_);
+  }
+
+  std::string ReplaceFirstMatch(const std::string &str, const std::string &repl) const {
+    return std::regex_replace(str, re_, repl);
+  }
+};
+
+// Registers our custom class with torch.
+static auto regex =
+    torch::class_<Regex>("torchtext", "Regex")
+        .def(torch::init<std::string>())
+        .def("ReplaceFirstMatch", &Regex::ReplaceFirstMatch)
+        .def_pickle(
+            // __getstate__
+            [](const c10::intrusive_ptr<Regex> &self) -> std::string {
+              return self->re_str_;
+            },
+            // __setstate__
+            [](std::string state) -> c10::intrusive_ptr<Regex> {
+              return c10::make_intrusive<Regex>(std::move(state));
+            });
+
+} // namespace
+} // namespace torchtext
