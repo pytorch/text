0c7f9887d87a50215c7cf25653b36f7c9bedf4bb
Nayef Ahmed <22487263+Nayef211@users.noreply.github.com>
2020-06-10 14:35:41 -0400

    Update build_tools/setup_helpers/extension.py
    Co-authored-by: moto <855818+mthrok@users.noreply.github.com>

--- build_tools/setup_helpers/extension.py
+++ build_tools/setup_helpers/extension.py
@@ -110,7 +110,7 @@ def _build_sentence_piece(debug):
 
 
 def _build_re2(debug):
-    build_dir = _TP_BASE_DIR / 're2' / 'build'
+    build_dir = _TP_BASE_DIR / 're2' / 'build_dir'
     # simply calling 'build_dir.mkdir(exist_ok=True)' fails on Windows CI Job
     if not build_dir.exists():
         build_dir.mkdir()
