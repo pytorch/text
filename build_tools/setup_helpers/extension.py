20dccb6993ed3418d5ba117c9aaf7720b315523c
moto <855818+mthrok@users.noreply.github.com>
2020-06-11 14:40:24 +0000

    fixup! check if wdong MSVC_RUNTIME_LIBRARY would make it fail

--- build_tools/setup_helpers/extension.py
+++ build_tools/setup_helpers/extension.py
@@ -118,7 +118,7 @@ def _build_re2(debug):
         extra_args = [
             '-GNinja',
             '-DCMAKE_CXX_FLAGS=/MT',
-            '-DCMAKE_MSVC_RUNTIME_LIBRARY="foo"',
+            '-DMSVC_RUNTIME_LIBRARY="foo"',
         ]
         build_env.setdefault('CC', 'cl')
         build_env.setdefault('CXX', 'cl')
