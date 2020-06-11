117d2f87ccb29af77da3919c51b2f8fd9d19ad86
Christian Puhrsch <cpuhrsch@fb.com>
2020-06-11 18:49:32 -0400

    Checkpoint

--- test/data/test_metrics.py
+++ test/data/test_metrics.py
@@ -1,5 +1,4 @@
 from torchtext.data.metrics import bleu_score
-from torch.testing import assert_allclose
 from ..common.torchtext_test_case import TorchtextTestCase
 
 
@@ -19,19 +18,19 @@ class TestUtils(TorchtextTestCase):
         # Partial match
         candidate = [['My', 'full', 'pytorch', 'test']]
         refs = [[['My', 'full', 'pytorch', 'test', '!'], ['Different']]]
-        assert_allclose(bleu_score(candidate, refs), 0.7788007)
+        self.assertEqual(bleu_score(candidate, refs), 0.7788007)
 
         # Bigrams and unigrams only
         candidate = [['My', 'pytorch', 'test']]
         refs = [[['My', 'full', 'pytorch', 'test'], ['Different']]]
-        assert_allclose(bleu_score(candidate, refs, max_n=2,
+        self.assertEqual(bleu_score(candidate, refs, max_n=2,
                                    weights=[0.5, 0.5]), 0.5066641)
 
         # Multi-sentence corpus
         candidate = [['My', 'full', 'pytorch', 'test'], ['Another', 'Sentence']]
         refs = [[['My', 'full', 'pytorch', 'test'], ['Completely', 'Different']],
                 [['No', 'Match']]]
-        assert_allclose(bleu_score(candidate, refs), 0.8408964)
+        self.assertEqual(bleu_score(candidate, refs), 0.8408964)
 
         # Empty input
         candidate = [[]]
@@ -52,13 +51,13 @@ class TestUtils(TorchtextTestCase):
 
         # The comments below give the code used to get each hardcoded bleu score
         # nltk.translate.bleu_score.corpus_bleu(refs, candidate)
-        assert_allclose(bleu_score(candidate, refs), 0.4573199)
+        self.assertEqual(bleu_score(candidate, refs), 0.4573199)
         # nltk.translate.bleu_score.corpus_bleu(refs, candidate, weights=[0.33]*3)
-        assert_allclose(bleu_score(candidate, refs, 3,
+        self.assertEqual(bleu_score(candidate, refs, 3,
                         weights=[0.33, 0.33, 0.33]), 0.4901113)
         # nltk.translate.bleu_score.corpus_bleu(refs, candidate, weights=[0.5]*2)
-        assert_allclose(bleu_score(candidate, refs, 2,
+        self.assertEqual(bleu_score(candidate, refs, 2,
                         weights=[0.5, 0.5]), 0.5119535)
         # nltk.translate.bleu_score.corpus_bleu(refs, candidate, weights=[1])
-        assert_allclose(bleu_score(candidate, refs, 1,
+        self.assertEqual(bleu_score(candidate, refs, 1,
                         weights=[1]), 0.5515605)
