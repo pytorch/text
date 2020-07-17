a7b6f2d7d48b9aea048b9f3fa9e6c465b145572a
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-07-17 08:59:24 -0700

    checkpoint

--- examples/data_pipeline/pipelines.py
+++ examples/data_pipeline/pipelines.py
@@ -4,15 +4,19 @@ from transforms import (
     PretrainedSPVocab,
     TextDataPipeline,
 )
+from torchtext.experimental.transforms import (
+    BasicEnglishNormalize,
+)
+from torchtext.experimental.vocab import vocab_from_file_object
 
 
 tokenizer = PretrainedSPTokenizer('m_user.model')
 jit_tokenizer = torch.jit.script(tokenizer)
-print('jit tokenizer success!')
+print('jit SPM tokenizer success!')
 
 vocab = PretrainedSPVocab('m_user.model')
 jit_vocab = torch.jit.script(vocab)
-print('jit vocab success!')
+print('jit SPM vocab success!')
 
 # Insert token in vocab to match a pretrained vocab
 vocab.insert_token('<pad>', 1)
@@ -22,3 +26,17 @@ print('txt_pipe1 success!')
 
 jit_txt_pipe1 = torch.jit.script(txt_pipe1)
 print('jit txt_pipe1 success!')
+
+tokenizer = BasicEnglishNormalize()
+jit_tokenizer = torch.jit.script(tokenizer)
+print('jit Basic English tokenizer success!')
+
+f = open('vocab.txt', 'r')
+vocab = vocab_from_file_object(f)
+jit_vocab = torch.jit.script(vocab)
+print('jit HF vocab success!')
+
+# Insert token in vocab to match a pretrained vocab
+pipeline2 = TextDataPipeline(tokenizer, vocab.lookup_indices)
+print(pipeline2('here is an example'))
+print('pipeline2 success!')
