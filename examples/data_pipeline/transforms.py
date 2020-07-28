92eba0acc2ffd927237d1ee0e3d64e21dccab66d
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-07-28 09:30:33 -0700

    add JITable TextSequential

--- examples/data_pipeline/transforms.py
+++ examples/data_pipeline/transforms.py
@@ -5,23 +5,6 @@ from typing import List
 from collections import OrderedDict
 import torch
 from torch import Tensor
-from typing import Any, overload, TypeVar, Union, Iterator
-from torch._jit_internal import _copy_to_script_wrapper
-
-T = TypeVar('T')
-
-
-class SequentialPipeline(nn.Module):
-    r"""Text classification pipeline template
-    """
-
-    def __init__(self, transforms):
-        self.transforms = transforms
-
-    def forward(self, line: str) -> List[int]:
-        for _transform in self.transforms:
-            line = _transform(line)
-        return line
 
 
 class TextClassificationPipeline(nn.Module):
@@ -124,90 +107,22 @@ class VectorTransform(nn.Module):
         return self.vector.lookup_vectors(tokens)
 
 
-class Sequential(nn.Module):
-    r"""A sequential container.
-    Modules will be added to it in the order they are passed in the constructor.
-    Alternatively, an ordered dict of modules can also be passed in.
-    To make it easier to understand, here is a small example::
-        # Example of using Sequential
-        model = nn.Sequential(
-                  nn.Conv2d(1,20,5),
-                  nn.ReLU(),
-                  nn.Conv2d(20,64,5),
-                  nn.ReLU()
-                )
-        # Example of using Sequential with OrderedDict
-        model = nn.Sequential(OrderedDict([
-                  ('conv1', nn.Conv2d(1,20,5)),
-                  ('relu1', nn.ReLU()),
-                  ('conv2', nn.Conv2d(20,64,5)),
-                  ('relu2', nn.ReLU())
-                ]))
+class TextSequential(nn.Sequential):
+    r"""Text Sequential pipeline
+
+        Example:
+            >>> from transforms import TextSequential
+            >>> from torchtext.experimental.transforms import BasicEnglishNormalize
+            >>> from transforms import VocabTransform
+            >>> tokenizer = BasicEnglishNormalize()
+            >>> from torchtext.experimental.vocab import Vocab
+            >>> from collections import Counter, OrderedDict
+            >>> v = Vocab(OrderedDict([(token, 1) for token in ['e', 'd', 'c', 'b', 'a']]))
+            >>> vocab = VocabTransform(v)
+            >>> txt_pipeline = TextSequential(tokenizer, vocab)
+            >>> import torch
+            >>> jit_txt_pipeline = torch.jit.script(txt_pipeline)
     """
-
-    @overload
-    def __init__(self, *args: nn.Module) -> None:
-        ...
-
-    @overload
-    def __init__(self, arg: 'OrderedDict[str, Module]') -> None:
-        ...
-
-    def __init__(self, *args: Any):
-        super(Sequential, self).__init__()
-        if len(args) == 1 and isinstance(args[0], OrderedDict):
-            for key, module in args[0].items():
-                self.add_module(key, module)
-        else:
-            for idx, module in enumerate(args):
-                self.add_module(str(idx), module)
-
-    def _get_item_by_idx(self, iterator, idx):
-        """Get the idx-th item of the iterator"""
-        size = len(self)
-        idx = operator.index(idx)
-        if not -size <= idx < size:
-            raise IndexError('index {} is out of range'.format(idx))
-        idx %= size
-        return next(islice(iterator, idx, None))
-
-    @_copy_to_script_wrapper
-    def __getitem__(self: T, idx) -> T:
-        if isinstance(idx, slice):
-            return self.__class__(OrderedDict(list(self._modules.items())[idx]))
-        else:
-            return self._get_item_by_idx(self._modules.values(), idx)
-
-    def __setitem__(self, idx: int, module: nn.Module) -> None:
-        key = self._get_item_by_idx(self._modules.keys(), idx)
-        return setattr(self, key, module)
-
-    def __delitem__(self, idx: Union[slice, int]) -> None:
-        if isinstance(idx, slice):
-            for key in list(self._modules.keys())[idx]:
-                delattr(self, key)
-        else:
-            key = self._get_item_by_idx(self._modules.keys(), idx)
-            delattr(self, key)
-
-    @_copy_to_script_wrapper
-    def __len__(self) -> int:
-        return len(self._modules)
-
-    @_copy_to_script_wrapper
-    def __dir__(self):
-        keys = super(Sequential, self).__dir__()
-        keys = [key for key in keys if not key.isdigit()]
-        return keys
-
-    @_copy_to_script_wrapper
-    def __iter__(self) -> Iterator[nn.Module]:
-        return iter(self._modules.values())
-
-    # NB: We can't really type check this function as the type of input
-    # may change dynamically (as is tested in
-    # TestScript.test_sequential_intermediary_types).  Cannot annotate
-    # with Any as TorchScript expects a more precise type
     def forward(self, input: str):
         for module in self:
             input = module(input)
