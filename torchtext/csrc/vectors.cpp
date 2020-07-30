cb9fce3a1e857be3cb0de35224bdec1b2ef623e2
Nayef Ahmed <nayef.ahmed211@gmail.com>
2020-07-30 13:18:25 -0700

    Fix lint

--- torchtext/csrc/vectors.cpp
+++ torchtext/csrc/vectors.cpp
@@ -30,8 +30,8 @@ public:
     if (static_cast<int>(tokens.size()) != vectors.size(0)) {
       throw std::runtime_error(
           "Mismatching sizes for tokens and vectors. Size of tokens: " +
-          std::to_string(tokens.size())
-          + ", size of vectors: " + std::to_string(vectors.size(0)) + ".");
+          std::to_string(tokens.size()) +
+          ", size of vectors: " + std::to_string(vectors.size(0)) + ".");
     }
 
     stovec_.reserve(tokens.size());
