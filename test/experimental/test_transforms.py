54311e4457abdfe6f9dc990850d0b3113aaf88b7
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-08-11 14:10:48 -0700

    update test

--- test/experimental/test_transforms.py
+++ test/experimental/test_transforms.py
@@ -4,12 +4,12 @@ from torchtext.experimental.transforms import (
     ToLongTensor,
     TextSequentialTransforms,
 )
-from ..common.torchtext_test_case import TorchtextTestCase
-from ..common.assets import get_asset_path
+from test.common.assets import get_asset_path
+from test.common.torchtext_test_case import TorchtextTestCase
 
 
 class TestTransforms(TorchtextTestCase):
-    def test_sentencepiece_transform_pipeline(self):
+    def test_sentencepiece_pretrained_pipeline(self):
         test_sample = 'SentencePiece is an unsupervised text tokenizer and detokenizer'
         model_path = get_asset_path('spm_example.model')
         pipeline = TextSequentialTransforms(PretrainedSPTransform(model_path), ToLongTensor)
@@ -23,7 +23,7 @@ class TestTransforms(TorchtextTestCase):
         self.assertEqual(spm_transform.decode([15340, 4286, 981, 1207, 1681, 17, 84, 684, 8896, 5366, 144, 3689,
                                                9, 5602, 12114, 6, 560, 649, 5602, 12114]), test_sample)
 
-    def test_sentencepiece_tokenizer_transform(self):
+    def test_sentencepiece_pretrained_tokenizer_transform(self):
         test_sample = 'SentencePiece is an unsupervised text tokenizer and detokenizer'
         model_path = get_asset_path('spm_example.model')
         spm_tokenizer = PretrainedSPTokenizer(model_path)
@@ -38,3 +38,14 @@ class TestTransforms(TorchtextTestCase):
         self.assertEqual(spm_tokenizer.decode(ref_results), test_sample)
         self.assertEqual(jit_spm_tokenizer(test_sample), ref_results)
         self.assertEqual(jit_spm_tokenizer.decode(ref_results), test_sample)
+
+    def test_builtin_pretrained_sentencepiece_transform(self):
+        spm_tokenizer = PretrainedSPTokenizer()
+        test_sample = 'the pretrained spm model names'
+        ref_results = ['\u2581the', '\u2581pre', 'trained', '\u2581sp', 'm', '\u2581model', '\u2581names']
+        self.assertEqual(spm_tokenizer(test_sample), ref_results)
+
+        spm_transform = PretrainedSPTransform()
+        test_sample = 'the pretrained spm model names'
+        ref_results = [9, 1546, 18811, 2849, 61, 2759, 2202]
+        self.assertEqual(spm_transform(test_sample), ref_results)
