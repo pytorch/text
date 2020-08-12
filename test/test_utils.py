1532fc584cac44578cdf1984647be677d1418db9
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-08-12 16:40:12 -0700

    flake8

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,6 +3,9 @@
 import os
 from torchtext import utils
 from .common.torchtext_test_case import TorchtextTestCase
+from test.common.assets import get_asset_path
+import tempfile
+import shutil
 
 
 def conditional_remove(f):
