a27ad90bdad0e2598478858875838b870b25718d
Nayef Ahmed <nayef.ahmed211@gmail.com>
2020-08-12 15:41:33 -0700

    Added registration for regex and senetencepiece

--- torchtext/csrc/register_bindings.cpp
+++ torchtext/csrc/register_bindings.cpp
@@ -14,12 +14,25 @@ namespace py = pybind11;
 // Registers our custom classes with pybind11.
 PYBIND11_MODULE(_torchtext, m) {
   // Classes
+  py::class_<Regex>(m, "Regex")
+      .def(py::init<std::string>())
+      .def("Sub", &Regex::Sub);
+
   py::class_<RegexTokenizer>(m, "RegexTokenizer")
       .def_readonly("patterns_", &RegexTokenizer::patterns_)
       .def_readonly("replacements_", &RegexTokenizer::replacements_)
       .def_readonly("to_lower_", &RegexTokenizer::to_lower_)
       .def(py::init<std::vector<std::string>, std::vector<std::string>, bool>())
       .def("forward", &RegexTokenizer::forward);
+
+  py::class_<SentencePiece>(m, "SentencePiece")
+      .def("Encode", &SentencePiece::Encode)
+      .def("EncodeAsIds", &SentencePiece::EncodeAsIds)
+      .def("EncodeAsPieces", &SentencePiece::EncodeAsPieces)
+      .def("GetPieceSize", &SentencePiece::GetPieceSize)
+      .def("unk_id", &SentencePiece::unk_id)
+      .def("PieceToId", &SentencePiece::PieceToId)
+      .def("IdToPiece", &SentencePiece::IdToPiece);
 
   py::class_<Vectors>(m, "Vectors")
       .def(py::init<std::vector<std::string>, std::vector<int64_t>,
