d184d0cfa305428ba0f3b35ffe7816732b5a792d
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-08-12 11:43:30 -0700

    update examples/text_classification

--- examples/text_classification/run_script.sh
+++ examples/text_classification/run_script.sh
@@ -2,5 +2,8 @@ if [ ! -d ".data" ]; then
     mkdir .data
 fi
 
-python train.py AG_NEWS --device cuda --save-model-path  model.i --dictionary vocab.i
+python train.py AG_NEWS --device cuda --save-model-path  model.i --dictionary vocab.i --use-sp-tokenizer True
+
+# To run spm with YelpReviewFull
+# python train.py YelpReviewFull --device cuda --save-model-path  model.i --dictionary vocab.i --use-sp-tokenizer True
 cut -f 2- -d "," .data/ag_news_csv/test.csv | python predict.py  model.i  vocab.i > predict_script.o
--- examples/text_classification/spm_dataset.py
+++ examples/text_classification/spm_dataset.py
@@ -1,23 +1,21 @@
-import logging
 import torch
 import io
 from torchtext.utils import download_from_url, extract_archive, unicode_csv_reader
-from os import path
 from torchtext.datasets.text_classification import URLS
-from torchtext.data.functional import generate_sp_model, \
-    load_sp_model, sentencepiece_numericalizer
 from torchtext.datasets import text_classification
+from torchtext.experimental.transforms import PretrainedSPTransform
 
 
-def _create_data_with_sp_transform(sp_generator, data_path):
+def _create_data_with_sp_transform(data_path):
 
     data = []
     labels = []
+    text_pipeline = PretrainedSPTransform('text_unigram_15000')
     with io.open(data_path, encoding="utf8") as f:
         reader = unicode_csv_reader(f)
         for row in reader:
             corpus = ' '.join(row[1:])
-            token_ids = list(sp_generator([corpus]))[0]
+            token_ids = text_pipeline(corpus)
             label = int(row[0]) - 1
             data.append((label, torch.tensor(token_ids)))
             labels.append(label)
@@ -33,18 +31,8 @@ def setup_datasets(dataset_name, root='.data', vocab_size=20000, include_unk=Fal
             train_csv_path = fname
         if fname.endswith('test.csv'):
             test_csv_path = fname
-
-    # generate sentencepiece  pretrained tokenizer
-    if not path.exists('m_user.model'):
-        logging.info('Generate SentencePiece pretrained tokenizer...')
-        generate_sp_model(train_csv_path, vocab_size)
-
-    sp_model = load_sp_model("m_user.model")
-    sp_generator = sentencepiece_numericalizer(sp_model)
-    train_data, train_labels = _create_data_with_sp_transform(sp_generator,
-                                                              train_csv_path)
-    test_data, test_labels = _create_data_with_sp_transform(sp_generator,
-                                                            test_csv_path)
+    train_data, train_labels = _create_data_with_sp_transform(train_csv_path)
+    test_data, test_labels = _create_data_with_sp_transform(test_csv_path)
 
     if len(train_labels ^ test_labels) > 0:
         raise ValueError("Training and test labels don't match")
