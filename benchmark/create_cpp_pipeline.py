3f0bdd3cef777257eded483b313ca526bd4113ce
Nayef Ahmed <nayef.ahmed211@gmail.com>
2020-08-12 22:07:09 -0700

    Debugging cpp benchmark

--- benchmark/create_cpp_pipeline.py
+++ benchmark/create_cpp_pipeline.py
@@ -24,10 +24,25 @@ class VocabTransform(nn.Module):
         return self.vocab.lookup_indices(tokens)
 
 
+class TestTokenizer(nn.Module):
+    def __init__(self):
+        super(TestTokenizer, self).__init__()
+
+    def forward(self, line: str) -> List[str]:
+        return line.split()
+
+
+# def process_dataset(file_path="/private/home/nayef211/torchtext/benchmark/.data/ag_news_csv/train.csv"):
+#     f = open(file_path, "r")
+
+    # for line in f:
+    #     line_list = line.split()
+    # for 
+
 def create_and_save_cpp_pipeline():
-    train, = AG_NEWS(data_select='train')
-    vocab = train.get_vocab()
-    tokens = []
+    # train, = AG_NEWS(data_select='train')
+    # vocab = train.get_vocab()
+    # tokens = []
     # for (_, text) in train:
     #     for id in text.tolist():
     #         tokens.append(vocab.itos[id])
@@ -37,13 +52,15 @@ def create_and_save_cpp_pipeline():
     # ordered_dict = OrderedDict(sorted_by_freq_tuples)
     # v_experimental = VocabExperimental(ordered_dict)
 
-    experimental_basic_english_normalize = BasicEnglishNormalize()
+    # experimental_basic_english_normalize = BasicEnglishNormalize()
 
-    pipeline = experimental_basic_english_normalize
+    pipeline = TestTokenizer()
     # pipeline = TextSequentialTransforms(experimental_basic_english_normalize, VocabTransform(v_experimental))
     jit_pipeline = torch.jit.script(pipeline)
 
     jit_pipeline.save("jit_pipeline.pt")
+
+    # print(jit_pipeline("test string random"))
 
     print("done")
 
--- benchmark/cscrc/benchmark_jit_pipeline.cpp
+++ benchmark/cscrc/benchmark_jit_pipeline.cpp
@@ -1,11 +1,89 @@
 #include <torch/script.h> // One-stop header.
 
 #include <iostream>
+#include <istream>
 #include <memory>
+#include <string>
+#include <vector>
+
+enum class CSVState { UnquotedField, QuotedField, QuotedQuote };
+
+std::vector<std::string> readCSVRow(const std::string &row) {
+  CSVState state = CSVState::UnquotedField;
+  std::vector<std::string> fields{""};
+  size_t field_index = 0; // index of the current field
+
+  for (char c : row) {
+    switch (state) {
+    case CSVState::UnquotedField:
+      switch (c) {
+      case ',': // end of field
+        fields.push_back("");
+        field_index++;
+        break;
+      case '"':
+        state = CSVState::QuotedField;
+        break;
+      default:
+        fields[field_index].push_back(c);
+        break;
+      }
+      break;
+    case CSVState::QuotedField:
+      switch (c) {
+      case '"':
+        state = CSVState::QuotedQuote;
+        break;
+      default:
+        fields[field_index].push_back(c);
+        break;
+      }
+      break;
+    case CSVState::QuotedQuote:
+      switch (c) {
+      case ',': // , after closing quote
+        fields.push_back("");
+        field_index++;
+        state = CSVState::UnquotedField;
+        break;
+      case '"': // "" -> "
+        fields[field_index].push_back('"');
+        state = CSVState::QuotedField;
+        break;
+      default: // end of quote
+        state = CSVState::UnquotedField;
+        break;
+      }
+      break;
+    }
+  }
+  return fields;
+}
+
+/// Read CSV file, Excel dialect. Accept "quoted fields ""with quotes"""
+std::vector<std::vector<std::string>>
+readCSV(std::string file_path,
+        std::vector<std::vector<std::string>> &all_lines) {
+  std::ifstream fin;
+  fin.open(file_path, std::ios::in);
+
+  // std::vector<std::vector<std::string>> all_lines;
+  std::string row;
+  while (!fin.eof()) {
+    std::getline(fin, row);
+    if (fin.bad() || fin.fail()) {
+      break;
+    }
+    auto fields = readCSVRow(row);
+    all_lines.push_back(fields);
+  }
+  return all_lines;
+}
 
 int main(int argc, const char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+  if (argc != 3) {
+    std::cerr << "usage: example-app <path-to-exported-script-module> "
+                 "<path-to-train-dataset-csv>\n";
     return -1;
 
     // // Create a vector of inputs.
@@ -18,13 +96,69 @@ int main(int argc, const char *argv[]) {
   }
 
   torch::jit::script::Module module;
+  std::string script_module_path = argv[1];
+  std::string file_path = argv[2];
+
   try {
     // Deserialize the ScriptModule from a file using torch::jit::load().
-    module = torch::jit::load(argv[1]);
+    module = torch::jit::load(script_module_path);
   } catch (const c10::Error &e) {
     std::cerr << "error loading the model\n";
     return -1;
   }
 
-  std::cout << "ok\n";
+  // std::vector<std::vector<std::string>> all_lines;
+  // readCSV(file_path, all_lines);
+
+  // Create a vector of inputs.
+  std::vector<torch::jit::IValue> inputs;
+  inputs.push_back("test string random");
+
+  std::cout << "[TEST 1]\n";
+
+  // std::vector<std::string> split_tokens;
+  auto split_tokens = module.forward(inputs).toListRef();
+
+  std::cout << "[TEST 2]\n";
+
+  for (int i = 0; i < split_tokens.size(); i++) {
+    // std::cout << "[TEST 3]\n";
+
+    // std::cout << split_tokens[i] << std::endl;
+    // std::string string_token = split_tokens[i].toStringRef();
+    c10::IValue ival_token = split_tokens[i];
+
+    std::cout << "[isString] " << ival_token.isString() << std::endl;
+    std::cout << "[isInt] " << ival_token.isInt() << std::endl;
+
+    std::string str_token = ival_token.toString()->string();
+    std::cout << "[print] " << str_token << std::endl;
+
+    // std::cout << string_token << std::endl;
+  }
+
+  // for (auto ivalue_token : split_tokens) {
+  //   std::cout << "[TEST 3]\n";
+
+  //   std::cout << *ivalue_token << std::endl;
+  //   std::string string_token = (*ivalue_token).toStringRef();
+
+  //   std::cout << "[TEST 4]\n";
+
+  //   std::cout << string_token << std::endl;
+  // }
+
+  // for (int i = 0; i < 5; i++) {
+  //   for (auto it = all_lines[i].begin() + 1; it != all_lines[i].end(); it++)
+  //   {
+  //     std::cout << *it << std::endl;
+  //     split_tokens = module.forward(*it);
+
+  //     for (auto &token : split_tokens) {
+  //       std::cout << token << std::endl;
+  //     }
+  //   }
+  // }
+
+  std::cout << "[DONE]\n";
 }
