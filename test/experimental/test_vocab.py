c952cd299f13c0e186c78267f26a85b583ada2b1
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-08-13 09:02:26 -0700

    checkpoint

--- test/experimental/test_vocab.py
+++ test/experimental/test_vocab.py
@@ -72,7 +72,7 @@ class TestVocab(TorchtextTestCase):
         v.append_token('b')
 
         print([v['b'], v['a'], v['<unk>']])
-        self.assertEqual([v['b'], v['a'], v['<unk>']], [2, 0, 1])
+        self.assertEqual([v['b'], v['a'], v['<unk>'], v.get_stoi(), v.get_itos()], [2, 0, 1])
         self.assertEqual(len(v), 3)
         self.assertEqual(v['b'], 2)
 
