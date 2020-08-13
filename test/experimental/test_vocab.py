6508f1cace80a0156fdace960a9b25fe4c9561b6
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-08-13 08:00:19 -0700

    checkpoint

--- test/experimental/test_vocab.py
+++ test/experimental/test_vocab.py
@@ -74,7 +74,9 @@ class TestVocab(TorchtextTestCase):
         print("[VOCAB TEST]", v['b'])
         print("v['a']", v['a'])
         print("v['<unk>']", v['<unk>'])
-        print("v.lookup_tokens([0, 1, 2])", v.lookup_tokens([0, 1, 2]))
+        print("v.lookup_token(0)", v.lookup_token(0))
+        print("v.lookup_token(1)", v.lookup_token(1))
+        print("v.lookup_token(2)", v.lookup_token(2))
 
         self.assertEqual(len(v), 3)
         self.assertEqual(v['b'], 2)
