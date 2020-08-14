251171a98c11ebd384777ac7e964df8663db8e8b
Nayef Ahmed <nayef.ahmed211@gmail.com>
2020-08-13 22:29:24 -0700

    Lint

--- torchtext/csrc/vectors.cpp
+++ torchtext/csrc/vectors.cpp
@@ -305,8 +305,8 @@ c10::intrusive_ptr<Vectors> _get_vectors_from_states(VectorsStates states) {
       stoi[strings[i]] = integers[i];
     }
 
-    return c10::make_intrusive<Vectors>(
-        std::move(stoi), std::move(tensors[0]), std::move(tensors[1]));
+    return c10::make_intrusive<Vectors>(std::move(stoi), std::move(tensors[0]),
+                                        std::move(tensors[1]));
   }
 
   throw std::runtime_error(
