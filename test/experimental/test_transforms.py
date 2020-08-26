3d7d9681272d5ff7b8c70079540cf3228b2d0e1b
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-08-26 14:27:14 -0700

    checkpoint

--- test/experimental/test_transforms.py
+++ test/experimental/test_transforms.py
@@ -29,15 +29,9 @@ class TestTransforms(TorchtextTestCase):
         with tempfile.TemporaryDirectory() as dir_name:
             data_path = os.path.join(dir_name, asset_name)
             shutil.copy(asset_path, data_path)
-            vectors_transform = VectorTransform(FastText(root=dir_name, validate_file=False))
-            jit_vectors_transform = torch.jit.script(vectors_transform.to_ivalue())
-
+            vector_transform = VectorTransform(FastText(root=dir_name, validate_file=False))
+            jit_vector_transform = torch.jit.script(vector_transform.to_ivalue())
             # The first 3 entries in each vector.
-            expected_fasttext_simple_en = {
-                'the': torch.tensor([-0.065334, -0.093031, -0.017571]),
-                'world': torch.tensor([-0.32423, -0.098845, -0.0073467]),
-            }
-
-            for word in expected_fasttext_simple_en.keys():
-                self.assertEqual(vectors_transform(word)[:3], expected_fasttext_simple_en[word])
-                self.assertEqual(jit_vectors_transform(word)[:3], expected_fasttext_simple_en[word])
+            expected_fasttext_simple_en = torch.tensor([[-0.065334, -0.093031, -0.017571], [-0.32423, -0.098845, -0.0073467]])
+            self.assertEqual(vector_transform(['the', 'world'])[:, 0:3], expected_fasttext_simple_en)
+            self.assertEqual(jit_vector_transform(['the', 'world'])[:, 0:3], expected_fasttext_simple_en)
