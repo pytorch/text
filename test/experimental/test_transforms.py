24d85005a01d5c7dfc9c8cf9cedd3d641c245f1c
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-09-01 11:31:31 -0700

    add tests

--- test/experimental/test_transforms.py
+++ test/experimental/test_transforms.py
@@ -35,3 +35,18 @@ class TestTransforms(TorchtextTestCase):
             expected_fasttext_simple_en = torch.tensor([[-0.065334, -0.093031, -0.017571], [-0.32423, -0.098845, -0.0073467]])
             self.assertEqual(vector_transform(['the', 'world'])[:, 0:3], expected_fasttext_simple_en)
             self.assertEqual(jit_vector_transform(['the', 'world'])[:, 0:3], expected_fasttext_simple_en)
+
+    def test_padding_func(self):
+        pad_transform = PadTransform(2)
+        seq_batch = [[5, 4, 5, 6, 7], [1, 3], [7, 5, 8]]
+        pad_seq, key_padding_mask = pad_transform(seq_batch)
+        expected_pad_seq = torch.tensor([[5, 4, 5, 6, 7], [1, 3, 2, 2, 2], [7, 5, 8, 2, 2]], dtype=torch.long)
+        expected_key_padding_mask = torch.tensor([[False, False, False, False, False],
+                                                  [False, False, True, True, True],
+                                                  [False, False, False, True, True]])
+        self.assertEqual(pad_seq, expected_pad_seq)
+        self.assertEqual(key_padding_mask, expected_key_padding_mask)
+        jit_pad_transform = torch.jit.script(pad_transform)
+        jit_pad_seq, jit_key_padding_mask = jit_pad_transform(seq_batch)
+        self.assertEqual(jit_pad_seq, expected_pad_seq)
+        self.assertEqual(jit_key_padding_mask, expected_key_padding_mask)
