5cde5fcbc2dcd917930d1d1e6bb05e3d655e7948
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-09-13 12:00:06 -0700

    checkpoint

--- torchtext/experimental/datasets/raw/question_answer.py
+++ torchtext/experimental/datasets/raw/question_answer.py
@@ -18,8 +18,7 @@ def _create_data_from_json(data_path):
         for layer1 in raw_json_data:
             for layer2 in layer1['paragraphs']:
                 for layer3 in layer2['qas']:
-                    _context = layer2['context']
-                    _question = layer3['question']
+                    _context, _question = layer2['context'], layer3['question']
                     _answers = [item['text'] for item in layer3['answers']]
                     _answer_start = [item['answer_start'] for item in layer3['answers']]
                     if len(_answers) == 0:
