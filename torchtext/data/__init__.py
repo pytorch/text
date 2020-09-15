1c12705536a3945e7750ea1d68a8b652aea9cbbc
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-09-15 10:20:47 -0700

    move dataset

--- torchtext/data/__init__.py
+++ torchtext/data/__init__.py
@@ -1,4 +1,3 @@
-from .dataset import Dataset, TabularDataset
 from .metrics import bleu_score
 from .pipeline import Pipeline
 from .utils import get_tokenizer, interleave_keys
@@ -8,8 +7,7 @@ from .functional import generate_sp_model, \
     sentencepiece_tokenizer, custom_replace, simple_space_split, \
     numericalize_tokens_from_iterator
 
-__all__ = ["Dataset", "TabularDataset",
-           "bleu_score",
+__all__ = ["bleu_score",
            "Pipeline",
            "get_tokenizer", "interleave_keys",
            "generate_sp_model", "load_sp_model",
--- torchtext/legacy/data/__init__.py
+++ torchtext/legacy/data/__init__.py
@@ -1,12 +1,12 @@
 from .batch import Batch
 from .example import Example
 from .field import RawField, Field, ReversibleField, SubwordField, NestedField, LabelField
-from .iterator import (batch, BucketIterator, Iterator, BPTTIterator,
-                       pool)
+from .iterator import (batch, BucketIterator, Iterator, BPTTIterator, pool)
+from .dataset import Dataset, TabularDataset
 
 __all__ = ["Batch",
            "Example",
            "RawField", "Field", "ReversibleField", "SubwordField", "NestedField",
            "LabelField",
-           "batch", "BucketIterator", "Iterator", "BPTTIterator",
-           "pool"]
+           "batch", "BucketIterator", "Iterator", "BPTTIterator", "pool",
+           "Dataset", "TabularDataset"]
