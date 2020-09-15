9fff71300b230baef9ac29ec0c50b565ab4d06b3
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-09-15 10:07:02 -0700

    first commit for legacy

---
+++ torchtext/legacy/data/__init__.py
@@ -0,0 +1,29 @@
+from .batch import Batch
+from .dataset import Dataset, TabularDataset
+from .example import Example
+from .field import RawField, Field, ReversibleField, SubwordField, NestedField, LabelField
+from .iterator import (batch, BucketIterator, Iterator, BPTTIterator,
+                       pool)
+from .metrics import bleu_score
+from .pipeline import Pipeline
+from .utils import get_tokenizer, interleave_keys
+from .functional import generate_sp_model, \
+    load_sp_model, \
+    sentencepiece_numericalizer, \
+    sentencepiece_tokenizer, custom_replace, simple_space_split, \
+    numericalize_tokens_from_iterator
+
+__all__ = ["Batch",
+           "Dataset", "TabularDataset",
+           "Example",
+           "RawField", "Field", "ReversibleField", "SubwordField", "NestedField",
+           "LabelField",
+           "batch", "BucketIterator", "Iterator", "BPTTIterator",
+           "pool",
+           "bleu_score",
+           "Pipeline",
+           "get_tokenizer", "interleave_keys",
+           "generate_sp_model", "load_sp_model",
+           "sentencepiece_numericalizer", "sentencepiece_tokenizer",
+           "custom_replace", "simple_space_split",
+           "numericalize_tokens_from_iterator"]
