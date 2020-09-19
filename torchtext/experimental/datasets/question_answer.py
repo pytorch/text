bacd1b6616d85475835a1f3002cdad8556ce90fa
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-09-19 11:33:08 -0700

    Checkpoint

--- torchtext/experimental/datasets/question_answer.py
+++ torchtext/experimental/datasets/question_answer.py
@@ -69,6 +69,9 @@ def _setup_datasets(dataset_name,
         data_select = [data_select]
     if not set(data_select).issubset(set(('train', 'dev'))):
         raise TypeError('Given data selection {} is not supported!'.format(data_select))
+    train, dev = raw.DATASETS[dataset_name](root=root)
+    raw_data = {'train': [item for item in train],
+                'dev': [item for item in dev]}
     if vocab is None:
         if 'train' not in data_select:
             raise TypeError("Must pass a vocab if train is not selected.")
@@ -79,10 +82,7 @@ def _setup_datasets(dataset_name,
                 for item in _answers:
                     tok_ans += text_transform(item)
                 yield text_transform(_context) + text_transform(_question) + tok_ans
-        vocab = build_vocab_from_iterator(apply_transform(raw.DATASETS[dataset_name](root=root)[0]))
-    train, dev = raw.DATASETS[dataset_name](root=root)
-    raw_data = {'train': [item for item in train],
-                'dev': [item for item in dev]}
+        vocab = build_vocab_from_iterator(apply_transform(raw_data['train']))
     text_transform = sequential_transforms(text_transform, vocab_func(vocab), totensor(dtype=torch.long))
     transforms = {'context': text_transform, 'question': text_transform,
                   'answers': text_transform, 'ans_pos': totensor(dtype=torch.long)}
