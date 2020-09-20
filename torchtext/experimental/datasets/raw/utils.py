eef89986b9c1ac469c3fcd77d34b2018d0368d83
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-09-20 14:26:41 -0700

    Checkpoint

--- torchtext/experimental/datasets/raw/utils.py
+++ torchtext/experimental/datasets/raw/utils.py
@@ -2,7 +2,7 @@ def process_data_select(data_select):
     if isinstance(data_select, str):
         data_select = (data_select,)
     if not isinstance(data_select, tuple):
-        raise TypeError('Expected tuple for data_select, got {} instead.'.format(type(data_select)))
+        raise TypeError('Expected tuple or string for data_select, got {} instead.'.format(type(data_select)))
     if not set(data_select).issubset(set(('train', 'test', 'valid'))):
         raise TypeError('Given data_select {} is not supported!'.format(data_select))
     return data_select
