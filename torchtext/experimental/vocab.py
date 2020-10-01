832d1d3e2ea892e4c453bbdc3cdbb119e11c717b
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-10-01 10:59:15 -0700

    checkpoint

--- torchtext/experimental/vocab.py
+++ torchtext/experimental/vocab.py
@@ -138,7 +138,7 @@ def vocab(ordered_dict, min_freq=1, unk_token='<unk>'):
 
 
 class Vocab(nn.Module):
-    __ignored_properties__ = ["is_jitable"]
+    __jit_unused_properties__ = ["is_jitable"]
     r"""Creates a vocab object which maps tokens to indices.
 
     Arguments:
