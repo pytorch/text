3a573cb5b96d09c5f5d5d0920b08206da99cbb0b
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-10-05 13:04:10 -0700

    checkpoint

--- torchtext/experimental/datasets/raw/common.py
+++ torchtext/experimental/datasets/raw/common.py
@@ -5,7 +5,7 @@ def check_default_set(data_select, target_select):
     if isinstance(data_select, str):
         data_select = (data_select)
     if not set(data_select).issubset(set(target_select)):
-        raise TypeError('A subset of data selection {} is supported! but get {}'.format(target_select, data_select))
+        raise TypeError('A subset of data selection {} is supported but {} is passed in'.format(target_select, data_select))
     return data_select
 
 
