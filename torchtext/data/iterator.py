df36c14b5b73d5b505a5dc4b9903b07b7e2b6c68
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-10-19 09:37:16 -0700

    Update the legacy retirement message

--- torchtext/data/iterator.py
+++ torchtext/data/iterator.py
@@ -45,7 +45,7 @@ class Iterator(object):
                  batch_size_fn=None, train=True,
                  repeat=False, shuffle=None, sort=None,
                  sort_within_batch=None):
-        warnings.warn('{} class will be retired in the 0.8.0 release and moved to torchtext.legacy. Please see 0.7.0 release notes for further information.'.format(self.__class__.__name__), UserWarning)
+        warnings.warn('{} class will be retired soon and moved to torchtext.legacy. Please see 0.7.0 release notes for further information.'.format(self.__class__.__name__), UserWarning)
         self.batch_size, self.train, self.dataset = batch_size, train, dataset
         self.batch_size_fn = batch_size_fn
         self.iterations = 0
