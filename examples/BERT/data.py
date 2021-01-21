cc35ce1113ed1b670880914820e49379c4e8ce85
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2021-01-21 15:02:38 -0800

    checkpoint

--- examples/BERT/data.py
+++ examples/BERT/data.py
@@ -56,27 +56,36 @@ def BookCorpus(vocab, tokenizer=get_tokenizer("basic_english"),
 
 
 class CC100(torch.utils.data.IterableDataset):
-    def __init__(self, data_directory, languages, chunk=16):
+    def __init__(self, data_directory, languages, start_line=0, chunk=16):
         """
 
         Examples:
             >>> from data import CC100
-            >>> dataset = CC100('/datasets01/cc100/031720/', {'zh_TW.txt', 'ja_XX.txt'}, chunk=10)
+            >>> dataset = CC100('/datasets01/cc100/031720/', {'zh_TW.txt', 'ja_XX.txt'}, start_line=30, chunk=10)
             >>> for rec in dataset:
             >>>     print(rec)
         """
 
         file_paths = ListDirFilesIterableDataset(data_directory, languages)
         self.dataset_list = [item[1] for item in LoadFilesFromDiskIterableDataset(file_paths)]
+        self.start_line = start_line
         self.chunk = chunk
         self._count = 0
         self._current_dataset = 0
 
     def __iter__(self):
-        for i, dataset in enumerate(self.dataset_list):
-            _count = 0
+        for i, dataset_handle in enumerate(self.dataset_list):
+            self.setup_dataset(dataset_handle)
             for _count in range(self.chunk):
-                _text = dataset.readline()
-                while _text == b'\n':
-                    _text = dataset.readline()
+                _text = self.readline(dataset_handle)
                 yield _text.decode('utf-8')
+
+    def setup_dataset(self, dataset_handle):
+        for _count in range(self.start_line):
+            _text = self.readline(dataset_handle)
+
+    def readline(self, dataset_handle):
+        _text = dataset_handle.readline()
+        while _text == b'\n':
+            _text = dataset_handle.readline()
+        return _text
