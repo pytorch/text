37ee3b7e8e8b706c2382a63facae5896e10eafea
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2021-01-24 08:00:22 -0800

    checkpoint

--- examples/BERT/cross_lingual_mlm_task.py
+++ examples/BERT/cross_lingual_mlm_task.py
@@ -110,7 +110,7 @@ def run_main(args, rank=None):
     model = model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 6.0, gamma=0.1)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.1)
     best_val_loss = None
     train_loss_log, val_loss_log = [], []
 
@@ -144,7 +144,7 @@ if __name__ == "__main__":
                         help='number of layers')
     parser.add_argument('--nhead', type=int, default=12,
                         help='the number of heads in the encoder/decoder of the transformer model')
-    parser.add_argument('--lr', type=float, default=1.0,
+    parser.add_argument('--lr', type=float, default=6.0,
                         help='initial learning rate')
     parser.add_argument('--clip', type=float, default=0.1,
                         help='gradient clipping')
