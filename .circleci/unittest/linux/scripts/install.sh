6f8a2ce3780608881c43c6676c73da6957f60b9e
George Guanheng Zhang <zhangguanheng@fb.com>
2021-01-25 15:26:58 -0800

    Import the hidden files in torchtext github repo
    Reviewed By: mthrok
    
    Differential Revision: D26001386
    
    fbshipit-source-id: f822f0f32232d3006ef629937520dee6c0faf414

---
+++ .circleci/unittest/linux/scripts/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+unset PYTORCH_VERSION
+# For unittest, nightly PyTorch is used as the following section,
+# so no need to set PYTORCH_VERSION.
+# In fact, keeping PYTORCH_VERSION forces us to hardcode PyTorch version in config.
+
+set -e
+
+eval "$(./conda/bin/conda shell.bash hook)"
+conda activate ./env
+
+printf "* Installing PyTorch\n"
+conda install -y -c "pytorch-${UPLOAD_CHANNEL}" pytorch cpuonly
+
+printf "* Installing torchtext\n"
+git submodule update --init --recursive
+python setup.py develop
