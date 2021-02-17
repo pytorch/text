6583c6b525d0766e2df0994d62813450d7363525
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-02-17 12:16:42 -0800

    Lint

--- torchtext/experimental/datasets/raw/imdb.py
+++ torchtext/experimental/datasets/raw/imdb.py
@@ -1,9 +1,7 @@
-from torchtext.utils import download_from_url, extract_archive, unicode_csv_reader
+from torchtext.utils import download_from_url
 from torchtext.experimental.datasets.raw.common import RawTextIterableDataset
 from torchtext.experimental.datasets.raw.common import wrap_split_argument
 from torchtext.experimental.datasets.raw.common import add_docstring_header
-from torchtext.experimental.datasets.raw.common import find_match
-import os
 import io
 
 URL = 'http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
