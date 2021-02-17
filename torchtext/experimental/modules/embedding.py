64964e28659013bed4774e67746b254608247e63
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2021-02-17 09:24:27 -0800

    max length of PositionalEmbedding + Batch first

--- torchtext/experimental/modules/embedding.py
+++ torchtext/experimental/modules/embedding.py
@@ -4,14 +4,14 @@ from torch.nn import Dropout, LayerNorm
 
 
 class PositionalEmbedding(nn.Module):
-    def __init__(self, embed_dim, max_len=5000):
+    def __init__(self, embed_dim, max_len=514):
         super(PositionalEmbedding, self).__init__()
         self.pos_embedding = nn.Embedding(max_len, embed_dim)
 
     def forward(self, x):
-        S, N = x.size()
+        N, S = x.size()
         pos = torch.arange(S, dtype=torch.long,
-                           device=x.device).unsqueeze(0).expand((N, S)).t()
+                           device=x.device).unsqueeze(0).expand((N, S))
         return self.pos_embedding(pos)
 
 
