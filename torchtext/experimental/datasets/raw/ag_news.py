8526764d2c757ea87fa00e76c7adcbd4004cef6d
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-02-17 12:37:28 -0800

    Lint

--- torchtext/experimental/datasets/raw/ag_news.py
+++ torchtext/experimental/datasets/raw/ag_news.py
@@ -1,4 +1,4 @@
-from torchtext.utils import download_from_url, extract_archive, unicode_csv_reader
+from torchtext.utils import download_from_url, unicode_csv_reader
 from torchtext.experimental.datasets.raw.common import RawTextIterableDataset
 from torchtext.experimental.datasets.raw.common import wrap_split_argument
 from torchtext.experimental.datasets.raw.common import add_docstring_header
