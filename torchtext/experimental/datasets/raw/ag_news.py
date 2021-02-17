d537eda040326f5d55aa9a41fe6934687981bdc3
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-02-16 20:45:24 -0800

    Fill text classification datasets

--- torchtext/experimental/datasets/raw/ag_news.py
+++ torchtext/experimental/datasets/raw/ag_news.py
@@ -1,9 +1,10 @@
-import logging
-from torchtext.utils import download_from_url, extract_archive
+from torchtext.utils import download_from_url, extract_archive, unicode_csv_reader
 from torchtext.experimental.datasets.raw.common import RawTextIterableDataset
 from torchtext.experimental.datasets.raw.common import wrap_split_argument
 from torchtext.experimental.datasets.raw.common import add_docstring_header
+from torchtext.experimental.datasets.raw.common import find_match
 import os
+import io
 
 URL = {
     'train': "https://raw.githubusercontent.com/mhjabreel/CharCnn_Keras/master/data/ag_news_csv/train.csv",
@@ -24,14 +25,20 @@ NUM_LINES = {
 @wrap_split_argument
 @add_docstring_header()
 def AG_NEWS(root='.data', split=('train', 'test'), offset=0):
+
+    def _create_data_from_csv(data_path):
+        with io.open(data_path, encoding="utf8") as f:
+            reader = unicode_csv_reader(f)
+            for row in reader:
+                yield int(row[0]), ' '.join(row[1:])
+
     extracted_files = [download_from_url(URL[item], root=root,
-                                         path=os.path.join(root, _PATHS[dataset_name][item]),
+                                         path=os.path.join(root, item + ".csv"),
                                          hash_value=MD5['AG_NEWS'][item],
                                          hash_type='md5') for item in ('train', 'test')]
     datasets = []
     for item in split:
         path = find_match(item + '.csv', extracted_files)
-        datasets.append(RawTextIterableDataset(dataset_name, NUM_LINES[dataset_name][item],
+        datasets.append(RawTextIterableDataset("AG_NEWS", NUM_LINES[item],
                                                _create_data_from_csv(path), offset=offset))
     return datasets
-
--- torchtext/experimental/datasets/raw/amazonreviewpolarity.py
+++ torchtext/experimental/datasets/raw/amazonreviewpolarity.py
@@ -1,9 +1,10 @@
-import logging
-from torchtext.utils import download_from_url, extract_archive
+from torchtext.utils import download_from_url, extract_archive, unicode_csv_reader
 from torchtext.experimental.datasets.raw.common import RawTextIterableDataset
 from torchtext.experimental.datasets.raw.common import wrap_split_argument
 from torchtext.experimental.datasets.raw.common import add_docstring_header
+from torchtext.experimental.datasets.raw.common import find_match
 import os
+import io
 
 URL = 'https://drive.google.com/uc?export=download&id=0Bz8a_Dbh9QhbaW12WVVZS2drcnM'
 
@@ -14,9 +15,25 @@ NUM_LINES = {
     'test': 400000,
 }
 
+_PATH = 'amazon_review_polarity_csv.tar.gz'
+
 
 @wrap_split_argument
 @add_docstring_header()
 def AmazonReviewPolarity(root='.data', split=('train', 'test'), offset=0):
-    pass
+    def _create_data_from_csv(data_path):
+        with io.open(data_path, encoding="utf8") as f:
+            reader = unicode_csv_reader(f)
+            for row in reader:
+                yield int(row[0]), ' '.join(row[1:])
+    dataset_tar = download_from_url(URL, root=root,
+                                    path=os.path.join(root, _PATH),
+                                    hash_value=MD5, hash_type='md5')
+    extracted_files = extract_archive(dataset_tar)
 
+    datasets = []
+    for item in split:
+        path = find_match(item + '.csv', extracted_files)
+        datasets.append(RawTextIterableDataset("AmazonReviewPolarity", NUM_LINES[item],
+                                               _create_data_from_csv(path), offset=offset))
+    return datasets
