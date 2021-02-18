f8f4e398857b03092be1c5c4c867b4cf02b58a0b
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-02-18 11:19:33 -0800

    Update init

--- torchtext/datasets/__init__.py
+++ torchtext/datasets/__init__.py
@@ -47,7 +47,7 @@ URLS = {}
 NUM_LINES = {}
 MD5 = {}
 for dataset in DATASETS:
-    dataset_module_path = "torchtext.experimental.datasets.raw." + dataset.lower()
+    dataset_module_path = "torchtext.datasets." + dataset.lower()
     dataset_module = importlib.import_module(dataset_module_path)
     URLS[dataset] = dataset_module.URL
     NUM_LINES[dataset] = dataset_module.NUM_LINES
