db8883645cf9ebf29e03fe3a2adb46e26b3f7649
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-02-17 20:02:59 -0800

    Some more cleaning

--- torchtext/experimental/datasets/raw/conll2000chunking.py
+++ torchtext/experimental/datasets/raw/conll2000chunking.py
@@ -2,6 +2,7 @@ from torchtext.utils import download_from_url, extract_archive
 from torchtext.experimental.datasets.raw.common import RawTextIterableDataset
 from torchtext.experimental.datasets.raw.common import wrap_split_argument
 from torchtext.experimental.datasets.raw.common import add_docstring_header
+from torchtext.experimental.datasets.raw.common import find_match
 
 URL = {
     'train': "https://www.clips.uantwerpen.be/conll2000/chunking/train.txt.gz",
@@ -37,23 +38,14 @@ def _create_data_from_iob(data_path, separator):
             yield columns
 
 
-def _construct_filepath(paths, file_suffix):
-    if file_suffix:
-        path = None
-        for p in paths:
-            path = p if p.endswith(file_suffix) else path
-        return path
-    return None
-
-
 @wrap_split_argument
 @add_docstring_header()
 def CoNLL2000Chunking(root='.data', split=('train', 'test'), offset=0):
     datasets = []
     for item in split:
-        dataset_tar = download_from_url(item, root=root, hash_value=MD5[item], hash_type='md5')
+        dataset_tar = download_from_url(URL[item], root=root, hash_value=MD5[item], hash_type='md5')
         extracted_files = extract_archive(dataset_tar)
-        data_filenames = _construct_filepath(extracted_files, split + ".txt")
+        data_filename = find_match(item + ".txt", extracted_files)
         datasets.append(RawTextIterableDataset("CoNLL2000Chunking", NUM_LINES[item],
-                                               _create_data_from_iob(data_filenames[item], " "), offset=offset))
+                                               _create_data_from_iob(data_filename, " "), offset=offset))
     return datasets
