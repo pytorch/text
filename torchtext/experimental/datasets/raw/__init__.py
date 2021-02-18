9743dad87b7173df8d7d2f899c768b54263d2cab
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-02-17 17:49:17 -0800

    init

--- torchtext/experimental/datasets/raw/__init__.py
+++ torchtext/experimental/datasets/raw/__init__.py
@@ -1,86 +1,56 @@
 import importlib
 from .ag_news import AG_NEWS
-from .sogounews import SogouNews
-from .dbpedia import DBpedia
-from .yelpreviewpolarity import YelpReviewPolarity
-from .yelpreviewfull import YelpReviewFull
-from .yahooanswers import YahooAnswers
-from .amazonreviewpolarity import AmazonReviewPolarity
 from .amazonreviewfull import AmazonReviewFull
+from .amazonreviewpolarity import AmazonReviewPolarity
+from .conll2000chunking import CoNLL2000Chunking
+from .dbpedia import DBpedia
 from .imdb import IMDB
-
-from .wikitext2 import WikiText2
-from .wikitext103 import WikiText103
+from .iwslt import IWSLT
+from .multi30k import Multi30k
 from .penntreebank import PennTreebank
-from .wmtnewscrawl import WMTNewsCrawl
-
+from .sogounews import SogouNews
 from .squad1 import SQuAD1
 from .squad2 import SQuAD2
-
-from .sequence_tagging import UDPOS, CoNLL2000Chunking
-
-from .multi30k import Multi30k
-from .iwslt import IWSLT
+from .udpos import UDPOS
+from .wikitext103 import WikiText103
+from .wikitext2 import WikiText2
 from .wmt14 import WMT14
+from .wmtnewscrawl import WMTNewsCrawl
+from .yahooanswers import YahooAnswers
+from .yelpreviewfull import YelpReviewFull
+from .yelpreviewpolarity import YelpReviewPolarity
 
-DATASETS = {'IMDB': IMDB,
-            'AG_NEWS': AG_NEWS,
-            'SogouNews': SogouNews,
-            'DBpedia': DBpedia,
-            'YelpReviewPolarity': YelpReviewPolarity,
-            'YelpReviewFull': YelpReviewFull,
-            'YahooAnswers': YahooAnswers,
-            'AmazonReviewPolarity': AmazonReviewPolarity,
-            'AmazonReviewFull': AmazonReviewFull,
-            'UDPOS': UDPOS,
-            'CoNLL2000Chunking': CoNLL2000Chunking,
-            'Multi30k': Multi30k,
-            'IWSLT': IWSLT,
-            'WMT14': WMT14,
-            'WikiText2': WikiText2,
-            'WikiText103': WikiText103,
-            'PennTreebank': PennTreebank,
-            'WMTNewsCrawl': WMTNewsCrawl,
-            'SQuAD1': SQuAD1,
-            'SQuAD2': SQuAD2}
+DATASETS = {
+    'AG_NEWS': AG_NEWS,
+    'AmazonReviewFull': AmazonReviewFull,
+    'AmazonReviewPolarity': AmazonReviewPolarity,
+    'CoNLL2000Chunking': CoNLL2000Chunking,
+    'DBpedia': DBpedia,
+    'IMDB': IMDB,
+    'IWSLT': IWSLT,
+    'Multi30k': Multi30k,
+    'PennTreebank': PennTreebank,
+    'SQuAD1': SQuAD1,
+    'SQuAD2': SQuAD2,
+    'SogouNews': SogouNews,
+    'UDPOS': UDPOS,
+    'WMT14': WMT14,
+    'WMTNewsCrawl': WMTNewsCrawl,
+    'WikiText103': WikiText103,
+    'WikiText2': WikiText2,
+    'YahooAnswers': YahooAnswers,
+    'YelpReviewFull': YelpReviewFull,
+    'YelpReviewPolarity': YelpReviewPolarity
+}
 
 URLS = {}
 NUM_LINES = {}
 MD5 = {}
-for dataset in ["AG_NEWS",
-                "SogouNews",
-                "DBpedia",
-                "YelpReviewPolarity",
-                "YelpReviewFull",
-                "YahooAnswers",
-                "AmazonReviewPolarity",
-                "AmazonReviewFull",
-                "IMDB",
-                "WikiText2",
-                "WikiText103",
-                "PennTreebank",
-                "WMTNewsCrawl",
-                "SQuAD1",
-                "Multi30k",
-                "IWSLT",
-                "WMT14",
-                "SQuAD2"]:
+for dataset in DATASETS:
     dataset_module_path = "torchtext.experimental.datasets.raw." + dataset.lower()
     dataset_module = importlib.import_module(dataset_module_path)
     URLS[dataset] = dataset_module.URL
     NUM_LINES[dataset] = dataset_module.NUM_LINES
     MD5[dataset] = dataset_module.MD5
-
-from .sequence_tagging import URLS as sequence_tagging_URLS
-
-URLS.update(sequence_tagging_URLS)
-
-from .sequence_tagging import NUM_LINES as sequence_tagging_NUM_LINES
-
-NUM_LINES.update(sequence_tagging_NUM_LINES)
-
-from .sequence_tagging import MD5 as sequence_tagging_MD5
-
-MD5.update(sequence_tagging_MD5)
 
 __all__ = sorted(list(map(str, DATASETS.keys())))
