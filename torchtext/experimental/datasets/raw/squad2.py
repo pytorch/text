a6f2594ca835dd2de25d651d4d32c964c6985e15
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-02-17 16:44:26 -0800

    init and Lint

--- torchtext/experimental/datasets/raw/squad2.py
+++ torchtext/experimental/datasets/raw/squad2.py
@@ -1,9 +1,8 @@
-import logging
-from torchtext.utils import download_from_url, extract_archive
+from torchtext.utils import download_from_url
+import json
 from torchtext.experimental.datasets.raw.common import RawTextIterableDataset
 from torchtext.experimental.datasets.raw.common import wrap_split_argument
 from torchtext.experimental.datasets.raw.common import add_docstring_header
-import os
 
 URL = {
     'train': "https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json",
