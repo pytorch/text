e3f727fe6968769623cdd93b8614e27ba32cadb0
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2021-02-18 20:07:15 -0800

    checkpoint

--- torchtext/datasets/snli.py
+++ torchtext/datasets/snli.py
@@ -33,9 +33,6 @@ def SNLI(root='.data', split=('train', 'dev', 'test'), offset=0):
     datasets = []
     for item in split:
         path = find_match(item + '.jsonl', extracted_files)
-        print("Looking for file that match: ", item + '.jsonl')
-        print("find_match returns:", path)
-        raise
         datasets.append(RawTextIterableDataset("SNLI", NUM_LINES[item],
                                                _create_data_from_jsonlines(path), offset=offset))
     return datasets
