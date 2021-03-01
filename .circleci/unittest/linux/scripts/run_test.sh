42ac3a99f152ea0147b11f1f0be1de4ad8aeab6c
moto <855818+mthrok@users.noreply.github.com>
2021-03-01 22:33:11 +0000

    Remove the source because it shadows the installed package

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -6,5 +6,6 @@ eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
 python -m torch.utils.collect_env
+rm -rf torchtext
 cd test
 pytest --cov=torchtext --junitxml=test-results/junit.xml -v --durations 20 .
--- .circleci/unittest/windows/scripts/run_test.sh
+++ .circleci/unittest/windows/scripts/run_test.sh
@@ -6,5 +6,6 @@ eval "$(./conda/Scripts/conda.exe 'shell.bash' 'hook')"
 conda activate ./env
 
 python -m torch.utils.collect_env
+rm -rf torchtext
 cd test
 pytest --cov=torchtext --junitxml=test-results/junit.xml -v --durations 20 .
