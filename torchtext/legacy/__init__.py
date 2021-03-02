34471115b12c7afe693d2d9b9555f881f7b3dfc3
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2021-03-02 15:55:47 -0800

    load version from legacy

--- torchtext/legacy/__init__.py
+++ torchtext/legacy/__init__.py
@@ -3,11 +3,15 @@ from .. import nn  # Not in the legacy folder
 from . import datasets
 from .. import utils  # Not in the legacy folder
 from .. import vocab  # Not in the legacy folder
-from torchtext import __version__, git_version, version
+
+try:
+    from .. import version  # noqa: F401
+    from ..version import __version__, git_version  # noqa: F401
+except ImportError:
+    pass
 
 __all__ = ['data',
            'nn',
            'datasets',
            'utils',
-           'vocab',
-           '__version__', 'git_version', 'version']
+           'vocab']
