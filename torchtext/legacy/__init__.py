493c1863dbe7be9091ba47b227772bf8ca9ca840
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2021-03-02 15:10:11 -0800

    add version inform to legacy folder

--- torchtext/legacy/__init__.py
+++ torchtext/legacy/__init__.py
@@ -3,9 +3,11 @@ from .. import nn  # Not in the legacy folder
 from . import datasets
 from .. import utils  # Not in the legacy folder
 from .. import vocab  # Not in the legacy folder
+from torchtext import __version__, git_version, version
 
 __all__ = ['data',
            'nn',
            'datasets',
            'utils',
-           'vocab']
+           'vocab',
+           '__version__', 'git_version', 'version']
