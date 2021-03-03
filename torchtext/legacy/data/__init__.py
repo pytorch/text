b4aee8dc8e582940fa5a983ea0a90ab9b4e4f9e6
Guanheng George Zhang <6156351+zhangguanheng66@users.noreply.github.com>
2021-03-03 12:34:51 -0500

    Enable importing metrics/utils/functional from torchtext.legacy.data

--- torchtext/legacy/data/__init__.py
+++ torchtext/legacy/data/__init__.py
@@ -5,8 +5,11 @@ from .iterator import (batch, BucketIterator, Iterator, BPTTIterator, pool)
 from .pipeline import Pipeline
 from .dataset import Dataset, TabularDataset
 # Those are not in the legacy folder.
+from ...data import metrics
 from ...data.metrics import bleu_score
+from ...data import utils
 from ...data.utils import get_tokenizer, interleave_keys
+from ...data import functional
 from ...data.functional import generate_sp_model, \
     load_sp_model, \
     sentencepiece_numericalizer, \
@@ -20,8 +23,11 @@ __all__ = ["Batch",
            "batch", "BucketIterator", "Iterator", "BPTTIterator", "pool",
            "Pipeline",
            "Dataset", "TabularDataset",
+           "metrics",
            "bleu_score",
+           "utils",
            "get_tokenizer", "interleave_keys",
+           "functional",
            "generate_sp_model", "load_sp_model",
            "sentencepiece_numericalizer", "sentencepiece_tokenizer",
            "custom_replace", "simple_space_split",
