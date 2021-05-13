4f8f1674bf3ac2b26c9424a3c316f0c7b5d5e6fd
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2021-05-13 10:25:13 -0400

    minor fixes

--- benchmark/benchmark_experimental_vocab.py
+++ benchmark/benchmark_experimental_vocab.py
@@ -7,11 +7,11 @@ from timeit import default_timer as timer
 from matplotlib import pyplot as plt
 import torch
 from torchtext.experimental.datasets import DATASETS
-from torchtext.vocab import (
-    vocab as VocabExperimental,
+from torchtext.experimental.vocab_factory import (
     load_vocab_from_file,
     build_vocab_from_text_file
 )
+from torchtext.vocab import vocab as VocabExperimental
 from torchtext.legacy.vocab import (
     Vocab,
     build_vocab_from_iterator
