610d6bab4eb0adadb1e9da4aa3aeee18c9ed2571
parmeet <parmeetbhatia@fb.com>
2021-06-15 07:22:57 -0400

    update docs for 0.10.0 (#1331)

---
+++ 0.10.0/_static/documentation_options.js
@@ -0,0 +1,11 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.11.0a0+e8bb3fc',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
