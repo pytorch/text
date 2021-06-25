97f66bfbce3a08ef4040ad467a3a12e58fff7c1d
Vincent Quenneville-Belair <vincentqb@fb.com>
2021-06-25 08:28:05 -0700

    Import torchtext #1300 0435df13924fd4582d67e5b17bc09f6ded18be8b
    Summary: Import torchtext #1300 0435df13924fd4582d67e5b17bc09f6ded18be8b
    
    Reviewed By: parmeet
    
    Differential Revision: D29371832
    
    fbshipit-source-id: 624280ddfa787a4e7628e60fa673cb9df0a66641

--- torchtext/experimental/datasets/__init__.py
+++ torchtext/experimental/datasets/__init__.py
@@ -1,33 +1,3 @@
-from .language_modeling import LanguageModelingDataset, WikiText2, WikiText103, PennTreebank, WMTNewsCrawl  # NOQA: F401
-from .text_classification import AG_NEWS, SogouNews, DBpedia, YelpReviewPolarity, \
-    YelpReviewFull, YahooAnswers, \
-    AmazonReviewPolarity, AmazonReviewFull, IMDB
-from .text_classification import TextClassificationDataset  # NOQA: F401
-from .sequence_tagging import SequenceTaggingDataset, UDPOS, CoNLL2000Chunking  # NOQA: F401
-from .translation import TranslationDataset, Multi30k, IWSLT2016, IWSLT2017, WMT14  # NOQA: F401
-from .question_answer import QuestionAnswerDataset, SQuAD1, SQuAD2  # NOQA: F401
+from . import raw
 
-
-DATASETS = {'WikiText2': WikiText2,
-            'WikiText103': WikiText103,
-            'PennTreebank': PennTreebank,
-            'WMTNewsCrawl': WMTNewsCrawl,
-            'IMDB': IMDB,
-            'AG_NEWS': AG_NEWS,
-            'SogouNews': SogouNews,
-            'DBpedia': DBpedia,
-            'YelpReviewPolarity': YelpReviewPolarity,
-            'YelpReviewFull': YelpReviewFull,
-            'YahooAnswers': YahooAnswers,
-            'AmazonReviewPolarity': AmazonReviewPolarity,
-            'AmazonReviewFull': AmazonReviewFull,
-            'UDPOS': UDPOS,
-            'CoNLL2000Chunking': CoNLL2000Chunking,
-            'Multi30k': Multi30k,
-            'IWSLT2016': IWSLT2016,
-            'IWSLT2017': IWSLT2017,
-            'WMT14': WMT14,
-            'SQuAD1': SQuAD1,
-            'SQuAD2': SQuAD2}
-
-__all__ = sorted(list(map(str, DATASETS.keys())))
+__all__ = ['raw']
