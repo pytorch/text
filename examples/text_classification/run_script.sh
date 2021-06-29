657997f3c003dbf5dd2ca5149ddee9ab9d247c67
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2021-06-28 22:32:31 -0400

    fixing text classification example

--- examples/text_classification/run_script.sh
+++ examples/text_classification/run_script.sh
@@ -2,7 +2,7 @@ if [ ! -d ".data" ]; then
     mkdir .data
 fi
 
-python train.py AG_NEWS --device cuda --save-model-path  model.i --dictionary vocab.i
+python train.py AG_NEWS --device cpu --save-model-path  model.i --dictionary vocab.i --use-sp-tokenizer True --num-epochs 1 
 # To run spm with YelpReviewFull
 # python train.py YelpReviewFull --device cuda --save-model-path  model.i --dictionary vocab.i --use-sp-tokenizer True
-cut -f 2- -d "," .data/ag_news_csv/test.csv | python predict.py  model.i  vocab.i > predict_script.o
+cut -f 2- -d "," .data/AG_NEWS/test.csv | python predict.py  model.i  vocab.i --use-sp-tokenizer True> predict_script.o
