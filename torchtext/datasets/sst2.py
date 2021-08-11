e7b975a576e426bfbed173239b04d7f4bafe27ef
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2021-08-10 22:16:40 -0400

    adding saver dp to sst2

--- torchtext/datasets/sst2.py
+++ torchtext/datasets/sst2.py
@@ -8,9 +8,11 @@ from datapipes.iter import(
     CSVParser,
     ReadFilesFromZip,
     HttpReader,
+    IterableAsDataPipe,
 )
 
 
+from torch.utils.data.datapipes.iter import LoadFilesFromDisk
 NUM_LINES = {}
 MD5 = {}
 URL = "https://dl.fbaipublicfiles.com/glue/data/SST-2.zip"
@@ -22,9 +24,7 @@ _PATH = "SST-2.zip"
 @_create_dataset_directory(dataset_name=DATASET_NAME)
 @_wrap_split_argument(('train', 'dev', 'test'))
 def SST2(root, split):
-    save_path = os.path.join(root, _PATH)
-    http_list = [d for d in HttpReader([URL]).map(lambda x: x[1])]
-    with open(save_path, 'wb') as f:
-        f.write(http_list[0].read())
-    extracted_files = ReadFilesFromZip([(os.path.dirname(save_path), open(save_path, 'rb'))])
-    return extracted_files.filter(lambda x: split in x[0]).parse_csv_files(delimiter='\t').map(lambda x: (x[1],x[2]))
+    # TODO: not working: cache_dp = IterableAsDataPipe([URL]).on_disk_cache(HttpReader, filepath_fn=lambda x: os.path.join(root, os.path.basename(x)))
+    saver_dp = HttpReader([URL]).map(lambda x: (x[0],x[1].read())).save_to_disk(filepath_fn=lambda x: os.path.join(root, os.path.basename(x)))
+    extracted_files=LoadFilesFromDisk(saver_dp).read_from_zip()
+    return extracted_files.filter(lambda x: split in x[0]).parse_csv_files(skip_header = True, delimiter='\t').map(lambda x: (x[1], int(x[2])))
