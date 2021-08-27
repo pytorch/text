aa12e9a4a402bfb030f453c5fea375a2b0746f0a
parmeet <parmeetbhatia@fb.com>
2021-08-26 20:37:46 -0400

    [fbsync] Enabling torchtext extension work seamlessly between OSS and on fbcode (#1382)

--- torchtext/__init__.py
+++ torchtext/__init__.py
@@ -5,6 +5,7 @@ from . import utils
 from . import vocab
 from . import experimental
 from . import legacy
+from ._extension import _init_extension
 
 
 try:
@@ -19,26 +20,6 @@ __all__ = ['data',
            'vocab',
            'experimental',
            'legacy']
-
-
-def _init_extension():
-    import os
-    import importlib
-    import torch
-
-    # load the custom_op_library and register the custom ops
-    lib_dir = os.path.dirname(__file__)
-    loader_details = (
-        importlib.machinery.ExtensionFileLoader,
-        importlib.machinery.EXTENSION_SUFFIXES
-    )
-
-    extfinder = importlib.machinery.FileFinder(lib_dir, loader_details)
-    ext_specs = extfinder.find_spec("_torchtext")
-    if ext_specs is None:
-        raise ImportError("torchtext C++ Extension is not found.")
-    torch.ops.load_library(ext_specs.origin)
-    torch.classes.load_library(ext_specs.origin)
 
 
 _init_extension()
