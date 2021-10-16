933f691990ecaccc4d899153494b921357bb632f
pytorchbot <soumith+bot@pytorch.org>
2021-10-16 11:35:11 +0000

    auto-generating sphinx docs

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.0.dev20211015',
+    VERSION: '0.12.0.dev20211016',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
