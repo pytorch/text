62874976940fd6c2336b910b89ccf4cbea134a0e
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2021-10-17 20:15:59 -0400

    remove pretrained spm model

--- torchtext/transforms.py
+++ torchtext/transforms.py
@@ -6,15 +6,6 @@ from typing import List
 
 import os
 
-PRETRAINED_SP_MODEL = {
-    'text_unigram_15000': 'https://pytorch.s3.amazonaws.com/models/text/pretrained_spm/text_unigram_15000.model',
-    'text_unigram_25000': 'https://pytorch.s3.amazonaws.com/models/text/pretrained_spm/text_unigram_25000.model',
-    'text_unigram_50000': 'https://pytorch.s3.amazonaws.com/models/text/pretrained_spm/text_unigram_50000.model',
-    'text_bpe_15000': 'https://pytorch.s3.amazonaws.com/models/text/pretrained_spm/text_bpe_15000.model',
-    'text_bpe_25000': 'https://pytorch.s3.amazonaws.com/models/text/pretrained_spm/text_bpe_25000.model',
-    'text_bpe_50000': 'https://pytorch.s3.amazonaws.com/models/text/pretrained_spm/text_bpe_50000.model'
-}
-
 
 class SpmTokenizerTransform(Module):
     """
