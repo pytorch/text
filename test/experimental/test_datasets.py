fe9bfe0adccda4da995ee6c42c2418a7b41c5daf
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared7391.03.atn3.facebook.com>
2021-10-23 04:33:59 -0700

    2021-10-23 nightly release (bcc14559c1a623cc05d2fb676b382f6a1efb8696)

--- test/experimental/test_datasets.py
+++ test/experimental/test_datasets.py
@@ -11,24 +11,28 @@ class TestDataset(TorchtextTestCase):
     @skipIfNoModule("torchdata")
     def test_sst2_dataset(self):
         split = ("train", "dev", "test")
-        train_dp, dev_dp, test_dp = sst2.SST2(split=split)
+        train_dataset, dev_dataset, test_dataset = sst2.SST2(split=split)
+
+        # verify datasets objects are instances of SST2Dataset
+        for dataset in (train_dataset, dev_dataset, test_dataset):
+            self.assertTrue(isinstance(dataset, sst2.SST2Dataset))
 
         # verify hashes of first line in dataset
         self.assertEqual(
             hashlib.md5(
-                json.dumps(next(iter(train_dp)), sort_keys=True).encode("utf-8")
+                json.dumps(next(iter(train_dataset)), sort_keys=True).encode("utf-8")
             ).hexdigest(),
             sst2._FIRST_LINE_MD5["train"],
         )
         self.assertEqual(
             hashlib.md5(
-                json.dumps(next(iter(dev_dp)), sort_keys=True).encode("utf-8")
+                json.dumps(next(iter(dev_dataset)), sort_keys=True).encode("utf-8")
             ).hexdigest(),
             sst2._FIRST_LINE_MD5["dev"],
         )
         self.assertEqual(
             hashlib.md5(
-                json.dumps(next(iter(test_dp)), sort_keys=True).encode("utf-8")
+                json.dumps(next(iter(test_dataset)), sort_keys=True).encode("utf-8")
             ).hexdigest(),
             sst2._FIRST_LINE_MD5["test"],
         )
