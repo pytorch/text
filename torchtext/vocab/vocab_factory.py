1b347dc0897cb62bc5b7b308ef442f6fccff5ec7
Abhinav Arora <abhinavarora@fb.com>
2021-11-09 16:24:33 -0800

    [Vocab] Refactor vocab factory method to accept special tokens as a keyword argument

--- torchtext/vocab/vocab_factory.py
+++ torchtext/vocab/vocab_factory.py
@@ -6,7 +6,7 @@ from torchtext._torchtext import (
 )
 
 
-def vocab(ordered_dict: Dict, min_freq: int = 1) -> Vocab:
+def vocab(ordered_dict: Dict, min_freq: int = 1, specials: List[str] = [], special_first: bool = True) -> Vocab:
     r"""Factory method for creating a vocab object which maps tokens to indices.
 
     Note that the ordering in which key value pairs were inserted in the `ordered_dict` will be respected when building the vocab.
@@ -15,6 +15,8 @@ def vocab(ordered_dict: Dict, min_freq: int = 1) -> Vocab:
     Args:
         ordered_dict: Ordered Dictionary mapping tokens to their corresponding occurance frequencies.
         min_freq: The minimum frequency needed to include a token in the vocabulary.
+        specials: Special symbols to add. The order of supplied tokens will be preserved.
+        special_first: Indicates whether to insert symbols at the beginning or at the end.
 
     Returns:
         torchtext.vocab.Vocab: A `Vocab` object
@@ -34,18 +36,26 @@ def vocab(ordered_dict: Dict, min_freq: int = 1) -> Vocab:
         >>> unk_token = '<unk>'
         >>> default_index = -1
         >>> if unk_token not in v2: v2.insert_token(unk_token, 0)
-        >>> v2.set_default_index(default_index)
-        >>> print(v2['<unk>']) #prints 0
-        >>> print(v2['out of vocab']) #prints -1
+        >>> v3 = vocab(OrderedDict([(token, 1) for token in tokens]), specials=[unk_token])
+        >>> v3.set_default_index(default_index)
+        >>> print(v3['<unk>']) #prints 0
+        >>> print(v3['out of vocab']) #prints -1
         >>> #make default index same as index of unk_token
-        >>> v2.set_default_index(v2[unk_token])
-        >>> v2['out of vocab'] is v2[unk_token] #prints True
+        >>> v3.set_default_index(v3[unk_token])
+        >>> v3['out of vocab'] is v3[unk_token] #prints True
     """
+    for token in specials:
+        ordered_dict.pop(token, None)
 
     tokens = []
     for token, freq in ordered_dict.items():
         if freq >= min_freq:
             tokens.append(token)
+
+    if special_first:
+        tokens[0:0] = specials
+    else:
+        tokens.extend(specials)
 
     return Vocab(VocabPybind(tokens, None))
 
@@ -79,20 +89,10 @@ def build_vocab_from_iterator(iterator: Iterable, min_freq: int = 1, specials: O
     for tokens in iterator:
         counter.update(tokens)
 
-    if specials is not None:
-        for tok in specials:
-            del counter[tok]
-
     sorted_by_freq_tuples = sorted(counter.items(), key=lambda x: x[0])
     sorted_by_freq_tuples.sort(key=lambda x: x[1], reverse=True)
     ordered_dict = OrderedDict(sorted_by_freq_tuples)
 
-    if specials is not None:
-        if special_first:
-            specials = specials[::-1]
-        for symbol in specials:
-            ordered_dict.update({symbol: min_freq})
-            ordered_dict.move_to_end(symbol, last=not special_first)
-
-    word_vocab = vocab(ordered_dict, min_freq=min_freq)
+    word_vocab = vocab(ordered_dict, min_freq=min_freq, specials=specials or [],
+                       special_first=special_first)
     return word_vocab
