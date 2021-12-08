3736f135a277ed16530a6b265885b8dbb7eb1a51
parmeet <parmeetbhatia@fb.com>
2021-12-08 15:33:48 -0500

    Update annotation types of transforms and functionals (#1453)

--- torchtext/functional.py
+++ torchtext/functional.py
@@ -1,7 +1,7 @@
 import torch
 from torch import Tensor
 from torch.nn.utils.rnn import pad_sequence
-from typing import List, Optional, Union
+from typing import List, Optional, Any
 
 __all__ = [
     'to_tensor',
@@ -10,10 +10,20 @@ __all__ = [
 ]
 
 
-def to_tensor(input: Union[List[int], List[List[int]]], padding_value: Optional[int] = None, dtype: Optional[torch.dtype] = torch.long) -> Tensor:
+def to_tensor(input: Any, padding_value: Optional[int] = None, dtype: Optional[torch.dtype] = torch.long) -> Tensor:
+    r"""Convert input to torch tensor
+
+    :param padding_value: Pad value to make each input in the batch of length equal to the longest sequence in the batch.
+    :type padding_value: Optional[int]
+    :param dtype: :class:`torch.dtype` of output tensor
+    :type dtype: :class:`torch.dtype`
+    :param input: Sequence or batch of token ids
+    :type input: Union[List[int], List[List[int]]]
+    :rtype: Tensor
+    """
     if torch.jit.isinstance(input, List[int]):
         return torch.tensor(input, dtype=torch.long)
-    else:
+    elif torch.jit.isinstance(input, List[List[int]]):
         if padding_value is None:
             output = torch.tensor(input, dtype=dtype)
             return output
@@ -24,27 +34,61 @@ def to_tensor(input: Union[List[int], List[List[int]]], padding_value: Optional[
                 padding_value=float(padding_value)
             )
             return output
+    else:
+        raise TypeError("Input type not supported")
 
 
-def truncate(input: Union[List[int], List[List[int]]], max_seq_len: int) -> Union[List[int], List[List[int]]]:
+def truncate(input: Any, max_seq_len: int) -> Any:
+    """ Truncate input sequence or batch
+
+    :param input: Input sequence or batch to be truncated
+    :type input: Union[List[Union[str, int]], List[List[Union[str, int]]]]
+    :param max_seq_len: Maximum length beyond which input is discarded
+    :type max_seq_len: int
+    :return: Truncated sequence
+    :rtype: Union[List[Union[str, int]], List[List[Union[str, int]]]]
+    """
     if torch.jit.isinstance(input, List[int]):
         return input[:max_seq_len]
-    else:
+    elif torch.jit.isinstance(input, List[str]):
+        return input[:max_seq_len]
+    elif torch.jit.isinstance(input, List[List[int]]):
         output: List[List[int]] = []
-
         for ids in input:
             output.append(ids[:max_seq_len])
-
         return output
+    elif torch.jit.isinstance(input, List[List[str]]):
+        output: List[List[str]] = []
+        for ids in input:
+            output.append(ids[:max_seq_len])
+        return output
+    else:
+        raise TypeError("Input type not supported")
 
 
-def add_token(input: Union[List[int], List[List[int]]], token_id: int, begin: bool = True) -> Union[List[int], List[List[int]]]:
-    if torch.jit.isinstance(input, List[int]):
+def add_token(input: Any, token_id: Any, begin: bool = True) -> Any:
+    """Add token to start or end of sequence
+
+    :param input: Input sequence or batch
+    :type input: Union[List[Union[str, int]], List[List[Union[str, int]]]]
+    :param token_id: token to be added
+    :type token_id: Union[str, int]
+    :param begin: Whether to insert token at start or end or sequence, defaults to True
+    :type begin: bool, optional
+    :return: sequence or batch with token_id added to begin or end or input
+    :rtype: Union[List[Union[str, int]], List[List[Union[str, int]]]]
+    """
+    if torch.jit.isinstance(input, List[int]) and torch.jit.isinstance(token_id, int):
         if begin:
             return [token_id] + input
         else:
             return input + [token_id]
-    else:
+    elif torch.jit.isinstance(input, List[str]) and torch.jit.isinstance(token_id, str):
+        if begin:
+            return [token_id] + input
+        else:
+            return input + [token_id]
+    elif torch.jit.isinstance(input, List[List[int]]) and torch.jit.isinstance(token_id, int):
         output: List[List[int]] = []
 
         if begin:
@@ -55,3 +99,15 @@ def add_token(input: Union[List[int], List[List[int]]], token_id: int, begin: bo
                 output.append(ids + [token_id])
 
         return output
+    elif torch.jit.isinstance(input, List[List[str]]) and torch.jit.isinstance(token_id, str):
+        output: List[List[str]] = []
+        if begin:
+            for ids in input:
+                output.append([token_id] + ids)
+        else:
+            for ids in input:
+                output.append(ids + [token_id])
+
+        return output
+    else:
+        raise TypeError("Input type not supported")
--- torchtext/transforms.py
+++ torchtext/transforms.py
@@ -5,7 +5,7 @@ import torch
 from torchtext.data.functional import load_sp_model
 from torchtext.utils import download_from_url
 from torchtext.vocab import Vocab
-from typing import List, Optional, Union
+from typing import List, Optional, Any
 import os
 
 from torchtext import _CACHE_DIR
@@ -20,9 +20,14 @@ __all__ = [
 
 class SentencePieceTokenizer(Module):
     """
-    Transform for Sentence Piece tokenizer from pre-trained SentencePiece model
+    Transform for Sentence Piece tokenizer from pre-trained sentencepiece model
 
-    Examples:
+    Additiona details: https://github.com/google/sentencepiece
+
+    :param sp_model_path: Path to pre-trained sentencepiece model
+    :type sp_model_path: str
+
+    Example
         >>> from torchtext.transforms import SpmTokenizerTransform
         >>> transform = SentencePieceTokenizer("spm_model")
         >>> transform(["hello world", "attention is all you need!"])
@@ -36,21 +41,28 @@ class SentencePieceTokenizer(Module):
             local_path = download_from_url(url=sp_model_path, root=_CACHE_DIR)
         self.sp_model = load_sp_model(local_path)
 
-    def forward(self, input: Union[str, List[str]]) -> Union[List[str], List[List[str]]]:
+    def forward(self, input: Any) -> Any:
+        """
+        :param input: Input sentence or list of sentences on which to apply tokenizer.
+        :type input: Union[str, List[str]]
+        :return: tokenized text
+        :rtype: Union[List[str], List[List(str)]]
+        """
         if torch.jit.isinstance(input, List[str]):
             tokens: List[List[str]] = []
             for text in input:
                 tokens.append(self.sp_model.EncodeAsPieces(text))
             return tokens
-        else:
+        elif torch.jit.isinstance(input, str):
             return self.sp_model.EncodeAsPieces(input)
+        else:
+            raise TypeError("Input type not supported")
 
 
 class VocabTransform(Module):
-    r"""Vocab transform
+    r"""Vocab transform to convert input batch of tokens into corresponding token ids
 
-    Args:
-        vocab: an instance of torchtext.vocab.Vocab class.
+    :param vocab: an instance of :class:`torchtext.vocab.Vocab` class.
 
     Example:
         >>> import torch
@@ -68,28 +80,33 @@ class VocabTransform(Module):
         assert isinstance(vocab, Vocab)
         self.vocab = vocab
 
-    def forward(self, input: Union[List[str], List[List[str]]]) -> Union[List[int], List[List[int]]]:
-        r"""
-
-        Args:
-            input: list of list tokens
+    def forward(self, input: Any) -> Any:
+        """
+        :param input: Input batch of token to convert to correspnding token ids
+        :type input: Union[List[str], List[List[str]]]
+        :return: Converted input into corresponding token ids
+        :rtype: Union[List[int], List[List[int]]]
         """
 
         if torch.jit.isinstance(input, List[str]):
             return self.vocab.lookup_indices(input)
-        else:
+        elif torch.jit.isinstance(input, List[List[str]]):
             output: List[List[int]] = []
             for tokens in input:
                 output.append(self.vocab.lookup_indices(tokens))
 
             return output
+        else:
+            raise TypeError("Input type not supported")
 
 
 class ToTensor(Module):
     r"""Convert input to torch tensor
 
-    Args:
-        padding_value (int, optional): Pad value to make each input in the batch of length equal to the longest sequence in the batch.
+    :param padding_value: Pad value to make each input in the batch of length equal to the longest sequence in the batch.
+    :type padding_value: Optional[int]
+    :param dtype: :class:`torch.dtype` of output tensor
+    :type dtype: :class:`torch.dtype`
     """
 
     def __init__(self, padding_value: Optional[int] = None, dtype: Optional[torch.dtype] = torch.long) -> None:
@@ -97,10 +114,11 @@ class ToTensor(Module):
         self.padding_value = padding_value
         self.dtype = dtype
 
-    def forward(self, input: Union[List[int], List[List[int]]]) -> Tensor:
-        r"""
-        Args:
-
+    def forward(self, input: Any) -> Tensor:
+        """
+        :param input: Sequence or batch of token ids
+        :type input: Union[List[int], List[List[int]]]
+        :rtype: Tensor
         """
         return F.to_tensor(input, padding_value=self.padding_value, dtype=self.dtype)
 
@@ -109,15 +127,16 @@ class LabelToIndex(Module):
     r"""
     Transform labels from string names to ids.
 
-    Args:
-        label_names (List[str], Optional): a list of unique label names
-        label_path (str, Optional): a path to file containing unique label names containing 1 label per line.
+    :param label_names: a list of unique label names
+    :type label_names: Optional[List[str]]
+    :param label_path: a path to file containing unique label names containing 1 label per line. Note that either label_names or label_path should be supplied
+                       but not both.
+    :type label_path: Optional[str]
     """
 
     def __init__(
         self, label_names: Optional[List[str]] = None, label_path: Optional[str] = None, sort_names=False,
     ):
-
         assert label_names or label_path, "label_names or label_path is required"
         assert not (label_names and label_path), "label_names and label_path are mutually exclusive"
         super().__init__()
@@ -133,11 +152,18 @@ class LabelToIndex(Module):
         self._label_vocab = Vocab(torch.classes.torchtext.Vocab(label_names, None))
         self._label_names = self._label_vocab.get_itos()
 
-    def forward(self, labels: Union[str, List[str]]) -> Union[int, List[int]]:
-        if torch.jit.isinstance(labels, List[str]):
-            return self._label_vocab.lookup_indices(labels)
+    def forward(self, input: Any) -> Any:
+        """
+        :param input: Input labels to convert to corresponding ids
+        :type input: Union[str, List[str]]
+        :rtype: Union[int, List[int]]
+        """
+        if torch.jit.isinstance(input, List[str]):
+            return self._label_vocab.lookup_indices(input)
+        elif torch.jit.isinstance(input, str):
+            return self._label_vocab.__getitem__(input)
         else:
-            return self._label_vocab.__getitem__(labels)
+            raise TypeError("Input type not supported")
 
     @property
     def label_names(self) -> List[str]:
