79551dbe05c5855e12154adaa3240560129610d3
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2021-12-08 11:05:49 -0500

    update test

--- test/test_functional.py
+++ test/test_functional.py
@@ -21,18 +21,20 @@ class TestFunctional(TorchtextTestCase):
         torch.testing.assert_close(actual, expected)
 
     def test_to_tensor(self):
+        """test tensorization on both single sequence and batch of sequence"""
         self._to_tensor(test_scripting=False)
 
     def test_to_tensor_jit(self):
+        """test tensorization with scripting on both single sequence and batch of sequence"""
         self._to_tensor(test_scripting=True)
 
     def _truncate(self, test_scripting):
-        input = [[1, 2], [1, 2, 3]]
         max_seq_len = 2
-
         func = functional.truncate
         if test_scripting:
             func = torch.jit.script(func)
+
+        input = [[1, 2], [1, 2, 3]]
         actual = func(input, max_seq_len=max_seq_len)
         expected = [[1, 2], [1, 2]]
         self.assertEqual(actual, expected)
@@ -42,13 +44,26 @@ class TestFunctional(TorchtextTestCase):
         expected = [1, 2]
         self.assertEqual(actual, expected)
 
+        input = [["a", "b"], ["a", "b", "c"]]
+        actual = func(input, max_seq_len=max_seq_len)
+        expected = [["a", "b"], ["a", "b"]]
+        self.assertEqual(actual, expected)
+
+        input = ["a", "b", "c"]
+        actual = func(input, max_seq_len=max_seq_len)
+        expected = ["a", "b"]
+        self.assertEqual(actual, expected)
+
     def test_truncate(self):
+        """test truncation on both sequence and batch of sequence with both str and int types"""
         self._truncate(test_scripting=False)
 
     def test_truncate_jit(self):
+        """test truncation with scripting on both sequence and batch of sequence with both str and int types"""
         self._truncate(test_scripting=True)
 
     def _add_token(self, test_scripting):
+
         func = functional.add_token
         if test_scripting:
             func = torch.jit.script(func)
