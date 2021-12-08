Fix bool attention mask in Transformer encoder

torchtext/models/roberta/modules.py
@@ -130,10 +130,9 @@ class MultiheadSelfAttention(Module):
             torch._assert(attn_mask.size(1) == source_length, "attn_mask shape didn't match for source length {}".format(source_length))
             torch._assert(attn_mask.is_floating_point() or attn_mask.dtype == torch.bool, f"Only float or bool types are supported for attn_mask not {attn_mask.dtype}")
             if attn_mask.dtype == torch.bool:
-                attn_mask = attn_mask.masked_fill(
-                    attn_mask,
-                    -1e8 if query.dtype == torch.float32 else -1e4
-                )
+                new_attn_mask = torch.zeros_like(attn_mask, dtype=query.dtype)
+                new_attn_mask.masked_fill_(attn_mask, -1e8 if query.dtype == torch.float32 else -1e4)
+                attn_mask = new_attn_mask
             attn_mask = attn_mask.unsqueeze(0)
             attn_weights += attn_mask
 
@@ -226,10 +225,9 @@ class TransformerEncoderLayer(Module):
             torch._assert(attn_mask.dim() == 2, "Expected attn_mask of dim 2 but got {}".format(attn_mask.dim()))
             torch._assert(attn_mask.is_floating_point() or attn_mask.dtype == torch.bool, f"Only float or bool types are supported for attn_mask not {attn_mask.dtype}")
             if attn_mask.dtype == torch.bool:
-                attn_mask = attn_mask.masked_fill(
-                    attn_mask,
-                    -1e8 if input.dtype == torch.float32 else -1e4
-                )
+                new_attn_mask = torch.zeros_like(attn_mask, dtype=input.dtype)
+                new_attn_mask.masked_fill_(attn_mask, -1e8 if input.dtype == torch.float32 else -1e4)
+                attn_mask = new_attn_mask
 
         if not hasattr(self, "normalize_before"):
             self.normalize_before = False
@@ -291,9 +289,11 @@ class TransformerEncoder(Module):
 
     def forward(self, tokens: torch.Tensor, attn_mask: Optional[torch.Tensor] = None) -> Union[torch.Tensor, List[torch.Tensor]]:
         if attn_mask is not None:
-            torch._assert(attn_mask.dtype == torch.bool, "Expected attn_mask dtype as `torch.bool` but got {}".format(attn_mask.dtype))
-            torch._assert(attn_mask.dim() == 2, "Expected attn_mask of dim 2 but got {}".format(attn_mask.dim()))
-
+            torch._assert(attn_mask.is_floating_point() or attn_mask.dtype == torch.bool, f"Only float or bool types are supported for attn_mask not {attn_mask.dtype}")
+            if attn_mask.dtype == torch.bool:
+                new_attn_mask = torch.zeros_like(attn_mask, dtype=input.dtype)
+                new_attn_mask.masked_fill_(attn_mask, -1e8 if input.dtype == torch.float32 else -1e4)
+                attn_mask = new_attn_mask
         padding_mask = tokens.eq(self.padding_idx)
 
         token_embeddings = self.token_embedding(tokens)