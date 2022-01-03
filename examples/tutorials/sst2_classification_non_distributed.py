e29367afb02e45ce2a90eebeb5f5be4fc54beebb
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2022-01-03 18:01:10 -0500

    setup for sphinx

--- examples/tutorials/sst2_classification_non_distributed.py
+++ examples/tutorials/sst2_classification_non_distributed.py
@@ -1,3 +1,17 @@
+"""
+SST-2 Binary text classification with XLM-RoBERTa model
+=======================================================
+
+**Author**: `Parmeet Bhatia<parmeetbhatia@fb.com>`__
+
+This tutorial shows how to train a text classifier using pre-trained XLM-RoBERTa model. 
+"""
+
+######################################################################
+
+
+
+
 import torch
 from torchtext.models import RobertaClassificationHead, XLMR_BASE_ENCODER
 import torchtext.transforms as T
@@ -6,6 +20,9 @@ from torchtext.experimental.datasets.sst2 import SST2
 from torch.hub import load_state_dict_from_url
 from torch.optim import AdamW
 import torch.nn as nn
+
+
+
 DEVICE = torch.DEVICE("cuda") if torch.cuda.is_available() else "cpu"
 
 
