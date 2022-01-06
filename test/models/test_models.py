f3ae69be805f2090c132d05f4c6f86d47eacd16b
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2022-01-05 19:11:27 -0500

    revert test_models.py

--- test/models/test_models.py
+++ test/models/test_models.py
@@ -87,18 +87,18 @@ class TestModels(TorchtextTestCase):
         xlmr_base = torchtext.models.XLMR_BASE_ENCODER
         transform = xlmr_base.transform()
         test_text = "XLMR base Model Comparison"
-        actual = transform(test_text)
-        expected = [0, 43523, 52005, 3647, 13293, 113307, 40514, 2]
-        self.assertEqual(actual, expected)
+        actual = transform([test_text])
+        expected = [[0, 43523, 52005, 3647, 13293, 113307, 40514, 2]]
+        torch.testing.assert_close(actual, expected)
 
     def test_xlmr_transform_jit(self):
         xlmr_base = torchtext.models.XLMR_BASE_ENCODER
         transform = xlmr_base.transform()
         transform_jit = torch.jit.script(transform)
         test_text = "XLMR base Model Comparison"
-        actual = transform_jit(test_text)
-        expected = [0, 43523, 52005, 3647, 13293, 113307, 40514, 2]
-        self.assertEqual(actual, expected)
+        actual = transform_jit([test_text])
+        expected = [[0, 43523, 52005, 3647, 13293, 113307, 40514, 2]]
+        torch.testing.assert_close(actual, expected)
 
     def test_roberta_bundler_build_model(self):
         from torchtext.models import RobertaEncoderConf, RobertaClassificationHead, RobertaModel, RobertaModelBundle
