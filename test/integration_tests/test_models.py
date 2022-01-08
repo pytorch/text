e2c79e7100e0e25f35050fe4d3546af43d862ec5
nayef211 <n63ahmed@edu.uwaterloo.ca>
2022-01-07 19:01:28 -0800

    Parameterized model tests

--- test/integration_tests/test_models.py
+++ test/integration_tests/test_models.py
@@ -1,127 +1,56 @@
 import torch
-import torchtext
+from parameterized import parameterized
+from torchtext.models import (
+    XLMR_BASE_ENCODER,
+    XLMR_LARGE_ENCODER,
+    ROBERTA_BASE_ENCODER,
+    ROBERTA_LARGE_ENCODER,
+)
 
 from ..common.assets import get_asset_path
 from ..common.torchtext_test_case import TorchtextTestCase
 
+TEST_MODELS_PARAMETERIZED_ARGS = [
+    ("xlmr.base.output.pt", "XLMR base Model Comparison", XLMR_BASE_ENCODER),
+    ("xlmr.large.output.pt", "XLMR base Model Comparison", XLMR_LARGE_ENCODER),
+    (
+        "roberta.base.output.pt",
+        "Roberta base Model Comparison",
+        ROBERTA_BASE_ENCODER,
+    ),
+    (
+        "roberta.large.output.pt",
+        "Roberta base Model Comparison",
+        ROBERTA_LARGE_ENCODER,
+    ),
+]
+
 
 class TestModels(TorchtextTestCase):
-    def test_xlmr_base(self):
-        asset_path = get_asset_path("xlmr.base.output.pt")
-        test_text = "XLMR base Model Comparison"
+    @parameterized.expand(TEST_MODELS_PARAMETERIZED_ARGS)
+    def test_model(self, expected_asset_name, test_text, model_bundler):
+        expected_asset_path = get_asset_path(expected_asset_name)
 
-        xlmr_base = torchtext.models.XLMR_BASE_ENCODER
-        transform = xlmr_base.transform()
-        model = xlmr_base.get_model()
+        transform = model_bundler.transform()
+        model = model_bundler.get_model()
         model = model.eval()
 
         model_input = torch.tensor(transform([test_text]))
         actual = model(model_input)
-        expected = torch.load(asset_path)
+        expected = torch.load(expected_asset_path)
         torch.testing.assert_close(actual, expected)
 
-    def test_xlmr_base_jit(self):
-        asset_path = get_asset_path("xlmr.base.output.pt")
-        test_text = "XLMR base Model Comparison"
+    @parameterized.expand(TEST_MODELS_PARAMETERIZED_ARGS)
+    def test_model_jit(self, expected_asset_name, test_text, model_bundler):
+        expected_asset_path = get_asset_path(expected_asset_name)
 
-        xlmr_base = torchtext.models.XLMR_BASE_ENCODER
-        transform = xlmr_base.transform()
+        transform = model_bundler.transform()
         transform_jit = torch.jit.script(transform)
-        model = xlmr_base.get_model()
+        model = model_bundler.get_model()
         model = model.eval()
         model_jit = torch.jit.script(model)
 
         model_input = torch.tensor(transform_jit([test_text]))
         actual = model_jit(model_input)
-        expected = torch.load(asset_path)
-        torch.testing.assert_close(actual, expected)
-
-    def test_xlmr_large(self):
-        asset_path = get_asset_path("xlmr.large.output.pt")
-        test_text = "XLMR base Model Comparison"
-
-        xlmr_large = torchtext.models.XLMR_LARGE_ENCODER
-        transform = xlmr_large.transform()
-        model = xlmr_large.get_model()
-        model = model.eval()
-
-        model_input = torch.tensor(transform([test_text]))
-        actual = model(model_input)
-        expected = torch.load(asset_path)
-        torch.testing.assert_close(actual, expected)
-
-    def test_xlmr_large_jit(self):
-        asset_path = get_asset_path("xlmr.large.output.pt")
-        test_text = "XLMR base Model Comparison"
-
-        xlmr_large = torchtext.models.XLMR_LARGE_ENCODER
-        transform = xlmr_large.transform()
-        transform_jit = torch.jit.script(transform)
-        model = xlmr_large.get_model()
-        model = model.eval()
-        model_jit = torch.jit.script(model)
-
-        model_input = torch.tensor(transform_jit([test_text]))
-        actual = model_jit(model_input)
-        expected = torch.load(asset_path)
-        torch.testing.assert_close(actual, expected)
-
-    def test_roberta_base(self):
-        asset_path = get_asset_path("roberta.base.output.pt")
-        test_text = "Roberta base Model Comparison"
-
-        roberta_base = torchtext.models.ROBERTA_BASE_ENCODER
-        transform = roberta_base.transform()
-        model = roberta_base.get_model()
-        model = model.eval()
-
-        model_input = torch.tensor(transform([test_text]))
-        actual = model(model_input)
-        expected = torch.load(asset_path)
-        torch.testing.assert_close(actual, expected)
-
-    def test_roberta_base_jit(self):
-        asset_path = get_asset_path("roberta.base.output.pt")
-        test_text = "Roberta base Model Comparison"
-
-        roberta_base = torchtext.models.ROBERTA_BASE_ENCODER
-        transform = roberta_base.transform()
-        transform_jit = torch.jit.script(transform)
-        model = roberta_base.get_model()
-        model = model.eval()
-        model_jit = torch.jit.script(model)
-
-        model_input = torch.tensor(transform_jit([test_text]))
-        actual = model_jit(model_input)
-        expected = torch.load(asset_path)
-        torch.testing.assert_close(actual, expected)
-
-    def test_roberta_large(self):
-        asset_path = get_asset_path("roberta.large.output.pt")
-        test_text = "Roberta base Model Comparison"
-
-        roberta_large = torchtext.models.ROBERTA_LARGE_ENCODER
-        transform = roberta_large.transform()
-        model = roberta_large.get_model()
-        model = model.eval()
-
-        model_input = torch.tensor(transform([test_text]))
-        actual = model(model_input)
-        expected = torch.load(asset_path)
-        torch.testing.assert_close(actual, expected)
-
-    def test_roberta_large_jit(self):
-        asset_path = get_asset_path("roberta.large.output.pt")
-        test_text = "Roberta base Model Comparison"
-
-        roberta_large = torchtext.models.ROBERTA_LARGE_ENCODER
-        transform = roberta_large.transform()
-        transform_jit = torch.jit.script(transform)
-        model = roberta_large.get_model()
-        model = model.eval()
-        model_jit = torch.jit.script(model)
-
-        model_input = torch.tensor(transform_jit([test_text]))
-        actual = model_jit(model_input)
-        expected = torch.load(asset_path)
+        expected = torch.load(expected_asset_path)
         torch.testing.assert_close(actual, expected)
