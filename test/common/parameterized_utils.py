e88d5751635ed813f6934dff87ef465de60b15fd
nayef211 <n63ahmed@edu.uwaterloo.ca>
2022-01-10 13:29:07 -0800

    Remove redundant get asset functions from parameterized_utils

--- test/common/parameterized_utils.py
+++ test/common/parameterized_utils.py
@@ -1,17 +1,10 @@
 import json
+
 from parameterized import param
-import os.path
 
-
-_TEST_DIR_PATH = os.path.realpath(
-    os.path.join(os.path.dirname(__file__), '..'))
-
-
-def get_asset_path(*paths):
-    """Return full path of a test asset"""
-    return os.path.join(_TEST_DIR_PATH, 'asset', *paths)
+from .assets import get_asset_path
 
 
 def load_params(*paths):
-    with open(get_asset_path(*paths), 'r') as file:
+    with open(get_asset_path(*paths), "r") as file:
         return [param(json.loads(line)) for line in file]
