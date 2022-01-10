226d5ca64cdd8e2c511ada9ccae2c9d02a70ecf1
nayef211 <n63ahmed@edu.uwaterloo.ca>
2022-01-10 13:52:15 -0800

    Added nested_params helper method. Updated model integration test to parameterize a single method covering jit and non-jit tests

--- test/integration_tests/test_models.py
+++ test/integration_tests/test_models.py
@@ -1,5 +1,4 @@
 import torch
-from parameterized import parameterized
 from torchtext.models import (
     XLMR_BASE_ENCODER,
     XLMR_LARGE_ENCODER,
@@ -8,49 +7,42 @@ from torchtext.models import (
 )
 
 from ..common.assets import get_asset_path
+from ..common.parameterized_utils import nested_params
 from ..common.torchtext_test_case import TorchtextTestCase
-
-TEST_MODELS_PARAMETERIZED_ARGS = [
-    ("xlmr.base.output.pt", "XLMR base Model Comparison", XLMR_BASE_ENCODER),
-    ("xlmr.large.output.pt", "XLMR base Model Comparison", XLMR_LARGE_ENCODER),
-    (
-        "roberta.base.output.pt",
-        "Roberta base Model Comparison",
-        ROBERTA_BASE_ENCODER,
-    ),
-    (
-        "roberta.large.output.pt",
-        "Roberta base Model Comparison",
-        ROBERTA_LARGE_ENCODER,
-    ),
-]
 
 
 class TestModels(TorchtextTestCase):
-    @parameterized.expand(TEST_MODELS_PARAMETERIZED_ARGS)
-    def test_model(self, expected_asset_name, test_text, model_bundler):
+    @nested_params(
+        [
+            ("xlmr.base.output.pt", "XLMR base Model Comparison", XLMR_BASE_ENCODER),
+            ("xlmr.large.output.pt", "XLMR base Model Comparison", XLMR_LARGE_ENCODER),
+            (
+                "roberta.base.output.pt",
+                "Roberta base Model Comparison",
+                ROBERTA_BASE_ENCODER,
+            ),
+            (
+                "roberta.large.output.pt",
+                "Roberta base Model Comparison",
+                ROBERTA_LARGE_ENCODER,
+            ),
+        ],
+        [True, False],
+    )
+    def test_model(self, model_args, is_jit):
+        expected_asset_name, test_text, model_bundler = model_args
+
         expected_asset_path = get_asset_path(expected_asset_name)
 
         transform = model_bundler.transform()
         model = model_bundler.get_model()
         model = model.eval()
+
+        if is_jit:
+            transform = torch.jit.script(transform)
+            model = torch.jit.script(model)
 
         model_input = torch.tensor(transform([test_text]))
         actual = model(model_input)
-        expected = torch.load(expected_asset_path)
-        torch.testing.assert_close(actual, expected)
-
-    @parameterized.expand(TEST_MODELS_PARAMETERIZED_ARGS)
-    def test_model_jit(self, expected_asset_name, test_text, model_bundler):
-        expected_asset_path = get_asset_path(expected_asset_name)
-
-        transform = model_bundler.transform()
-        transform_jit = torch.jit.script(transform)
-        model = model_bundler.get_model()
-        model = model.eval()
-        model_jit = torch.jit.script(model)
-
-        model_input = torch.tensor(transform_jit([test_text]))
-        actual = model_jit(model_input)
         expected = torch.load(expected_asset_path)
         torch.testing.assert_close(actual, expected)
