e56eafb584cd66027e634c45c10b389a821eac54
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2022-01-10 10:29:08 -0500

    remove comments in code

--- torchtext/datasets/amazonreviewpolarity.py
+++ torchtext/datasets/amazonreviewpolarity.py
@@ -40,20 +40,11 @@ def AmazonReviewPolarity(root, split):
         raise ModuleNotFoundError("Package `torchdata` not found. Please install following instructions at `https://github.com/pytorch/data`")
 
     url_dp = IterableWrapper([URL])
-
-    # cache data on-disk with sanity check
     cache_dp = url_dp.on_disk_cache(
         filepath_fn=lambda x: os.path.join(root, _PATH), hash_dict={os.path.join(root, _PATH): MD5}, hash_type="md5"
     )
     cache_dp = GDriveReader(cache_dp).end_caching(mode="wb", same_filepath_fn=True)
-
     cache_dp = FileOpener(cache_dp, mode="b")
-
-    # stack TAR extractor on top of loader DP
     extracted_files = cache_dp.read_from_tar()
-
-    # filter files as necessary
     filter_extracted_files = extracted_files.filter(lambda x: split in x[0])
-
-    # stack CSV reader and do some mapping
     return filter_extracted_files.parse_csv().map(fn=lambda t: (int(t[0]), ' '.join(t[1:])))
