f99609d7c56d8b742f6b0f281fcd726d05aa4923
Elijah Rippeth <elijah.rippeth@gmail.com>
2022-01-12 14:21:41 -0500

    migrate YahooAnswers to datapipes. (#1508)
    * add initial pass at migrating YahooAnswersto datapipes.
    
    * fix flake.

--- torchtext/datasets/yahooanswers.py
+++ torchtext/datasets/yahooanswers.py
@@ -1,10 +1,15 @@
-from torchtext.utils import download_from_url, extract_archive
-from torchtext.data.datasets_utils import _RawTextIterableDataset
-from torchtext.data.datasets_utils import _wrap_split_argument
-from torchtext.data.datasets_utils import _add_docstring_header
-from torchtext.data.datasets_utils import _find_match
-from torchtext.data.datasets_utils import _create_dataset_directory
-from torchtext.data.datasets_utils import _create_data_from_csv
+from torchtext._internal.module_utils import is_module_available
+from typing import Union, Tuple
+
+if is_module_available("torchdata"):
+    from torchdata.datapipes.iter import FileOpener, GDriveReader, IterableWrapper
+
+from torchtext.data.datasets_utils import (
+    _wrap_split_argument,
+    _add_docstring_header,
+    _create_dataset_directory,
+)
+
 import os
 
 URL = 'https://drive.google.com/uc?export=download&id=0Bz8a_Dbh9Qhbd2JNdDBsQUdocVU'
@@ -20,16 +25,30 @@ _PATH = 'yahoo_answers_csv.tar.gz'
 
 DATASET_NAME = "YahooAnswers"
 
+_EXTRACTED_FILES = {
+    'train': os.path.join('yahoo_answers_csv', 'train.csv'),
+    'test': os.path.join('yahoo_answers_csv', 'test.csv'),
+}
+
 
 @_add_docstring_header(num_lines=NUM_LINES, num_classes=10)
 @_create_dataset_directory(dataset_name=DATASET_NAME)
 @_wrap_split_argument(('train', 'test'))
-def YahooAnswers(root, split):
-    dataset_tar = download_from_url(URL, root=root,
-                                    path=os.path.join(root, _PATH),
-                                    hash_value=MD5, hash_type='md5')
-    extracted_files = extract_archive(dataset_tar)
+def YahooAnswers(root: str, split: Union[Tuple[str], str]):
+    if not is_module_available("torchdata"):
+        raise ModuleNotFoundError("Package `torchdata` not found. Please install following instructions at `https://github.com/pytorch/data`")
 
-    path = _find_match(split + '.csv', extracted_files)
-    return _RawTextIterableDataset(DATASET_NAME, NUM_LINES[split],
-                                   _create_data_from_csv(path))
+    url_dp = IterableWrapper([URL])
+
+    cache_dp = url_dp.on_disk_cache(
+        filepath_fn=lambda x: os.path.join(root, _PATH),
+        hash_dict={os.path.join(root, _PATH): MD5}, hash_type="md5"
+    )
+    cache_dp = GDriveReader(cache_dp).end_caching(mode="wb", same_filepath_fn=True)
+    cache_dp = FileOpener(cache_dp, mode="b")
+
+    extracted_files = cache_dp.read_from_tar()
+
+    filter_extracted_files = extracted_files.filter(lambda x: _EXTRACTED_FILES[split] in x[0])
+
+    return filter_extracted_files.parse_csv().map(fn=lambda t: (int(t[0]), " ".join(t[1:])))
