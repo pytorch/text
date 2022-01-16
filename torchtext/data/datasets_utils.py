a5ca19407b844e49679d87c94003e08c5efd6d78
Elijah Rippeth <elijah.rippeth@gmail.com>
2022-01-15 19:24:31 -0500

    migrate SQUAD1 to datapipes. (#1513)

--- torchtext/data/datasets_utils.py
+++ torchtext/data/datasets_utils.py
@@ -10,6 +10,7 @@ from torchtext.utils import (
     extract_archive,
     unicode_csv_reader,
 )
+from torch.utils.data import IterDataPipe, functional_datapipe
 import codecs
 try:
     import defusedxml.ElementTree as ET
@@ -318,3 +319,26 @@ class _RawTextIterableDataset(torch.utils.data.IterableDataset):
 
     def __str__(self):
         return self.description
+
+
+@functional_datapipe("read_squad")
+class _ParseSQuADQAData(IterDataPipe):
+    r"""Iterable DataPipe to parse the contents of a stream of JSON objects
+    as provided by SQuAD QA. Used in SQuAD1 and SQuAD2.
+    """
+    def __init__(self, source_datapipe) -> None:
+        self.source_datapipe = source_datapipe
+
+    def __iter__(self):
+        for _, stream in self.source_datapipe:
+            raw_json_data = stream["data"]
+            for layer1 in raw_json_data:
+                for layer2 in layer1["paragraphs"]:
+                    for layer3 in layer2["qas"]:
+                        _context, _question = layer2["context"], layer3["question"]
+                        _answers = [item["text"] for item in layer3["answers"]]
+                        _answer_start = [item["answer_start"] for item in layer3["answers"]]
+                        if len(_answers) == 0:
+                            _answers = [""]
+                            _answer_start = [-1]
+                        yield _context, _question, _answers, _answer_start
