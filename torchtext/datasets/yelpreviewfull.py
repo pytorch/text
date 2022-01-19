ad4bb57b474206462a22502b33617b604e2d3e80
Elijah Rippeth <elijah.rippeth@gmail.com>
2022-01-19 14:43:29 -0500

    rename dps for consistency.

--- torchtext/datasets/yelpreviewfull.py
+++ torchtext/datasets/yelpreviewfull.py
@@ -40,15 +40,15 @@ def YelpReviewFull(root: str, split: Union[Tuple[str], str]):
 
     url_dp = IterableWrapper([URL])
 
-    cache_dp = url_dp.on_disk_cache(
+    cache_compressed_dp = url_dp.on_disk_cache(
         filepath_fn=lambda x: os.path.join(root, _PATH),
         hash_dict={os.path.join(root, _PATH): MD5}, hash_type="md5"
     )
-    cache_dp = GDriveReader(cache_dp).end_caching(mode="wb", same_filepath_fn=True)
-    cache_dp = FileOpener(cache_dp, mode="b")
+    cache_compressed_dp = GDriveReader(cache_compressed_dp).end_caching(mode="wb", same_filepath_fn=True)
+    cache_compressed_dp = FileOpener(cache_compressed_dp, mode="b")
 
-    cache_dp = cache_dp.on_disk_cache(filepath_fn=lambda x: os.path.join(root, _EXTRACTED_FILES[split]))
-    cache_dp = cache_dp.read_from_tar().filter(lambda x: _EXTRACTED_FILES[split] in x[0])
-    cache_dp = cache_dp.end_caching(mode="wb", same_filepath_fn=True)
-    cache_dp = FileOpener(cache_dp, mode="b")
-    return cache_dp.parse_csv().map(fn=lambda t: (int(t[0]), " ".join(t[1:])))
+    cache_decompressed_dp = cache_compressed_dp.on_disk_cache(filepath_fn=lambda x: os.path.join(root, _EXTRACTED_FILES[split]))
+    cache_decompressed_dp = cache_decompressed_dp.read_from_tar().filter(lambda x: _EXTRACTED_FILES[split] in x[0])
+    cache_decompressed_dp = cache_decompressed_dp.end_caching(mode="wb", same_filepath_fn=True)
+    data_dp = FileOpener(cache_decompressed_dp, mode="b")
+    return data_dp.parse_csv().map(fn=lambda t: (int(t[0]), " ".join(t[1:])))
