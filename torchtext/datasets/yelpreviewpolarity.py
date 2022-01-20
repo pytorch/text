68feea622c7bd1009069b5160c00a6460450c412
Elijah Rippeth <elijah.rippeth@gmail.com>
2022-01-20 12:08:09 -0500

    add FileOpener within caching block for more consistency.

--- torchtext/datasets/yelpreviewpolarity.py
+++ torchtext/datasets/yelpreviewpolarity.py
@@ -51,6 +51,8 @@ def YelpReviewPolarity(root: str, split: Union[Tuple[str], str]):
     cache_decompressed_dp = cache_compressed_dp.on_disk_cache(
         filepath_fn=lambda x: os.path.join(root, _EXTRACTED_FILES[split])
     )
+    cache_decompressed_dp = FileOpener(cache_decompressed_dp, mode="b")
+
     cache_decompressed_dp = cache_decompressed_dp.read_from_tar()
 
     cache_decompressed_dp = cache_decompressed_dp.filter(lambda x: _EXTRACTED_FILES[split] in x[0])
