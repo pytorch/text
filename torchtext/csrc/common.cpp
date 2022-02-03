88ed9547331f73e960f129b8c3d7b6587a6d8389
Abhinav Arora <abhinavarora@fb.com>
2022-02-03 14:36:42 -0800

    Add clang-format changes

--- torchtext/csrc/common.cpp
+++ torchtext/csrc/common.cpp
@@ -8,11 +8,16 @@
 namespace torchtext {
 namespace impl {
 
-int64_t divup(int64_t x, int64_t y) { return (x + y - 1) / y; }
+int64_t divup(int64_t x, int64_t y) {
+  return (x + y - 1) / y;
+}
 
-void infer_offsets(const std::string &file_path, int64_t num_lines,
-                   int64_t chunk_size, std::vector<size_t> &offsets,
-                   int64_t num_header_lines) {
+void infer_offsets(
+    const std::string& file_path,
+    int64_t num_lines,
+    int64_t chunk_size,
+    std::vector<size_t>& offsets,
+    int64_t num_header_lines) {
   std::ifstream fin;
   fin.open(file_path, std::ios::in);
 
