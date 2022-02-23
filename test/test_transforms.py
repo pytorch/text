170b74aa3c96fb0a0d6e66418cec1984bf4a674d
Abhinav Arora <abhinavarora@fb.com>
2022-02-22 19:34:31 -0800

    Modify CLIPTokenizer to either infer number of merges from encoder json or take it in constructor (#1622) (#1626)

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -294,13 +294,22 @@ class TestGPT2BPETokenizer(TorchtextTestCase):
 
 
 class TestCLIPTokenizer(TorchtextTestCase):
-    def _load_tokenizer(self, test_scripting):
+    def _load_tokenizer(self, init_using_merge_only: bool, test_scripting: bool):
         encoder_json = "clip_encoder.json"
         bpe_vocab = "clip_vocab.bpe"
-        tokenizer = transforms.CLIPTokenizer(
-            encoder_json_path=get_asset_path(encoder_json),
-            vocab_bpe_path=get_asset_path(bpe_vocab),
-        )
+        num_merges = (
+            49152 - 256 - 2
+        )  # https://github.com/mlfoundations/open_clip/blob/57b3e8ea6ad6bfc2974203945f8fd577e0659468/src/clip/tokenizer.py#L67
+        if init_using_merge_only:
+            tokenizer = transforms.CLIPTokenizer(
+                merges_path=get_asset_path(bpe_vocab),
+                num_merges=num_merges,
+            )
+        else:
+            tokenizer = transforms.CLIPTokenizer(
+                encoder_json_path=get_asset_path(encoder_json),
+                merges_path=get_asset_path(bpe_vocab),
+            )
         if test_scripting:
             tokenizer = torch.jit.script(tokenizer)
         return tokenizer
@@ -308,12 +317,67 @@ class TestCLIPTokenizer(TorchtextTestCase):
     def _clip_tokenizer(self, tokenizer):
         sample_texts = [
             "Hello World!, how are you?",
-            "<|startoftext|> the quick brown fox jumped over the lazy dog <|endoftext|>"
+            "<|startoftext|> the quick brown fox jumped over the lazy dog <|endoftext|>",
+            "Awaiting their due award... Photo by Frederick (FN) Noronha. Copyleft. Creative Commons 3.0. Non-commercial. Attribution. May be copied for non-commercial purposes. For other purposes, contact fn at goa-india.org",
         ]
 
         expected_token_ids = [
-            ['3306', '1002', '29325', '829', '631', '592', '286'],
-            ['49406', '518', '3712', '2866', '3240', '16901', '962', '518', '10753', '1929', '49407'],
+            ["3306", "1002", "29325", "829", "631", "592", "286"],
+            ["49406", "518", "3712", "2866", "3240", "16901", "962", "518", "10753", "1929", "49407"],
+            [
+                "14872",
+                "911",
+                "2887",
+                "2047",
+                "678",
+                "1125",
+                "638",
+                "18570",
+                "263",
+                "21763",
+                "264",
+                "1062",
+                "521",
+                "1429",
+                "269",
+                "11376",
+                "1823",
+                "269",
+                "4450",
+                "16653",
+                "274",
+                "269",
+                "271",
+                "269",
+                "3353",
+                "268",
+                "6287",
+                "269",
+                "24624",
+                "740",
+                "269",
+                "1270",
+                "655",
+                "36770",
+                "556",
+                "3353",
+                "268",
+                "6287",
+                "22020",
+                "269",
+                "556",
+                "1010",
+                "22020",
+                "267",
+                "3523",
+                "21763",
+                "536",
+                "14399",
+                "268",
+                "1762",
+                "269",
+                "5593",
+            ],
         ]
 
         # test batch of sentences
@@ -325,22 +389,24 @@ class TestCLIPTokenizer(TorchtextTestCase):
 
     def test_clip_tokenizer(self):
         """test tokenization on single sentence input as well as batch on sentences"""
-        self._clip_tokenizer(self._load_tokenizer(test_scripting=False))
+        self._clip_tokenizer(self._load_tokenizer(init_using_merge_only=True, test_scripting=False))
+        self._clip_tokenizer(self._load_tokenizer(init_using_merge_only=False, test_scripting=False))
 
     def test_clip_tokenizer_jit(self):
         """test tokenization with scripting on single sentence input as well as batch on sentences"""
-        self._clip_tokenizer(self._load_tokenizer(test_scripting=True))
+        self._clip_tokenizer(self._load_tokenizer(init_using_merge_only=True, test_scripting=True))
+        self._clip_tokenizer(self._load_tokenizer(init_using_merge_only=False, test_scripting=True))
 
     def test_clip_tokenizer_save_load_pybind(self):
-        tokenizer = self._load_tokenizer(test_scripting=False)
-        tokenizer_path = os.path.join(self.test_dir, 'gpt2_tokenizer_pybind.pt')
+        tokenizer = self._load_tokenizer(init_using_merge_only=True, test_scripting=False)
+        tokenizer_path = os.path.join(self.test_dir, "gpt2_tokenizer_pybind.pt")
         torch.save(tokenizer, tokenizer_path)
         loaded_tokenizer = torch.load(tokenizer_path)
         self._clip_tokenizer((loaded_tokenizer))
 
     def test_clip_tokenizer_save_load_torchscript(self):
-        tokenizer = self._load_tokenizer(test_scripting=False)
-        tokenizer_path = os.path.join(self.test_dir, 'gpt2_tokenizer_torchscript.pt')
+        tokenizer = self._load_tokenizer(init_using_merge_only=True, test_scripting=False)
+        tokenizer_path = os.path.join(self.test_dir, "gpt2_tokenizer_torchscript.pt")
         # Call the __prepare_scriptable__() func and convert the building block to the torbhind version
         # Not expect users to use the torchbind version on eager mode but still need a CI test here.
         torch.save(tokenizer.__prepare_scriptable__(), tokenizer_path)
