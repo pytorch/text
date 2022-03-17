821dec2c1ae446504cc17fe9c37ec699a4784179
Virgile Mison <virgilem@fb.com>
2022-03-17 12:58:09 -0400

    Update functional/modules to avoid code repetition

--- test/data/test_modules.py
+++ test/data/test_modules.py
@@ -166,14 +166,6 @@ class TestModels(TorchtextTestCase):
         assert list(sdp_attn_weights.size()) == [3, 3, bsz * nhead, tgt_len, embed_dim]
         self.assertEqual(sdp_attn_output[2][2], sdp_attn_output_full)
         self.assertEqual(sdp_attn_weights[2][2], sdp_attn_weights_full)
-        # dim -2 is not equal to neither key/value's dim -2 or 1
-        with self.assertRaises(RuntimeError):
-            SDP(
-                query.expand(tgt_len, 2, embed_dim),
-                key.expand(3, 3, src_len, bsz * nhead, embed_dim),
-                value.expand(3, 3, src_len, bsz * nhead, embed_dim),
-                attn_mask=attn_mask_2D.expand(bsz * nhead, tgt_len, src_len),
-            )
 
         # key/value have a size of (src_len, 1, embed_dim)
         # while query has a size of (1, 2, 3, tgt_len, bsz * nhead, embed_dim)
@@ -187,40 +179,6 @@ class TestModels(TorchtextTestCase):
         assert list(sdp_attn_weights.size()) == [1, 2, 3, bsz * nhead, tgt_len, embed_dim]
         self.assertEqual(sdp_attn_output[0][1][2], sdp_attn_output_full)
         self.assertEqual(sdp_attn_weights[0][1][2], sdp_attn_weights_full)
-        # key dim -2 is not equal to value dim -2
-        with self.assertRaisesRegex(AssertionError, "Shape of key, value must match"):
-            SDP(
-                query.expand(1, 2, 3, tgt_len, bsz * nhead, embed_dim),
-                key.expand(src_len, 2, embed_dim),
-                value.expand(src_len, 1, embed_dim),
-                attn_mask=attn_mask_2D.expand(bsz * nhead, tgt_len, src_len),
-            )
-        # key/value dim -2 is not equal to neither query's dim -2 or 1
-        with self.assertRaises(RuntimeError):
-            SDP(
-                query.expand(1, 2, 3, tgt_len, bsz * nhead, embed_dim),
-                key.expand(src_len, 2, embed_dim),
-                value.expand(src_len, 2, embed_dim),
-                attn_mask=attn_mask_2D.expand(bsz * nhead, tgt_len, src_len),
-            )
-        # attn_mask must be a 3D tensor.
-        with self.assertRaises(RuntimeError):
-            SDP(
-                query.expand(1, 2, 3, tgt_len, bsz * nhead, embed_dim),
-                key.expand(src_len, 2, embed_dim),
-                value.expand(src_len, 2, embed_dim),
-                attn_mask=attn_mask_2D,
-            )
-
-        # Only bool tensor is supported for attn_mask
-        attn_mask_2D_not_bool = torch.randint(0, 2, (tgt_len, src_len))
-        with self.assertRaises(RuntimeError):
-            SDP(
-                query.expand(1, 2, 3, tgt_len, bsz * nhead, embed_dim),
-                key.expand(src_len, 2, embed_dim),
-                value.expand(src_len, 2, embed_dim),
-                attn_mask=attn_mask_2D_not_bool,
-            )
 
         # attn_mask in a size of (1, tgt_len, src_len)
         # 2D tensor is not supported for attn_mask
@@ -232,14 +190,74 @@ class TestModels(TorchtextTestCase):
         )
         self.assertEqual(sdp_attn_output, sdp_attn_output_full)
         self.assertEqual(sdp_attn_weights, sdp_attn_weights_full)
-        # attn_mask's dim -3 is not equal to neither batch size or 1
-        with self.assertRaisesRegex(RuntimeError, "The size of the attn_mask is not correct."):
-            SDP(
-                query.expand(tgt_len, bsz * nhead, embed_dim),
-                key.expand(src_len, bsz * nhead, embed_dim),
-                value.expand(src_len, bsz * nhead, embed_dim),
-                attn_mask=attn_mask_2D.expand(2, tgt_len, src_len),
-            )
+
+    def test_assert_raises(self):
+        embed_dim, nhead, tgt_len, src_len, bsz = 10, 5, 6, 10, 64
+        SDP = ScaledDotProduct()
+        query = torch.rand((tgt_len, 1, embed_dim))
+        key = value = torch.rand((src_len, 1, embed_dim))
+        attn_mask_2D = torch.randint(0, 2, (tgt_len, src_len))
+
+        configs = [
+            # case: dim -2 is not equal to neither key/value's dim -2 or 1
+            [
+                RuntimeError, "",
+                (tgt_len, 2, embed_dim),
+                (3, 3, src_len, bsz * nhead, embed_dim),
+                (3, 3, src_len, bsz * nhead, embed_dim),
+                (bsz * nhead, tgt_len, src_len), True
+            ],
+            # case: attn_mask's dim -3 is not equal to neither batch size or 1
+            [
+                RuntimeError, "The size of the attn_mask is not correct.",
+                (tgt_len, bsz * nhead, embed_dim),
+                (src_len, bsz * nhead, embed_dim),
+                (src_len, bsz * nhead, embed_dim),
+                (2, tgt_len, src_len), True
+            ],
+            # case: key dim -2 is not equal to value dim -2
+            [
+                AssertionError, "Shape of key, value must match",
+                (1, 2, 3, tgt_len, bsz * nhead, embed_dim),
+                (src_len, 2, embed_dim),
+                (src_len, 1, embed_dim),
+                (bsz * nhead, tgt_len, src_len), True
+            ],
+            # kcase: ey/value dim -2 is not equal to neither query's dim -2 or 1
+            [
+                RuntimeError, "",
+                (1, 2, 3, tgt_len, bsz * nhead, embed_dim),
+                (src_len, 2, embed_dim),
+                (src_len, 2, embed_dim),
+                (bsz * nhead, tgt_len, src_len), True
+            ],
+            # case: attn_mask must be a 3D tensor.
+            [
+                RuntimeError, "",
+                (1, 2, 3, tgt_len, bsz * nhead, embed_dim),
+                (src_len, 2, embed_dim),
+                (src_len, 2, embed_dim),
+                (), True
+            ],
+            # case: only bool tensor is supported for attn_mask
+            [
+                RuntimeError, "",
+                (1, 2, 3, tgt_len, bsz * nhead, embed_dim),
+                (src_len, 2, embed_dim),
+                (src_len, 2, embed_dim),
+                (), False
+            ],
+
+        ]
+        for (error_type, error_regex, query_size, key_size, value_size, attn_mask_size, att_masc_bool) in configs:
+            with self.assertRaisesRegex(error_type, error_regex) if error_regex else self.assertRaises(error_type):
+                attn_mask = attn_mask_2D.to(torch.bool) if att_masc_bool else attn_mask_2D
+                SDP(
+                    query.expand(*query_size),
+                    key.expand(*key_size),
+                    value.expand(*value_size),
+                    attn_mask=attn_mask.expand(*attn_mask_size) if attn_mask_size else attn_mask
+                )
 
     def test_sdp_batch_first(self):
         embed_dim, nhead, tgt_len, src_len, bsz = 10, 5, 6, 10, 64
