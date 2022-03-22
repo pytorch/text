f8ce695394a7e4f99e87cc580fcb8c880fc05eb4
VirgileHlav <virgilem@fb.com>
2022-03-22 10:08:08 -0400

    Add more test coverage (#1653)
    Add more tests to modules, models and functional files.

--- test/test_functional.py
+++ test/test_functional.py
@@ -1,90 +1,78 @@
 import torch
 from torchtext import functional
+from .common.parameterized_utils import nested_params
 
 from .common.torchtext_test_case import TorchtextTestCase
 
-
 class TestFunctional(TorchtextTestCase):
-    def _to_tensor(self, test_scripting):
-        input = [[1, 2], [1, 2, 3]]
-        padding_value = 0
 
+    @nested_params(
+        [True, False],
+        [
+            [[[1, 2], [1, 2, 3]], 0, [[1, 2, 0], [1, 2, 3]]],
+            [[[1, 2], [1, 2, 3]], 1, [[1, 2, 1], [1, 2, 3]]],
+            [[1, 2], 0, [1, 2]],
+        ]
+    )
+    def test_to_tensor(self, test_scripting, configs):
+        """test tensorization on both single sequence and batch of sequence"""
+        inputs, padding_value, expected_list = configs
         func = functional.to_tensor
         if test_scripting:
             func = torch.jit.script(func)
-        actual = func(input, padding_value=padding_value)
-        expected = torch.tensor([[1, 2, 0], [1, 2, 3]], dtype=torch.long)
+        else:
+            with self.assertRaises(TypeError):
+                func("test")
+
+        actual = func(inputs, padding_value=padding_value)
+        expected = torch.tensor(expected_list, dtype=torch.long)
         torch.testing.assert_close(actual, expected)
 
-        input = [1, 2]
-        actual = func(input, padding_value=padding_value)
-        expected = torch.tensor([1, 2], dtype=torch.long)
-        torch.testing.assert_close(actual, expected)
-
-    def test_to_tensor(self):
-        """test tensorization on both single sequence and batch of sequence"""
-        self._to_tensor(test_scripting=False)
-
-    def test_to_tensor_jit(self):
-        """test tensorization with scripting on both single sequence and batch of sequence"""
-        self._to_tensor(test_scripting=True)
-
-    def _truncate(self, test_scripting):
+    @nested_params(
+        [True, False],
+        [
+            [[[1, 2], [1, 2, 3]], [[1, 2], [1, 2]]],
+            [[1, 2, 3], [1, 2]],
+            [[["a", "b"], ["a", "b", "c"]], [["a", "b"], ["a", "b"]]],
+            [["a", "b", "c"], ["a", "b"]],
+        ]
+    )
+    def test_truncate(self, test_scripting, configs):
+        """test truncation on both sequence and batch of sequence with both str and int types"""
+        inputs, expected = configs
         max_seq_len = 2
         func = functional.truncate
         if test_scripting:
             func = torch.jit.script(func)
+        else:
+            with self.assertRaises(TypeError):
+                func("test", max_seq_len=max_seq_len)
 
-        input = [[1, 2], [1, 2, 3]]
-        actual = func(input, max_seq_len=max_seq_len)
-        expected = [[1, 2], [1, 2]]
+        actual = func(inputs, max_seq_len=max_seq_len)
         self.assertEqual(actual, expected)
 
-        input = [1, 2, 3]
-        actual = func(input, max_seq_len=max_seq_len)
-        expected = [1, 2]
-        self.assertEqual(actual, expected)
-
-        input = [["a", "b"], ["a", "b", "c"]]
-        actual = func(input, max_seq_len=max_seq_len)
-        expected = [["a", "b"], ["a", "b"]]
-        self.assertEqual(actual, expected)
-
-        input = ["a", "b", "c"]
-        actual = func(input, max_seq_len=max_seq_len)
-        expected = ["a", "b"]
-        self.assertEqual(actual, expected)
-
-    def test_truncate(self):
-        """test truncation on both sequence and batch of sequence with both str and int types"""
-        self._truncate(test_scripting=False)
-
-    def test_truncate_jit(self):
-        """test truncation with scripting on both sequence and batch of sequence with both str and int types"""
-        self._truncate(test_scripting=True)
-
-    def _add_token(self, test_scripting):
-
+    @nested_params(
+        [True, False],
+        [
+            # case: List[List[int]]
+            [[[1, 2], [1, 2, 3]], 0, [[0, 1, 2], [0, 1, 2, 3]], True],
+            [[[1, 2], [1, 2, 3]], 0, [[1, 2, 0], [1, 2, 3, 0]], False],
+            # case: List[int]
+            [[1, 2], 0, [0, 1, 2], True],
+            [[1, 2], 0, [1, 2, 0], False],
+            # case: List[List[str]]
+            [[["a", "b"], ["c", "d"]], "x", [["x", "a", "b"], ["x", "c", "d"]], True],
+            [[["a", "b"], ["c", "d"]], "x", [["a", "b", "x"], ["c", "d", "x"]], False],
+            # case: List[str]
+            [["a", "b"], "x", ["x", "a", "b"], True],
+            [["a", "b"], "x", ["a", "b", "x"], False],
+        ]
+    )
+    def test_add_token(self, test_scripting, configs):
+        inputs, token_id, expected, begin = configs
         func = functional.add_token
         if test_scripting:
             func = torch.jit.script(func)
-        input = [[1, 2], [1, 2, 3]]
-        token_id = 0
-        actual = func(input, token_id=token_id)
-        expected = [[0, 1, 2], [0, 1, 2, 3]]
+
+        actual = func(inputs, token_id=token_id, begin=begin)
         self.assertEqual(actual, expected)
-
-        actual = func(input, token_id=token_id, begin=False)
-        expected = [[1, 2, 0], [1, 2, 3, 0]]
-        self.assertEqual(actual, expected)
-
-        input = [1, 2]
-        actual = func(input, token_id=token_id, begin=False)
-        expected = [1, 2, 0]
-        self.assertEqual(actual, expected)
-
-    def test_add_token(self):
-        self._add_token(test_scripting=False)
-
-    def test_add_token_jit(self):
-        self._add_token(test_scripting=True)
