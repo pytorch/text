0cc26685deb283eeffa3486bf3beb8b40b00147b
Nayef Ahmed <nayef211@fb.com>
2022-04-01 12:03:52 -0400

    Added test for GPT2BPE udf from torcharrow

--- test/test_torcharrow_udfs.py
+++ test/test_torcharrow_udfs.py
@@ -6,30 +6,53 @@
 
 import unittest
 
-import numpy as np
+import json
 import torcharrow as ta
 import torcharrow.dtypes as dt
 from torcharrow import functional
-# import torchtext.transforms as T
-import torcharrow._torchtext
+import torchtext.transforms as T
+import torcharrow._torcharrow as _ta
+from torchtext.utils import get_asset_local_path
+from .common.assets import get_asset_path
+
+
+# copied from GPT2BPETokenizer __init__ method
+def init_bpe_encoder():
+    encoder_json_path = get_asset_path("gpt2_bpe_encoder.json")
+    vocab_bpe_path = get_asset_path("gpt2_bpe_vocab.bpe")
+    _seperator = "\u0001"
+
+    # load bpe encoder and bpe decoder
+    with open(get_asset_local_path(encoder_json_path), "r", encoding="utf-8") as f:
+        bpe_encoder = json.load(f)
+    # load bpe vocab
+    with open(get_asset_local_path(vocab_bpe_path), "r", encoding="utf-8") as f:
+        bpe_vocab = f.read()
+    bpe_merge_ranks = {
+        _seperator.join(merge_pair.split()): i for i, merge_pair in enumerate(bpe_vocab.split("\n")[1:-1])
+    }
+    # Caching is enabled in Eager mode
+    bpe = _ta.GPT2BPEEncoder(bpe_encoder, bpe_merge_ranks, _seperator, T.bytes_to_unicode(), True)
+    return bpe
 
 
 class _TestFunctionalBase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        encoder_json_path = "https://download.pytorch.org/models/text/gpt2_bpe_encoder.json"
-        vocab_bpe_path = "https://download.pytorch.org/models/text/gpt2_bpe_vocab.bpe"
-        cls.tokenizer = functional.GPT2BPETokenizer(encoder_json_path, vocab_bpe_path)
-        
-        cls.base_df1 = ta.dataframe(
+
+        cls.tokenizer = init_bpe_encoder()
+
+        cls.base_df = ta.dataframe(
             {
-                "text": ["hello world!", "how do you do?"],
-                "labels": [0, 1]
+                "text": ["Hello World!, how are you?", "Respublica superiorem"],
+                "labels": [0, 1],
+                "tokens": [["15496", "2159", "28265", "703", "389", "345", "30"], ["4965", "11377", "64", "2208", "72", "29625"]],
             },
             dtype=dt.Struct(
                 fields=[
                     dt.Field("text", dt.List(dt.string)),
-                    dt.Field("a", dt.int32),
+                    dt.Field("labels", dt.int32),
+                    dt.Field("tokens", dt.List(dt.string)),
                 ]
             ),
         )
@@ -43,18 +66,14 @@ class _TestFunctionalBase(unittest.TestCase):
         raise unittest.SkipTest("abstract base test")
 
     def test_bpe_encode(self):
-        df = type(self).df_int_float
-
-        buckets = [2.0, 5.0, 10.0]
-        self.assertEqual(
-            list(functional.bucketize(df["a"], buckets)), [0, 0, 1, 1, 2, 2, 3]
-        )
+        out_df = functional.bpe_tokenize(self.tokenizer, self.df["text"])
+        self.assertEqual(out_df, self.df["tokens"])
 
 
 class TestFunctionalCpu(_TestFunctionalBase):
     @classmethod
     def setUpTestCaseData(cls):
-        cls.df1 = cls.base_df2.copy()
+        cls.df = cls.base_df.copy()
 
 
 if __name__ == "__main__":
