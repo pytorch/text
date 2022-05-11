4b4d50bd101e265962d8e2551567a0f506433325
Kevin Tse <NivekT@users.noreply.github.com>
2022-05-11 12:40:01 -0400

    Updating dataset code to avoid creating multiple iterators from a DataPipe (#1708)

--- torchtext/datasets/iwslt2017.py
+++ torchtext/datasets/iwslt2017.py
@@ -104,7 +104,7 @@ DATASET_NAME = "IWSLT2017"
 # avoid additional conditional imports.
 def _filter_clean_cache(cache_decompressed_dp, full_filepath, uncleaned_filename):
     cache_inner_decompressed_dp = cache_decompressed_dp.on_disk_cache(filepath_fn=lambda x: full_filepath)
-    cache_inner_decompressed_dp = FileOpener(cache_inner_decompressed_dp, mode="b").load_from_tar()
+    cache_inner_decompressed_dp = cache_inner_decompressed_dp.open_files(mode="b").load_from_tar()
     cache_inner_decompressed_dp = cache_inner_decompressed_dp.filter(
         lambda x: os.path.basename(uncleaned_filename) in x[0]
     )
@@ -208,8 +208,9 @@ def IWSLT2017(root=".data", split=("train", "valid", "test"), language_pair=("de
     )
 
     cache_decompressed_dp = cache_compressed_dp.on_disk_cache(filepath_fn=lambda x: inner_iwslt_tar)
-    cache_decompressed_dp = FileOpener(cache_decompressed_dp, mode="b").load_from_tar()
+    cache_decompressed_dp = cache_decompressed_dp.open_files(mode="b").load_from_tar()
     cache_decompressed_dp = cache_decompressed_dp.end_caching(mode="wb", same_filepath_fn=True)
+    cache_decompressed_dp_1, cache_decompressed_dp_2 = cache_decompressed_dp.fork(num_instances=2)
 
     src_filename = file_path_by_lang_and_split[src_language][split]
     uncleaned_src_filename = uncleaned_filenames_by_lang_and_split[src_language][split]
@@ -224,7 +225,7 @@ def IWSLT2017(root=".data", split=("train", "valid", "test"), language_pair=("de
     )
 
     cache_inner_src_decompressed_dp = _filter_clean_cache(
-        cache_decompressed_dp, full_src_filepath, uncleaned_src_filename
+        cache_decompressed_dp_1, full_src_filepath, uncleaned_src_filename
     )
 
     tgt_filename = file_path_by_lang_and_split[tgt_language][split]
@@ -240,7 +241,7 @@ def IWSLT2017(root=".data", split=("train", "valid", "test"), language_pair=("de
     )
 
     cache_inner_tgt_decompressed_dp = _filter_clean_cache(
-        cache_decompressed_dp, full_tgt_filepath, uncleaned_tgt_filename
+        cache_decompressed_dp_2, full_tgt_filepath, uncleaned_tgt_filename
     )
 
     tgt_data_dp = FileOpener(cache_inner_tgt_decompressed_dp, encoding="utf-8")
