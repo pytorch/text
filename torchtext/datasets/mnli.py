a93279ddcc7cb3233fae7f48705c88e9a17ddb27
Virgile Mison <virgilem@fb.com>
2022-05-18 15:49:33 -0400

    Add shuffle and sharding

--- torchtext/datasets/mnli.py
+++ torchtext/datasets/mnli.py
@@ -100,4 +100,4 @@ def MNLI(root, split):
 
     data_dp = FileOpener(cache_decompressed_dp, encoding="utf-8")
     parsed_data = data_dp.parse_csv(skip_lines=1, delimiter="\t", quoting=csv.QUOTE_NONE).filter(_filter_res).map(_modify_res)
-    return parsed_data
+    return parsed_data.shuffle().set_shuffle(False).sharding_filter()
