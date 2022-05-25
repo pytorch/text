123977ace3478cbe9bc7adc3ad5bd579afafab22
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2022-05-25 18:18:25 -0400

    Take TORCH_HOME env variable into account while setting the cache dir

--- torchtext/__init__.py
+++ torchtext/__init__.py
@@ -4,7 +4,11 @@ import os
 from torchtext import _extension  # noqa: F401
 
 _TEXT_BUCKET = "https://download.pytorch.org/models/text/"
-_CACHE_DIR = os.path.expanduser("~/.cache/torch/text")
+
+_TORCH_HOME = os.getenv("TORCH_HOME")
+if _TORCH_HOME is None:
+    _TORCH_HOME = "~/.cache/torch" #default
+_CACHE_DIR = os.path.expanduser(os.path.join(_TORCH_HOME, "text"))
 
 from . import data, datasets, experimental, functional, models, nn, transforms, utils, vocab
 
