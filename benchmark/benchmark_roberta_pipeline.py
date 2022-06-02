f92c5c979e2327ebc53613c662ea6cc56cf7f5c7
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2022-06-02 14:30:44 -0400

    update benchmark (error in to_tensor due to self.dtype.nullable)

--- benchmark/benchmark_roberta_pipeline.py
+++ benchmark/benchmark_roberta_pipeline.py
@@ -1,44 +1,21 @@
+import json
 from argparse import ArgumentParser
 from functools import partial
 from typing import Dict, Any
 
+import torch
 import torcharrow as ta
 import torcharrow._torcharrow as _ta
-import json
-from torchtext.utils import get_asset_local_path
 import torcharrow.dtypes as dt
 import torcharrow.pytorch as tap
 import torchtext.functional as F
 import torchtext.transforms as T
-from torcharrow import functional as ta_F
 from benchmark.utils import Timer
 from torch.hub import load_state_dict_from_url
 from torch.nn import Module
+from torcharrow import functional as ta_F
 from torchtext.datasets import DATASETS
-
-
-def init_bpe_encoder():
-    encoder_json_path = "https://download.pytorch.org/models/text/gpt2_bpe_encoder.json"
-    vocab_bpe_path = "https://download.pytorch.org/models/text/gpt2_bpe_vocab.bpe"
-    encoder_json_path = get_asset_local_path(encoder_json_path)
-    vocab_bpe_path = get_asset_local_path(vocab_bpe_path)
-    _seperator = "\u0001"
-
-    # load bpe encoder and bpe decoder
-    with open(encoder_json_path, "r", encoding="utf-8") as f:
-        bpe_encoder = json.load(f)
-    # load bpe vocab
-    with open(vocab_bpe_path, "r", encoding="utf-8") as f:
-        bpe_vocab = f.read()
-    bpe_merge_ranks = {
-        _seperator.join(merge_pair.split()): i
-        for i, merge_pair in enumerate(bpe_vocab.split("\n")[1:-1])
-    }
-    # Caching is enabled in Eager mode
-    bpe = _ta.GPT2BPEEncoder(
-        bpe_encoder, bpe_merge_ranks, _seperator, T.bytes_to_unicode(), True
-    )
-    return bpe
+from torchtext.utils import get_asset_local_path
 
 
 class RobertaTransformDataPipe(Module):
@@ -71,21 +48,44 @@ class RobertaTransformDataPipe(Module):
         return input
 
 
+def init_ta_gpt2bpe_encoder():
+    encoder_json_path = "https://download.pytorch.org/models/text/gpt2_bpe_encoder.json"
+    vocab_bpe_path = "https://download.pytorch.org/models/text/gpt2_bpe_vocab.bpe"
+
+    encoder_json_path = get_asset_local_path(encoder_json_path)
+    vocab_bpe_path = get_asset_local_path(vocab_bpe_path)
+    _seperator = "\u0001"
+
+    # load bpe encoder and bpe decoder
+    with open(encoder_json_path, "r", encoding="utf-8") as f:
+        bpe_encoder = json.load(f)
+    # load bpe vocab
+    with open(vocab_bpe_path, "r", encoding="utf-8") as f:
+        bpe_vocab = f.read()
+    bpe_merge_ranks = {
+        _seperator.join(merge_pair.split()): i for i, merge_pair in enumerate(bpe_vocab.split("\n")[1:-1])
+    }
+    # Caching is enabled in Eager mode
+    bpe = _ta.GPT2BPEEncoder(bpe_encoder, bpe_merge_ranks, _seperator, T.bytes_to_unicode(), True)
+    return bpe
+
+
+def init_ta_gpt2bpe_vocab():
+    vocab_path = "https://download.pytorch.org/models/text/roberta.vocab.pt"
+    vocab_path = get_asset_local_path(vocab_path)
+    vocab = torch.load(vocab_path)
+    ta_vocab = _ta.Vocab(vocab.get_itos(), vocab.get_default_index())
+    return ta_vocab
+
+
 class RobertaTransformDataFrame(Module):
     def __init__(self) -> None:
         super().__init__()
-        # Instantiate various transforms
-
         # Tokenizer to split input text into tokens
-        encoder_json_path = "https://download.pytorch.org/models/text/gpt2_bpe_encoder.json"
-        vocab_bpe_path = "https://download.pytorch.org/models/text/gpt2_bpe_vocab.bpe"
-        self.tokenizer = T.GPT2BPETokenizer(encoder_json_path, vocab_bpe_path)
-
-        # self.tokenizer = init_bpe_encoder()
+        self.tokenizer = init_ta_gpt2bpe_encoder()
 
         # vocabulary converting tokens to IDs
-        vocab_path = "https://download.pytorch.org/models/text/roberta.vocab.pt"
-        self.vocab = T.VocabTransform(load_state_dict_from_url(vocab_path))
+        self.vocab = init_ta_gpt2bpe_vocab()
 
         # Add BOS token to the beginning of sentence
         self.add_bos = T.AddToken(token=0, begin=True)
@@ -94,11 +94,10 @@ class RobertaTransformDataFrame(Module):
         self.add_eos = T.AddToken(token=2, begin=False)
 
     def forward(self, input: ta.DataFrame) -> ta.DataFrame:
-        input["tokens"] = input["text"].transform(self.tokenizer, dtype=dt.List(dt.string), format="python")
         input["tokens"] = ta_F.bpe_tokenize(self.tokenizer, input["text"])
         input["tokens"] = input["tokens"].list.slice(stop=254)
-        input["tokens"] = input["tokens"].map(lambda x: [str(i) for i in input], dtype=dt.List(dt.string))
-        input["tokens"] = input["tokens"].transform(self.vocab, dtype=dt.List(dt.int32), format="python")
+        input["tokens"] = ta_F.lookup_indices(self.vocab, input["tokens"])
+        input["tokens"]._dtype = dt.List(dt.int64)
         input["tokens"] = input["tokens"].transform(self.add_bos, format="python")
         input["tokens"] = input["tokens"].transform(self.add_eos, format="python")
         return input
@@ -151,20 +150,20 @@ def benchmark_roberta_dataframe(args):
         # Apply transformation on DataFrame
         train_dp = train_dp.map(transform)
 
-        # # Remove not required columns
-        # train_dp = train_dp.map(lambda x: x.drop(["text"]))
+        # Remove not required columns
+        train_dp = train_dp.map(lambda x: x.drop(["text"]))
 
-        # # convert DataFrame to tensor (This will yeild named tuple)
-        # train_dp = train_dp.map(lambda x: x.to_tensor({"tokens": tap.PadSequence(padding_value=1)}))
+        # convert DataFrame to tensor (This will yeild named tuple)
+        train_dp = train_dp.map(lambda x: x.to_tensor({"tokens": tap.PadSequence(padding_value=1)}))
 
     with Timer("Execute Pipeline"):
-        list(train_dp.header(1))
+        list(train_dp)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--batch-size", default=32, type=int)
+    parser.add_argument("--batch-size", default=4, type=int)
     parser.add_argument("--dataset-name", default="SST2", type=str)
     parser.add_argument("--columns", default=["text", "label"], nargs="+")
-    benchmark_roberta_datapipe(parser.parse_args())
+    # benchmark_roberta_datapipe(parser.parse_args())
     benchmark_roberta_dataframe(parser.parse_args())
