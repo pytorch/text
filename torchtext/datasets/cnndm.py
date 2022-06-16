b9c816e1958d3f36a0ed92e5cab49bc5e898c386
pmabbo13 <pabbo@fb.com>
2022-06-16 15:21:50 -0400

    run pre-commit

--- torchtext/datasets/cnndm.py
+++ torchtext/datasets/cnndm.py
@@ -1,60 +1,66 @@
-import sys
-import os
 import hashlib
-import struct
-import subprocess
-import collections
+import os
 from functools import partial
 from typing import Union, Tuple
 
 from torchtext._internal.module_utils import is_module_available
-from torchtext.data.datasets_utils import (
-    _wrap_split_argument,
-    _create_dataset_directory,
-)
 
 if is_module_available("torchdata"):
-    from torchdata.datapipes.iter import FileOpener, IterableWrapper, StreamReader, OnlineReader, FileLister, GDriveReader
-    from torchtext._download_hooks import HttpReader
+    from torchdata.datapipes.iter import (
+        FileOpener,
+        IterableWrapper,
+        OnlineReader,
+        FileLister,
+        GDriveReader,
+    )
 
 
-dm_single_close_quote = u'\u2019' # unicode
-dm_double_close_quote = u'\u201d'
-END_TOKENS = ['.', '!', '?', '...', "'", "`", '"', dm_single_close_quote, dm_double_close_quote, ")"] # acceptable ways to end a sentence
-SENTENCE_START = '<s>'
-SENTENCE_END = '</s>'
+dm_single_close_quote = "\u2019"  # unicode
+dm_double_close_quote = "\u201d"
+END_TOKENS = [
+    ".",
+    "!",
+    "?",
+    "...",
+    "'",
+    "`",
+    '"',
+    dm_single_close_quote,
+    dm_double_close_quote,
+    ")",
+]  # acceptable ways to end a sentence
+SENTENCE_START = "<s>"
+SENTENCE_END = "</s>"
 
 URL_LIST = {
-    'train': 'https://raw.githubusercontent.com/abisee/cnn-dailymail/master/url_lists/all_train.txt',
-    'val': 'https://raw.githubusercontent.com/abisee/cnn-dailymail/master/url_lists/all_val.txt',
-    'test': 'https://raw.githubusercontent.com/abisee/cnn-dailymail/master/url_lists/all_test.txt',
+    "train": "https://raw.githubusercontent.com/abisee/cnn-dailymail/master/url_lists/all_train.txt",
+    "val": "https://raw.githubusercontent.com/abisee/cnn-dailymail/master/url_lists/all_val.txt",
+    "test": "https://raw.githubusercontent.com/abisee/cnn-dailymail/master/url_lists/all_test.txt",
 }
 
 URL_LIST_MD5 = {
-    'train': 'c8ca98cfcb6cf3f99a404552568490bc',
-    'val': '83a3c483b3ed38b1392285bed668bfee',
-    'test': '4f3ac04669934dbc746b7061e68a0258',
+    "train": "c8ca98cfcb6cf3f99a404552568490bc",
+    "val": "83a3c483b3ed38b1392285bed668bfee",
+    "test": "4f3ac04669934dbc746b7061e68a0258",
 }
 
 STORIES_LIST = {
-    'cnn': 'https://drive.google.com/uc?export=download&id=0BwmD_VLjROrfTHk4NFg2SndKcjQ',
-    'dailymail': 'https://drive.google.com/uc?export=download&id=0BwmD_VLjROrfM1BxdkxVaTY2bWs',
+    "cnn": "https://drive.google.com/uc?export=download&id=0BwmD_VLjROrfTHk4NFg2SndKcjQ",
+    "dailymail": "https://drive.google.com/uc?export=download&id=0BwmD_VLjROrfM1BxdkxVaTY2bWs",
 }
 
 PATH_LIST = {
-    'cnn': "cnn_stories.tgz",
-    'dailymail': "dailymail_stories.tgz",
+    "cnn": "cnn_stories.tgz",
+    "dailymail": "dailymail_stories.tgz",
 }
 
-STORIES_MD5 = {
-    'cnn': '85ac23a1926a831e8f46a6b8eaf57263',
-    'dailymail': 'f9c5f565e8abe86c38bfa4ae8f96fd72'
-}
+STORIES_MD5 = {"cnn": "85ac23a1926a831e8f46a6b8eaf57263", "dailymail": "f9c5f565e8abe86c38bfa4ae8f96fd72"}
 
 _EXTRACTED_FILES = {
     "cnn": os.path.join("cnn_stories.tgz", "cnn", "stories"),
     "daily_mail": os.path.join("dailymail_stories.tgz", "dailymail", "stories"),
 }
+
 
 def _filepath_fn(root: str, source: str, _=None):
     return os.path.join(root, PATH_LIST[source])
@@ -68,17 +74,17 @@ def _modify_res(t):
     return t[1]
 
 
-def _get_url_list(split:str):
+def _get_url_list(split: str):
 
     url_dp = IterableWrapper([URL_LIST[split]])
     online_dp = OnlineReader(url_dp)
     return online_dp.readlines().map(_modify_res)
 
 
-def _get_stories(root:str, source: str):
+def _get_stories(root: str, source: str):
 
     story_dp = IterableWrapper([STORIES_LIST[source]])
-    
+
     cache_compressed_dp = story_dp.on_disk_cache(
         filepath_fn=partial(_filepath_fn, root, source),
         hash_dict={_filepath_fn(root, source): STORIES_MD5[source]},
@@ -87,9 +93,7 @@ def _get_stories(root:str, source: str):
 
     cache_compressed_dp = GDriveReader(cache_compressed_dp).end_caching(mode="wb", same_filepath_fn=True)
     cache_decompressed_dp = cache_compressed_dp.on_disk_cache(filepath_fn=partial(_extracted_filepath_fn, root, source))
-    cache_decompressed_dp = (
-        FileOpener(cache_decompressed_dp, mode="b").load_from_tar()
-    )
+    cache_decompressed_dp = FileOpener(cache_decompressed_dp, mode="b").load_from_tar()
     cache_decompressed_dp = cache_decompressed_dp.end_caching(mode="wb", same_filepath_fn=True)
 
     stories = FileLister(cache_compressed_dp)
@@ -100,7 +104,7 @@ def _get_stories(root:str, source: str):
 
     for filename, stream in stories:
         stories_dict[filename] = stream
-    
+
     return stories_dict
 
 
@@ -116,26 +120,29 @@ def _get_url_hashes(url_list):
 
 
 def _read_text_file(text_file):
-    
+
     lines = []
     with open(text_file, "r") as f:
         for line in f:
             lines.append(line.strip())
-    
+
     return lines
 
 
 def _fix_missing_period(line):
     """Adds a period to a line that is missing a period"""
-    if "@highlight" in line: return line
-    if line=="": return line
-    if line[-1] in END_TOKENS: return line
+    if "@highlight" in line:
+        return line
+    if line == "":
+        return line
+    if line[-1] in END_TOKENS:
+        return line
     # print line[-1]
     return line + " ."
 
 
 def _get_art_abs(story_file):
-    #lines = _read_text_file(story_file)
+    # lines = _read_text_file(story_file)
     lines = story_file.readlines()
     # Lowercase everything
     lines = [line.decode().lower() for line in lines]
@@ -147,9 +154,9 @@ def _get_art_abs(story_file):
     article_lines = []
     highlights = []
     next_is_highlight = False
-    for idx,line in enumerate(lines):
+    for idx, line in enumerate(lines):
         if line == "":
-            continue # empty line
+            continue  # empty line
         elif line.startswith("@highlight"):
             next_is_highlight = True
         elif next_is_highlight:
@@ -158,10 +165,10 @@ def _get_art_abs(story_file):
             article_lines.append(line)
 
     # Make article into a single string
-    article = ' '.join(article_lines)
+    article = " ".join(article_lines)
 
     # Make abstract into a signle string, putting <s> and </s> tags around the sentences
-    abstract = ' '.join(["%s %s %s" % (SENTENCE_START, sent, SENTENCE_END) for sent in highlights])
+    abstract = " ".join(["%s %s %s" % (SENTENCE_START, sent, SENTENCE_END) for sent in highlights])
 
     return article, abstract
 
@@ -170,21 +177,23 @@ def CNNDM(root: str, split: Union[Tuple[str], str]):
 
     urls = list(_get_url_list(split))
 
-    cnn_stories = _get_stories(root, 'cnn')
-    dm_stories = _get_stories(root, 'dailymail')
+    cnn_stories = _get_stories(root, "cnn")
+    dm_stories = _get_stories(root, "dailymail")
 
     url_hashes = _get_url_hashes(urls)
-    story_fnames = [s+".story" for s in url_hashes]
-    num_stories = len(story_fnames)
+    story_fnames = [s + ".story" for s in url_hashes]
+    # num_stories = len(story_fnames)
 
     for story in story_fnames:
 
-        if os.path.join(root, _EXTRACTED_FILES['cnn'], story) in cnn_stories:
-            story_file = cnn_stories[os.path.join(root, _EXTRACTED_FILES['cnn'], story)]
-        elif os.path.join(root, _EXTRACTED_FILES['dailymail'], story) in cnn_stories:
-            story_file = dm_stories[os.path.join(root, _EXTRACTED_FILES['cnn'], story)]
+        if os.path.join(root, _EXTRACTED_FILES["cnn"], story) in cnn_stories:
+            story_file = cnn_stories[os.path.join(root, _EXTRACTED_FILES["cnn"], story)]
+        elif os.path.join(root, _EXTRACTED_FILES["dailymail"], story) in cnn_stories:
+            story_file = dm_stories[os.path.join(root, _EXTRACTED_FILES["cnn"], story)]
         else:
-            print(f"Error: Couldn't find story file {story} in either cnn or dailymail directories. Was there an error when loading the files?")
+            print(
+                f"Error: Couldn't find story file {story} in either cnn or dailymail directories. Was there an error when loading the files?"
+            )
 
         article, abstract = _get_art_abs(story_file)
         print(f"article: {article}\n\n")
@@ -193,14 +202,11 @@ def CNNDM(root: str, split: Union[Tuple[str], str]):
 
     return cnn_stories
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     print("start")
-    out = CNNDM(os.path.expanduser('~/.torchtext/cache'), 'train')
-    #print(out['/data/home/pabbo/.torchtext/cache/dailymail_stories.tgz/dailymail/stories/70afd5d444a63b17ae663b1ff5b13d4fe1d507f6.story'].read().decode())
-    #print(out['/data/home/pabbo/.torchtext/cache/cnn_stories.tgz/cnn/stories/ee8871b15c50d0db17b0179a6d2beab35065f1e9.story'].read().decode())
-    #print(out[:10])
-
-
-
-
+    out = CNNDM(os.path.expanduser("~/.torchtext/cache"), "train")
+    # print(out['/data/home/pabbo/.torchtext/cache/dailymail_stories.tgz/dailymail/stories/70afd5d444a63b17ae663b1ff5b13d4fe1d507f6.story'].read().decode())
+    # print(out['/data/home/pabbo/.torchtext/cache/cnn_stories.tgz/cnn/stories/ee8871b15c50d0db17b0179a6d2beab35065f1e9.story'].read().decode())
+    # print(out[:10])
