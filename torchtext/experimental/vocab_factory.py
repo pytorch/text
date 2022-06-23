49d3af5fca981bc3b28ef517b728f1a4103f7b64
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2022-06-23 19:43:40 -0400

    add missing api

--- torchtext/experimental/vocab_factory.py
+++ torchtext/experimental/vocab_factory.py
@@ -1,14 +1,20 @@
 def __getattr__(name):
-    if name == "build_vocab_from_text_file":
+    moved_apis = ["build_vocab_from_text_file", "load_vocab_from_file"]
+    if name in moved_apis:
         import warnings
-
-        from torchtext.prototype.vocab_factory import build_vocab_from_text_file
 
         warnings.warn(
             "experimental package has been moved to prototype. You may change all imports from `torchtext.experimental` to `torchtext.prototype`",
             UserWarning,
         )
 
-        return build_vocab_from_text_file
+        if name == "build_vocab_from_text_file":
+            from torchtext.prototype.vocab_factory import build_vocab_from_text_file
+
+            return build_vocab_from_text_file
+        else:
+            from torchtext.prototype.vocab_factory import load_vocab_from_file
+
+            return load_vocab_from_file
 
     raise AttributeError(f"module {__name__} has no attribute {name}")
