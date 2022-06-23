383470317885be318526a564d72894bac2a93b30
parmeet <bhatia.parmeet@gmail.com>
2022-06-23 19:38:54 -0400

    Update torchtext/experimental/vocab_factory.py
    Co-authored-by: moto <855818+mthrok@users.noreply.github.com>

--- torchtext/experimental/vocab_factory.py
+++ torchtext/experimental/vocab_factory.py
@@ -5,7 +5,7 @@ def __getattr__(name):
         from torchtext.prototype.vocab_factory import build_vocab_from_text_file
 
         warnings.warn(
-            "experimental package has been moved to prototype. You may change all imports from `experimental` to `prototype`",
+            "experimental package has been moved to prototype. You may change all imports from `torchtext.experimental` to `torchtext.prototype`",
             UserWarning,
         )
 
