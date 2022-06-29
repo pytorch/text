e9cef16c93d2fcf029de520171bac202fddaa313
pmabbo13 <pabbo@fb.com>
2022-06-29 12:15:34 -0400

    add t5 layer normalization module

--- torchtext/prototype/t5/modules.py
+++ torchtext/prototype/t5/modules.py
@@ -1,7 +1,9 @@
 import math
+import warnings
 from typing import Optional, Tuple
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
 
@@ -582,3 +584,30 @@ class T5MultiheadAttention(nn.MultiheadAttention):
             return attn_output.transpose(1, 0), attn_output_weights, position_bias
         else:
             return attn_output, attn_output_weights, position_bias
+
+
+# NOTE: Taken from HF
+class T5LayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-6):
+        """
+        Construct a layernorm module in the T5 style. No bias and no subtraction of mean.
+        """
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states):
+
+        # T5 uses a layer_norm which only scales and doesn't shift, which is also known as Root Mean
+        # Square Layer Normalization https://arxiv.org/abs/1910.07467 thus varience is calculated
+        # w/o mean and there is no bias. Additionally we want to make sure that the accumulation for
+        # half-precision inputs is done in fp32
+
+        variance = hidden_states.to(torch.float32).pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+
+        # convert into half-precision if necessary
+        if self.weight.dtype in [torch.float16, torch.bfloat16]:
+            hidden_states = hidden_states.to(self.weight.dtype)
+
+        return self.weight * hidden_states
