52eb09c16301c714d2a4cd9ac6082e39361536bb
Nayef Ahmed <nayef211@fb.com>
2022-07-07 19:07:42 -0400

    Resolve PR comments

--- examples/libtorchtext/tokenizer/main.cpp
+++ examples/libtorchtext/tokenizer/main.cpp
@@ -1,25 +1,24 @@
-#include <torch/script.h>
 #include <torch/nn/functional/activation.h>
+#include <torch/script.h>
 
 #include <iostream>
-#include <vector>
 #include <string>
-
-using namespace std;
+#include <vector>
 
 int main(int argc, const char* argv[]) {
-    std::cout << "Loading model...\n";
+  std::cout << "Loading model...\n";
 
-    torch::jit::script::Module module;
-    try {
-        module = torch::jit::load(argv[1]);
-    } catch (const c10::Error& e) {
-        return -1;
-    }
+  torch::jit::script::Module module;
+  try {
+    module = torch::jit::load(argv[1]);
+  } catch (const c10::Error& e) {
+    return -1;
+  }
 
-    torch::NoGradGuard no_grad; // ensures that autograd is off
-    torch::jit::IValue tokens_ivalue = module.forward(std::vector<c10::IValue>(1,"The green grasshopper jumped over the fence"));
-    std::cout << "Result: " << tokens_ivalue << std::endl;
+  torch::NoGradGuard no_grad; // ensures that autograd is off
+  torch::jit::IValue tokens_ivalue = module.forward(std::vector<c10::IValue>(
+      1, "The green grasshopper jumped over the fence"));
+  std::cout << "Result: " << tokens_ivalue << std::endl;
 
-    return 0;
+  return 0;
 }
