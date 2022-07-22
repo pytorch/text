07a14fc1e50a570d50b8b46334c2070593bd7810
pmabbo13 <pabbo@fb.com>
2022-07-22 15:51:59 -0400

    update integration tests to take input text and use transform

--- test/prototype/integration_tests/test_models.py
+++ test/prototype/integration_tests/test_models.py
@@ -8,14 +8,16 @@ from torchtext.prototype.models import (
 
 
 class TestT5(TorchtextTestCase):
-    def _t5_model(self, t5_model, expected_asset_name, model_input):
+    def _t5_model(self, t5_model, expected_asset_name, test_text):
         """Verify that pre-trained T5 models in torchtext produce
         the same output as the HuggingFace reference implementation.
         """
         expected_asset_path = get_asset_path(expected_asset_name)
+        transform = t5_model.transform()
         model = t5_model.get_model()
         model = model.eval()
 
+        model_input = transform(test_text)
         if model.encoder_only:
             actual = model(model_input)["encoder_output"]
         else:
@@ -26,10 +28,10 @@ class TestT5(TorchtextTestCase):
 
     def test_t5_base_encoder_model(self):
         expected_asset_name = "t5.base.encoder.output.pt"
-        model_input = torch.tensor([[1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 0, 0]])
-        self._t5_model(t5_model=T5_BASE_ENCODER, expected_asset_name=expected_asset_name, model_input=model_input)
+        test_text = ["Hello world", "Attention rocks!"]
+        self._t5_model(t5_model=T5_BASE_ENCODER, expected_asset_name=expected_asset_name, test_text=test_text)
 
     def test_t5_base_model(self):
         expected_asset_name = "t5.base.output.pt"
-        model_input = torch.tensor([[1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 0, 0]])
-        self._t5_model(t5_model=T5_BASE, expected_asset_name=expected_asset_name, model_input=model_input)
+        test_text = ["Hello world", "Attention rocks!"]
+        self._t5_model(t5_model=T5_BASE, expected_asset_name=expected_asset_name, test_text=test_text)
