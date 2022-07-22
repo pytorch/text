75a48d2cc08ccab6dd97903a983c42e1564b18d8
pmabbo13 <pabbo@fb.com>
2022-07-22 13:25:48 -0400

    coalesce encode and decode tests

--- test/prototype/models/test_transforms.py
+++ test/prototype/models/test_transforms.py
@@ -12,47 +12,34 @@ class TestTransforms(TorchtextTestCase):
         if test_scripting:
             transform = torch.jit.script(transform)
 
-        # input is a list (i.e. batched)
-        input_seq = ["Hello World!, how are you?"]
-        actual = transform(input_seq)
-        expected = torch.tensor([[8774, 1150, 55, 6, 149, 33, 25, 58, 1]])
-        self.assertEqual(actual, expected)
-
-        # input is a single string
-        input_seq = "Hello World!, how are you?"
-        actual = transform(input_seq)
+        # test encode; input is a single string
+        encode_seq = "Hello World!, how are you?"
+        actual = transform(encode_seq)
         expected = torch.tensor([8774, 1150, 55, 6, 149, 33, 25, 58, 1])
         self.assertEqual(actual, expected)
 
-    def test_t5tokenizer(self):
-        """test tokenization on string input (batched and un-batched)"""
-        self._t5tokenizer(test_scripting=False)
-
-    def test_t5tokenizer_jit(self):
-        """test tokenization with scripting on string input (batched and un-batched)"""
-        self._t5tokenizer(test_scripting=True)
-
-    def _t5tokenizer_decode(self, test_scripting):
-        asset_name = "t5_tokenizer_base.model"
-        asset_path = get_asset_path(asset_name)
-        transform = T5Transform(asset_path, max_seq_len=512, eos_idx=1, padding_idx=0)
-        if test_scripting:
-            transform = torch.jit.script(transform)
-
-        input_seq = [[8774, 1150, 55, 6, 149, 33, 25, 58, 1]]
-        actual = transform.decode(input_seq)
-        expected = ["Hello World!, how are you?"]
+        # test encode; input is a batched string
+        encode_seq = ["Hello World!, how are you?"]
+        actual = transform(encode_seq)
+        expected = torch.tensor([[8774, 1150, 55, 6, 149, 33, 25, 58, 1]])
         self.assertEqual(actual, expected)
 
-        input_seq = [8774, 1150, 55, 6, 149, 33, 25, 58, 1]
-        actual = transform.decode(input_seq)
+        # test decode; input is a list of token ids
+        decode_seq = [8774, 1150, 55, 6, 149, 33, 25, 58, 1]
+        actual = transform.decode(decode_seq)
         expected = "Hello World!, how are you?"
         self.assertEqual(actual, expected)
 
-    def test_t5tokenizer_decode(self):
-        """test translation from token ids to strings (batched and un-batched)"""
-        self._t5tokenizer_decode(test_scripting=False)
+        # test decode; input is a batched list of token ids
+        decode_seq = [[8774, 1150, 55, 6, 149, 33, 25, 58, 1]]
+        actual = transform.decode(decode_seq)
+        expected = ["Hello World!, how are you?"]
+        self.assertEqual(actual, expected)
 
-    def test_t5tokenizer_decode_jit(self):
-        """test translation from token ids to strings with scripting (batched and un-batched)"""
-        self._t5tokenizer_decode(test_scripting=True)
+    def test_t5tokenizer(self):
+        """test tokenization on string input (encode) and translation from token ids to strings (decode)"""
+        self._t5tokenizer(test_scripting=False)
+
+    def test_t5tokenizer_jit(self):
+        """test tokenization on string input (encode) and translation from token ids to strings (decode) with scripting"""
+        self._t5tokenizer(test_scripting=True)
