608ccb4934de8961ddef9c1229898d4a0c24549b
pmabbo13 <pabbo@fb.com>
2022-08-02 10:46:56 -0400

    setting model.eval()

--- examples/tutorials/cnndm_summarization.py
+++ examples/tutorials/cnndm_summarization.py
@@ -51,7 +51,7 @@ from torchtext.prototype.models import T5Transform
 padding_idx = 0
 eos_idx = 1
 max_seq_len = 512
-t5_sp_model_path = r"https://download.pytorch.org/models/text/t5_tokenizer_base.model"
+t5_sp_model_path = "https://download.pytorch.org/models/text/t5_tokenizer_base.model"
 
 transform = T5Transform(
     sp_model_path=t5_sp_model_path,
@@ -139,6 +139,7 @@ from torchtext.prototype.models import T5_BASE_GENERATION
 t5_base = T5_BASE_GENERATION
 transform = t5_base.transform()
 model = t5_base.get_model()
+model.eval()
 model.to(DEVICE)
 
 
