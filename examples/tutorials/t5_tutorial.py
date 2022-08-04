9b11269dc129ea6415f3c053b79031b398a8caa1
pmabbo13 <pabbo@fb.com>
2022-08-04 19:31:25 +0000

    renaming tutorial and removing hard-coded outputs

--- examples/tutorials/t5_tutorial.py
+++ examples/tutorials/t5_tutorial.py
@@ -86,8 +86,8 @@ from torchtext.prototype.models import T5_BASE_GENERATION
 t5_base = T5_BASE_GENERATION
 transform = t5_base.transform()
 model = t5_base.get_model()
-model.eval()
-model.to(DEVICE)
+model = model.eval()
+model = model.to(DEVICE)
 
 
 #######################################################################
@@ -303,7 +303,7 @@ cnndm_dataloader = DataLoader(cnndm_datapipe, batch_size=None)
 
 from torchtext.datasets import IMDB
 
-imdb_batch_size = 3
+imdb_batch_size = 5
 imdb_datapipe = IMDB(split="test")
 task = "sst2 sentence"
 labels = {"neg": "negative", "pos": "positive"}
@@ -338,7 +338,7 @@ multi_datapipe = multi_datapipe.rows2columnar(["english", "german"])
 multi_dataloader = DataLoader(multi_datapipe, batch_size=None)
 
 #######################################################################
-# Generate Summaries
+# Summaries
 # ------------------
 #
 # We can put all of the components together the generate summaries on the first batch of articles in the CNNDM test set
@@ -361,74 +361,7 @@ for i in range(cnndm_batch_size):
 
 
 #######################################################################
-# Summarization Output
-# --------------------
-#
-# ::
-#
-#    Example 1:
-#
-#    prediction: the Palestinians become the 123rd member of the international criminal
-#    court . the accession was marked by a ceremony at the Hague, where the court is based .
-#    the ICC opened a preliminary examination into the situation in the occupied
-#    Palestinian territory .
-#
-#    target: Membership gives the ICC jurisdiction over alleged crimes committed in
-#    Palestinian territories since last June . Israel and the United States opposed the
-#    move, which could open the door to war crimes investigations against Israelis .
-#
-#
-#    Example 2:
-#
-#    prediction: a stray pooch has used up at least three of her own after being hit by a
-#    car and buried in a field . the dog managed to stagger to a nearby farm, dirt-covered
-#    and emaciated, where she was found . she suffered a dislocated jaw, leg injuries and a
-#    caved-in sinus cavity -- and still requires surgery to help her breathe .
-#
-#    target: Theia, a bully breed mix, was apparently hit by a car, whacked with a hammer
-#    and buried in a field . "She's a true miracle dog and she deserves a good life," says
-#    Sara Mellado, who is looking for a home for Theia .
-#
-#
-#    Example 3:
-#
-#    prediction: mohammad Javad Zarif arrived in Iran on a sunny friday morning . he has gone
-#    a long way to bring Iran in from the cold and allow it to rejoin the international
-#    community . but there are some facts about him that are less well-known .
-#
-#    target: Mohammad Javad Zarif has spent more time with John Kerry than any other
-#    foreign minister . He once participated in a takeover of the Iranian Consulate in San
-#    Francisco . The Iranian foreign minister tweets in English .
-#
-#
-#    Example 4:
-#
-#    prediction: five americans were monitored for three weeks after being exposed to Ebola in
-#    west africa . one of the five had a heart-related issue and has been discharged but hasn't
-#    left the area . they are clinicians for Partners in Health, a Boston-based aid group .
-#
-#    target: 17 Americans were exposed to the Ebola virus while in Sierra Leone in March .
-#    Another person was diagnosed with the disease and taken to hospital in Maryland .
-#    National Institutes of Health says the patient is in fair condition after weeks of
-#    treatment .
-#
-#
-#    Example 5:
-#
-#    prediction: the student was identified during an investigation by campus police and
-#    the office of student affairs . he admitted to placing the noose on the tree early
-#    Wednesday morning . the incident is one of several recent racist events to affect
-#    college students .
-#
-#    target: Student is no longer on Duke University campus and will face disciplinary
-#    review . School officials identified student during investigation and the person
-#    admitted to hanging the noose, Duke says . The noose, made of rope, was discovered on
-#    campus about 2 a.m.
-#
-
-
-#######################################################################
-# Generate Sentiment Classifications
+# Sentiment Classifications
 # ----------------------------------
 #
 # Similarly, we can now use the model to generate sentiment classifications on the first batch of reviews from the IMDB test set
@@ -452,77 +385,7 @@ for i in range(imdb_batch_size):
 
 
 #######################################################################
-# Sentiment Output
-# ----------------
-#
-# ::
-#
-#    Example 1:
-#
-#    input_text: sst2 sentence: I love sci-fi and am willing to put up with a lot. Sci-fi
-#    movies/TV are usually underfunded, under-appreciated and misunderstood. I tried to like
-#    this, I really did, but it is to good TV sci-fi as Babylon 5 is to Star Trek (the original).
-#    Silly prosthetics, cheap cardboard sets, stilted dialogues, CG that doesn't match the
-#    background, and painfully one-dimensional characters cannot be overcome with a 'sci-fi'
-#    setting. (I'm sure there are those of you out there who think Babylon 5 is good sci-fi TV.
-#    It's not. It's clichéd and uninspiring.) While US viewers might like emotion and character
-#    development, sci-fi is a genre that does not take itself seriously (cf. Star Trek). It may
-#    treat important issues, yet not as a serious philosophy. It's really difficult to care about
-#    the characters here as they are not simply foolish, just missing a spark of life. Their
-#    actions and reactions are wooden and predictable, often painful to watch. The makers of Earth
-#    KNOW it's rubbish as they have to always say "Gene Roddenberry's Earth..." otherwise people
-#    would not continue watching. Roddenberry's ashes must be turning in their orbit as this dull,
-#    cheap, poorly edited (watching it without advert breaks really brings this home) trudging
-#    Trabant of a show lumbers into space. Spoiler. So, kill off a main character. And then bring
-#    him back as another actor. Jeeez. Dallas all over again.
-#
-#    prediction: negative
-#
-#    target: negative
-#
-#
-#    Example 2:
-#
-#    input_text: sst2 sentence: Worth the entertainment value of a rental, especially if you like
-#    action movies. This one features the usual car chases, fights with the great Van Damme kick
-#    style, shooting battles with the 40 shell load shotgun, and even terrorist style bombs. All
-#    of this is entertaining and competently handled but there is nothing that really blows you
-#    away if you've seen your share before.<br /><br />The plot is made interesting by the
-#    inclusion of a rabbit, which is clever but hardly profound. Many of the characters are
-#    heavily stereotyped -- the angry veterans, the terrified illegal aliens, the crooked cops,
-#    the indifferent feds, the bitchy tough lady station head, the crooked politician, the fat
-#    federale who looks like he was typecast as the Mexican in a Hollywood movie from the 1940s.
-#    All passably acted but again nothing special.<br /><br />I thought the main villains were
-#    pretty well done and fairly well acted. By the end of the movie you certainly knew who the
-#    good guys were and weren't. There was an emotional lift as the really bad ones got their just
-#    deserts. Very simplistic, but then you weren't expecting Hamlet, right? The only thing I found
-#    really annoying was the constant cuts to VDs daughter during the last fight scene.<br /><br />
-#    Not bad. Not good. Passable 4.
-#
-#    prediction: negative
-#
-#    target: negative
-#
-#
-#    Example 3:
-#
-#    input_text: sst2 sentence: its a totally average film with a few semi-alright action sequences
-#    that make the plot seem a little better and remind the viewer of the classic van dam films.
-#    parts of the plot don't make sense and seem to be added in to use up time. the end plot is that
-#    of a very basic type that doesn't leave the viewer guessing and any twists are obvious from the
-#    beginning. the end scene with the flask backs don't make sense as they are added in and seem to
-#    have little relevance to the history of van dam's character. not really worth watching again,
-#    bit disappointed in the end production, even though it is apparent it was shot on a low budget
-#    certain shots and sections in the film are of poor directed quality.
-#
-#    prediction: negative
-#
-#    target: negative
-#
-
-
-#######################################################################
-# Generate Translations
+# Translations
 # ---------------------
 #
 # Finally, we can also use the model to generate English to German translations on the first batch of examples from the Multi30k
@@ -543,55 +406,3 @@ for i in range(multi_batch_size):
     print(f"input_text: {input_text[i]}\n")
     print(f"prediction: {output_text[i]}\n")
     print(f"target: {target[i]}\n\n")
-
-
-#######################################################################
-# Translation Output
-# ------------------
-#
-# ::
-#
-#    Example 1:
-#
-#    input_text: translate English to German: A man in an orange hat starring at something.
-#
-#    prediction: Ein Mann in einem orangen Hut, der an etwas schaut.
-#
-#    target: Ein Mann mit einem orangefarbenen Hut, der etwas anstarrt.
-#
-#
-#    Example 2:
-#
-#    input_text: translate English to German: A Boston Terrier is running on lush green grass in front of a white fence.
-#
-#    prediction: Ein Boston Terrier läuft auf üppigem grünem Gras vor einem weißen Zaun.
-#
-#    target: Ein Boston Terrier läuft über saftig-grünes Gras vor einem weißen Zaun.
-#
-#
-#    Example 3:
-#
-#    input_text: translate English to German: A girl in karate uniform breaking a stick with a front kick.
-#
-#    prediction: Ein Mädchen in Karate-Uniform bricht einen Stöck mit einem Frontkick.
-#
-#    target: Ein Mädchen in einem Karateanzug bricht ein Brett mit einem Tritt.
-#
-#
-#    Example 4:
-#
-#    input_text: translate English to German: Five people wearing winter jackets and helmets stand in the snow, with snowmobiles in the background.
-#
-#    prediction: Fünf Menschen mit Winterjacken und Helmen stehen im Schnee, mit Schneemobilen im Hintergrund.
-#
-#    target: Fünf Leute in Winterjacken und mit Helmen stehen im Schnee mit Schneemobilen im Hintergrund.
-#
-#
-#    Example 5:
-#
-#    input_text: translate English to German: People are fixing the roof of a house.
-#
-#    prediction: Die Leute fixieren das Dach eines Hauses.
-#
-#    target: Leute Reparieren das Dach eines Hauses.
-#
