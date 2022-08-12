616f72f80d51c8eb469e6de159569cdd18393f91
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared62533.02.ldc2.facebook.com>
2022-08-12 04:30:31 -0700

    2022-08-12 nightly release (e7bcf3c682f83e9fad88d894920be38cddccd32b)

--- test/prototype/integration_tests/test_models.py
+++ test/prototype/integration_tests/test_models.py
@@ -1,4 +1,5 @@
 import torch
+from parameterized import parameterized
 from test.common.assets import get_asset_path
 from test.common.torchtext_test_case import TorchtextTestCase
 from torchtext.prototype.models import (
@@ -9,7 +10,7 @@ from torchtext.prototype.models import (
 
 
 class TestT5(TorchtextTestCase):
-    def _t5_model(self, t5_model, expected_asset_name, test_text):
+    def _t5_model(self, is_jit, t5_model, expected_asset_name, test_text):
         """Verify that pre-trained T5 models in torchtext produce
         the same output as the HuggingFace reference implementation.
         """
@@ -17,6 +18,10 @@ class TestT5(TorchtextTestCase):
         transform = t5_model.transform()
         model = t5_model.get_model()
         model = model.eval()
+
+        if is_jit:
+            transform = torch.jit.script(transform)
+            model = torch.jit.script(model)
 
         model_input = transform(test_text)
         if model.encoder_only:
@@ -27,17 +32,24 @@ class TestT5(TorchtextTestCase):
         expected = torch.load(expected_asset_path)
         torch.testing.assert_close(actual, expected, atol=1e-04, rtol=2.5e-06)
 
-    def test_t5_base_encoder_model(self) -> None:
+    @parameterized.expand([("jit", True), ("not_jit", False)])
+    def test_t5_base_encoder_model(self, name, is_jit) -> None:
         expected_asset_name = "t5.base.encoder.output.pt"
         test_text = ["Hello world", "Attention rocks!"]
-        self._t5_model(t5_model=T5_BASE_ENCODER, expected_asset_name=expected_asset_name, test_text=test_text)
+        self._t5_model(
+            is_jit=is_jit, t5_model=T5_BASE_ENCODER, expected_asset_name=expected_asset_name, test_text=test_text
+        )
 
-    def test_t5_base_model(self) -> None:
+    @parameterized.expand([("jit", True), ("not_jit", False)])
+    def test_t5_base_model(self, name, is_jit) -> None:
         expected_asset_name = "t5.base.output.pt"
         test_text = ["Hello world", "Attention rocks!"]
-        self._t5_model(t5_model=T5_BASE, expected_asset_name=expected_asset_name, test_text=test_text)
+        self._t5_model(is_jit=is_jit, t5_model=T5_BASE, expected_asset_name=expected_asset_name, test_text=test_text)
 
-    def test_t5_base_generation_model(self) -> None:
+    @parameterized.expand([("jit", True), ("not_jit", False)])
+    def test_t5_base_generation_model(self, name, is_jit) -> None:
         expected_asset_name = "t5.base.generation.output.pt"
         test_text = ["Hello world", "Attention rocks!"]
-        self._t5_model(t5_model=T5_BASE_GENERATION, expected_asset_name=expected_asset_name, test_text=test_text)
+        self._t5_model(
+            is_jit=is_jit, t5_model=T5_BASE_GENERATION, expected_asset_name=expected_asset_name, test_text=test_text
+        )
