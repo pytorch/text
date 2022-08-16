8ae717cd7b31a7df3d749ae428c586f63cc098a6
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared15189.05.pnb2.facebook.com>
2022-08-16 04:30:38 -0700

    2022-08-16 nightly release (2fd12f323fa21f85a9e0600d1f4ad1cdb494848b)

--- test/prototype/integration_tests/test_models.py
+++ test/prototype/integration_tests/test_models.py
@@ -2,11 +2,8 @@ import torch
 from parameterized import parameterized
 from test.common.assets import get_asset_path
 from test.common.torchtext_test_case import TorchtextTestCase
-from torchtext.prototype.models import (
-    T5_BASE_ENCODER,
-    T5_BASE,
-    T5_BASE_GENERATION,
-)
+from torchtext.prototype.models import T5_BASE_ENCODER, T5_BASE, T5_BASE_GENERATION, T5Conf, T5Transform
+from torchtext.prototype.models.t5.wrapper import T5Wrapper
 
 
 class TestT5(TorchtextTestCase):
@@ -53,3 +50,42 @@ class TestT5(TorchtextTestCase):
         self._t5_model(
             is_jit=is_jit, t5_model=T5_BASE_GENERATION, expected_asset_name=expected_asset_name, test_text=test_text
         )
+
+    @parameterized.expand([("jit", True), ("not_jit", False)])
+    def test_t5_wrapper(self, name, is_jit) -> None:
+        test_text = ["translate English to French: I want to eat pizza for dinner."]
+        expected_text = ["Je veux manger de la pizza pour le dîner."]
+        beam_size = 3
+        max_seq_len = 512
+        model = T5Wrapper(configuration="base")
+        if is_jit:
+            model = torch.jit.script(model)
+
+        output_text = model(test_text, beam_size, max_seq_len)
+        self.assertEqual(output_text, expected_text)
+
+    @parameterized.expand([("jit", True), ("not_jit", False)])
+    def test_t5_wrapper_checkpoing(self, name, is_jit) -> None:
+        test_text = ["translate English to French: I want to eat pizza for dinner."]
+        expected_text = ["Je veux manger de la pizza pour le dîner."]
+        beam_size = 3
+        max_seq_len = 512
+        config = T5Conf(encoder_only=False, linear_head=True)
+        transform = T5Transform(
+            "https://download.pytorch.org/models/text/t5_tokenizer_base.model",
+            max_seq_len=512,
+            eos_idx=1,
+            padding_idx=0,
+        )
+        model = T5Wrapper(
+            checkpoint="https://download.pytorch.org/models/text/t5.base.generation.pt",
+            t5_config=config,
+            transform=transform,
+            freeze_model=True,
+            strict=True,
+        )
+        if is_jit:
+            model = torch.jit.script(model)
+
+        output_text = model(test_text, beam_size, max_seq_len)
+        self.assertEqual(output_text, expected_text)
