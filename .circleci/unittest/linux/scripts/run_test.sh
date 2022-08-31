27901204ba2f757ce162d670abce3feba92edb5d
Nayef Ahmed <nayef211@fb.com>
2022-08-31 00:01:02 -0400

    Revert "Created new unittest directory and new workflow for integration tests"
    This reverts commit 8694c20667d667aa45a4ec4da97ea8effa7258d3.

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -6,5 +6,4 @@ eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
 python -m torch.utils.collect_env
-cd test
-pytest --cov=torchtext --junitxml=test-results/junit.xml -v --durations 20 torchtext_unittest
+pytest --cov=torchtext --junitxml=test-results/junit.xml -v --durations 20 test
