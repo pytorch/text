4e807215cf497113d1bd6a51d57d787b60aa4d40
atalman <atalman@fb.com>
2022-10-06 14:17:34 -0700

    Fix version extraction

--- packaging/pkg_helpers.bash
+++ packaging/pkg_helpers.bash
@@ -206,7 +206,7 @@ setup_conda_pytorch_constraint() {
     if python3 --version >/dev/null 2>/dev/null; then
       PYTHON="python3"
     fi
-    export PYTORCH_VERSION="$(conda search --json 'pytorch[channel=pytorch-${UPLOAD_CHANNEL}]' | ${PYTHON} -c "import sys, json, re; print(re.sub(r'\\+.*$', '', json.load(sys.stdin)['pytorch'][-1]['version']))")"
+    export PYTORCH_VERSION="$(conda search --json pytorch[channel=pytorch-${UPLOAD_CHANNEL}] | ${PYTHON} -c "import sys, json, re; print(re.sub(r'\\+.*$', '', json.load(sys.stdin)['pytorch'][-1]['version']))")"
   else
     export CONDA_CHANNEL_FLAGS="${CONDA_CHANNEL_FLAGS} -c pytorch -c pytorch-${UPLOAD_CHANNEL}"
   fi
