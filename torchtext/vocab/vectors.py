6145aba1cb77cdc427aff164f65b276b0dacb87f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 02:45:43 +0000

    Adding tarfile member sanitization to extractall()

--- torchtext/vocab/vectors.py
+++ torchtext/vocab/vectors.py
@@ -104,7 +104,26 @@ class Vectors(object):
                 elif ext == "gz":
                     if dest.endswith(".tar.gz"):
                         with tarfile.open(dest, "r:gz") as tar:
-                            tar.extractall(path=cache)
+                            def is_within_directory(directory, target):
+                                
+                                abs_directory = os.path.abspath(directory)
+                                abs_target = os.path.abspath(target)
+                            
+                                prefix = os.path.commonprefix([abs_directory, abs_target])
+                                
+                                return prefix == abs_directory
+                            
+                            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                            
+                                for member in tar.getmembers():
+                                    member_path = os.path.join(path, member.name)
+                                    if not is_within_directory(path, member_path):
+                                        raise Exception("Attempted Path Traversal in Tar File")
+                            
+                                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                
+                            
+                            safe_extract(tar, path=cache)
             if not os.path.isfile(path):
                 raise RuntimeError("no vectors found at {}".format(path))
 
