36b65facd40d37a2aebbf22d6c357f4487026106
Omkar Salpekar <osalpekar@fb.com>
2022-10-20 11:06:41 -0400

    Adding Smoke Tests for Binary Release

---
+++ test/smoke_tests/smoke_tests.py
@@ -0,0 +1,30 @@
+"""Run smoke tests"""
+
+import torchtext # noqa: F401
+import torchtext.data # noqa: F401
+import torchtext.data.batch # noqa: F401
+import torchtext.data.dataset # noqa: F401
+import torchtext.data.example # noqa: F401
+import torchtext.data.field # noqa: F401
+import torchtext.data.functional # noqa: F401
+import torchtext.data.iterator # noqa: F401
+import torchtext.data.metrics # noqa: F401
+import torchtext.data.pipeline # noqa: F401
+import torchtext.data.utils # noqa: F401
+import torchtext.datasets # noqa: F401
+import torchtext.datasets.babi # noqa: F401
+import torchtext.datasets.imdb # noqa: F401
+import torchtext.datasets.language_modeling # noqa: F401
+import torchtext.datasets.nli # noqa: F401
+import torchtext.datasets.sequence_tagging # noqa: F401
+import torchtext.datasets.sst # noqa: F401
+import torchtext.datasets.text_classification # noqa: F401
+import torchtext.datasets.translation # noqa: F401
+import torchtext.datasets.trec # noqa: F401
+import torchtext.datasets.unsupervised_learning # noqa: F401
+import torchtext.experimental # noqa: F401
+import torchtext.experimental.datasets # noqa: F401
+import torchtext.experimental.datasets.language_modeling # noqa: F401
+import torchtext.experimental.datasets.text_classification # noqa: F401
+import torchtext.utils # noqa: F401
+import torchtext.vocab # noqa: F401
