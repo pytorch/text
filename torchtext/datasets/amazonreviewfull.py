1e591a922b76779854dac0f201f64d78d66d1c6c
chronos_secgrp_pytorch_oss_ci_oncall <chronos_secgrp_pytorch_oss_ci_oncall@twshared6172.02.ldc5.facebook.com>
2022-10-26 04:30:51 -0700

    2022-10-26 nightly release (0208a0ed4fa5f55e1e50cc26c24cb7a132e0d62f)

--- torchtext/datasets/amazonreviewfull.py
+++ torchtext/datasets/amazonreviewfull.py
@@ -2,15 +2,13 @@ import os
 from functools import partial
 from typing import Union, Tuple
 
+from torchdata.datapipes.iter import FileOpener, IterableWrapper
+from torchtext._download_hooks import GDriveReader
 from torchtext._internal.module_utils import is_module_available
 from torchtext.data.datasets_utils import (
     _wrap_split_argument,
     _create_dataset_directory,
 )
-
-if is_module_available("torchdata"):
-    from torchdata.datapipes.iter import FileOpener, IterableWrapper
-    from torchtext._download_hooks import GDriveReader
 
 URL = "https://drive.google.com/uc?export=download&id=0Bz8a_Dbh9QhbZVhsUnRWRDhETzA"
 
