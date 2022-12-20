de368d759c368b61d51225fb9460ce54823db1d1
Andrey Talman <atalman@fb.com>
2022-12-19 21:07:27 -0500

    Validate  binaries (#2010)

---
+++ .github/scripts/validate_binaries.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -ex
+
+if [[ ${TARGET_OS} == 'windows' ]]; then
+    source /c/Jenkins/Miniconda3/etc/profile.d/conda.sh
+else
+    eval "$(conda shell.bash hook)"
+fi
+
+conda create -y -n ${ENV_NAME} python=${DESIRED_PYTHON} numpy
+conda activate ${ENV_NAME}
+export CONDA_CHANNEL="pytorch"
+export PIP_DOWNLOAD_URL="https://download.pytorch.org/whl/cpu"
+export PIP_PREFIX=""
+
+if [[ ${CHANNEL} = 'nightly' ]]; then
+    export PIP_PREFIX="--pre "
+    export PIP_DOWNLOAD_URL="https://download.pytorch.org/whl/nightly/cpu"
+    export CONDA_CHANNEL="pytorch-nightly"
+elif [[ ${CHANNEL} = 'test' ]]; then
+    export PIP_DOWNLOAD_URL="https://download.pytorch.org/whl/test/cpu"
+    export CONDA_CHANNEL="pytorch-test"
+fi
+
+if [[ ${PACKAGE_TYPE} = 'conda' ]]; then
+    conda install -y torchtext pytorch -c ${CONDA_CHANNEL}
+else
+    pip install ${PIP_PREFIX} torchtext torch --extra-index-url ${PIP_DOWNLOAD_URL}
+fi
+
+python  ./test/smoke_tests/smoke_tests.py
