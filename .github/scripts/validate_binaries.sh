9cec1f94fc4e7cccc2db57dd8308144f8e8ad601
atalman <atalman@fb.com>
2022-12-20 18:58:08 -0800

    test

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -14,7 +14,7 @@ export PIP_DOWNLOAD_URL="https://download.pytorch.org/whl/cpu"
 export PIP_PREFIX=""
 
 if [[ ${CHANNEL} = 'nightly' ]]; then
-    export PIP_PREFIX="--pre "
+    export PIP_PREFIX="--pre"
     export PIP_DOWNLOAD_URL="https://download.pytorch.org/whl/nightly/cpu"
     export CONDA_CHANNEL="pytorch-nightly"
 elif [[ ${CHANNEL} = 'test' ]]; then
@@ -25,7 +25,7 @@ fi
 if [[ ${PACKAGE_TYPE} = 'conda' ]]; then
     conda install -y torchtext pytorch -c ${CONDA_CHANNEL}
 else
-    pip install ${PIP_PREFIX} torchtext torch --extra-index-url ${PIP_DOWNLOAD_URL}
+    pip3 install ${PIP_PREFIX} torchtext torch --extra-index-url ${PIP_DOWNLOAD_URL}
 fi
 
 python3  ./test/smoke_tests/smoke_tests.py
