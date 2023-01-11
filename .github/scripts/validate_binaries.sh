9f1501587217596dad9e5cf669d7495e0df875b1
atalman <atalman@fb.com>
2023-01-11 08:28:02 -0800

    fix formatting and install

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -4,16 +4,19 @@ export CONDA_CHANNEL="pytorch"
 export PIP_DOWNLOAD_URL="https://download.pytorch.org/whl/cpu"
 export TEXT_PIP_PREFIX=""
 
-if [[ ${MATRIX_CHANNEL} = 'nightly' ]]; then
+if [[ ${MATRIX_CHANNEL} = "nightly" ]]; then
     export TEXT_PIP_PREFIX="--pre"
     export PIP_DOWNLOAD_URL="https://download.pytorch.org/whl/nightly/cpu"
     export CONDA_CHANNEL="pytorch-nightly"
-elif [[ ${MATRIX_CHANNEL} = 'test' ]]; then
+elif [[ ${MATRIX_CHANNEL} = "test" ]]; then
     export PIP_DOWNLOAD_URL="https://download.pytorch.org/whl/test/cpu"
     export CONDA_CHANNEL="pytorch-test"
 fi
 
-conda install -y  pytorch -c ${CONDA_CHANNEL}
-pip install ${TEXT_PIP_PREFIX} torchtext torch --extra-index-url ${PIP_DOWNLOAD_URL}
+if [[ ${MATRIX_PACKAGE_TYPE} = "conda" ]]; then
+    conda install -y torchtext pytorch -c ${CONDA_CHANNEL}
+else
+    pip install ${TEXT_PIP_PREFIX} torchtext torch --extra-index-url ${PIP_DOWNLOAD_URL}
+fi
 
 python  ./test/smoke_tests/smoke_tests.py
