38fdaf21474aaeb753be0afc04789be53b4e9372
Roman Shraga <roman.shraga@gmail.com>
2023-01-26 12:50:32 -0500

    fix lint

--- test/torchtext_unittest/models/gpu_tests/models_gpu_test.py
+++ test/torchtext_unittest/models/gpu_tests/models_gpu_test.py
@@ -1,8 +1,9 @@
 import unittest
-import torch
 
+import torch
 from torchtext_unittest.common.torchtext_test_case import TorchtextTestCase
 from torchtext_unittest.models.models_test_impl import BaseTestModels
+
 
 @unittest.skipIf(not torch.cuda.is_available(), reason="CUDA is not available")
 class TestModels32GPU(BaseTestModels, TorchtextTestCase):
