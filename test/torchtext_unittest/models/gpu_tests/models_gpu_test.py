1d40bb31796ad2d9d6077a340031922bfa5bd907
Roman Shraga <roman.shraga@gmail.com>
2023-01-26 12:43:16 -0500

    add back gpu tests

---
+++ test/torchtext_unittest/models/gpu_tests/models_gpu_test.py
@@ -0,0 +1,10 @@
+import unittest
+import torch
+
+from torchtext_unittest.common.torchtext_test_case import TorchtextTestCase
+from torchtext_unittest.models.models_test_impl import BaseTestModels
+
+@unittest.skipIf(not torch.cuda.is_available(), reason="CUDA is not available")
+class TestModels32GPU(BaseTestModels, TorchtextTestCase):
+    dtype = torch.float32
+    device = torch.device("cuda")
