189f403e7416dd2a179a3454ab04f7ed0cf156ab
Nayef Ahmed <nayef211@meta.com>
2023-01-26 14:26:48 -0800

    Remove print statements

--- test/smoke_tests/smoke_tests.py
+++ test/smoke_tests/smoke_tests.py
@@ -15,12 +15,9 @@ def validateTorchdataVersion():
         year=int(torchdata_date_str[:4]), month=int(torchdata_date_str[4:6]), day=int(torchdata_date_str[6:])
     )
 
-    print("[DEBUG1]", torchdata.__version__, torchdata_date_str)
-    print("[DEBUG2]", torchdata_datetime, datetime.now() - timedelta(days=3))
-    raise Exception(f"[DEBUG3] {torchdata_datetime}, {datetime.now() - timedelta(days=3)}")
     assert torchdata_datetime >= datetime.now() - timedelta(
         days=3
-    ), "torchdata package version cannot be older than 3 days"
+    ), f"torchdata package version '{torchdata.__version__}' cannot be older than 3 days"
 
 
 validateTorchdataVersion()
