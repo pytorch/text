e4d2b0f3def4e7badf88d4da69a98d453bdc21df
Nayef Ahmed <22487263+Nayef211@users.noreply.github.com>
2023-01-26 18:33:39 -0800

    [TEST] Add torchdata version to smoke tests (#2034)
    * Add torchdata version to smoke tests
    
    * Debug failure
    
    * Remove print statements
    
    * Resolve PR comments
    
    * Remove prints
    
    Co-authored-by: Nayef Ahmed <nayef211@meta.com>

--- test/smoke_tests/smoke_tests.py
+++ test/smoke_tests/smoke_tests.py
@@ -1,6 +1,23 @@
 """Run smoke tests"""
 
+import re
+
+import torchdata
 import torchtext
 import torchtext.version  # noqa: F401
 
+NIGHTLY_ALLOWED_DELTA = 3
+
+
+def validateTorchdataVersion():
+    from datetime import datetime
+
+    date_t_str = re.findall(r"dev\d+", torchdata.__version__)[0]
+    date_t_delta = datetime.now() - datetime.strptime(date_t_str[3:], "%Y%m%d")
+
+    if date_t_delta.days >= NIGHTLY_ALLOWED_DELTA:
+        raise RuntimeError(f"torchdata binary {torchdata.__version__} is more than {NIGHTLY_ALLOWED_DELTA} days old!")
+
+
+validateTorchdataVersion()
 print("torchtext version is ", torchtext.__version__)
