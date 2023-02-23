265f14c35268c0755b41902e4bbf34020f6ff9fb
Andrey Talman <atalman@fb.com>
2023-02-23 18:44:26 -0500

    Use index-url for torchtext installs (#2075)

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -2,7 +2,7 @@
 if [[ ${MATRIX_PACKAGE_TYPE} = "conda" ]]; then
     conda install -y torchtext -c ${PYTORCH_CONDA_CHANNEL}
 else
-    pip install ${PYTORCH_PIP_PREFIX} torchtext --extra-index-url ${PYTORCH_PIP_DOWNLOAD_URL}
+    pip install ${PYTORCH_PIP_PREFIX} torchtext --index-url ${PYTORCH_PIP_DOWNLOAD_URL}
 fi
 
 python  ./test/smoke_tests/smoke_tests.py
