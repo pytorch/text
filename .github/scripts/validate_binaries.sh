8b77b67aeebf7906acf37ae1bf35dd95e74b2383
Joe Cummings <jrcummings@fb.com>
2023-02-27 16:04:21 -0500

    Double quote to prevent globbing and word splitting

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -2,12 +2,12 @@
 if [[ ${MATRIX_PACKAGE_TYPE} = "conda" ]]; then
     #special case for Python 3.11
     if [[ ${MATRIX_PYTHON_VERSION} == '3.11' ]]; then
-        conda install -y torchtext -c malfet -c ${PYTORCH_CONDA_CHANNEL}
+        conda install -y torchtext -c malfet -c "${PYTORCH_CONDA_CHANNEL}"
     else
-        conda install -y torchtext -c ${PYTORCH_CONDA_CHANNEL}
+        conda install -y torchtext -c "${PYTORCH_CONDA_CHANNEL}"
     fi
 else
-    pip install ${PYTORCH_PIP_PREFIX} torchtext --index-url ${PYTORCH_PIP_DOWNLOAD_URL}
+    pip install "${PYTORCH_PIP_PREFIX}" torchtext --index-url "${PYTORCH_PIP_DOWNLOAD_URL}"
 fi
 
 python  ./test/smoke_tests/smoke_tests.py
