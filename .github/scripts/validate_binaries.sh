6f4c53426a9ae1a8b60c00c7e1236d67ea47aa87
atalman <atalman@fb.com>
2023-03-14 14:46:16 -0700

    test

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -7,7 +7,13 @@ if [[ ${MATRIX_PACKAGE_TYPE} = "conda" ]]; then
         conda install -y torchtext -c ${PYTORCH_CONDA_CHANNEL}
     fi
 else
-    pip install ${PYTORCH_PIP_PREFIX} torchtext --index-url ${PYTORCH_PIP_DOWNLOAD_URL}
+    export filedownload=$(pip install ${PYTORCH_PIP_PREFIX} torchtext --index-url ${PYTORCH_PIP_DOWNLOAD_URL} | grep Downloading.*torchtext.* | grep -Eio '\bhttps://.*whl\b')
+    curl -O filedownload
+    unzip torchtext*
+    cd $(ls | grep -P "torchtext.*dist-info")
+    export match=$(cat METADATA | grep "torch (==2.0.0)")
+    [ -z "$var" ] && exit(1)
+    #pip install ${PYTORCH_PIP_PREFIX} torchtext --index-url ${PYTORCH_PIP_DOWNLOAD_URL}
 fi
 
 python  ./test/smoke_tests/smoke_tests.py
