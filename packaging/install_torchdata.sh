ccc461c992493872865db1e6bbafa437ea9ac170
atalman <atalman@fb.com>
2023-03-28 06:39:30 -0700

    fix regex

--- packaging/install_torchdata.sh
+++ packaging/install_torchdata.sh
@@ -33,7 +33,7 @@ $install_cmd torchdata $install_channel
 if [ "$package_type" = "wheel" ]; then
   TORCHDATA_VERSION="$(pip show torchdata | grep ^Version: | sed 's/Version:  *//' | sed 's/+.\+//')"
 else
-  TORCHDATA_VERSION="$(conda list -fe torchdata | grep torchdata | sed -e 's/torchdata=\(.*\)=py/\1/')"
+  TORCHDATA_VERSION="$(conda list -fe torchdata | grep torchdata | sed -e 's/torchdata=\(.*\)=py.*/\1/')"
   echo "export CONDA_TORCHDATA_CONSTRAINT='- torchdata==${TORCHDATA_VERSION}'" >> "${BUILD_ENV_FILE}"
 fi
 
