85255cfac75d02f859a98206e92c8cf18e7de796
Andrey Talman <atalman@fb.com>
2024-03-21 17:16:43 -0400

    Update validate_binaries.sh

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -1,8 +1,8 @@
 
 if [[ ${MATRIX_PACKAGE_TYPE} = "conda" ]]; then
-    conda install -y torchtext -c ${PYTORCH_CONDA_CHANNEL}
+    conda install -y torchtext==0.18.0 -c ${PYTORCH_CONDA_CHANNEL}
 else
-    pip install ${PYTORCH_PIP_PREFIX} torchtext --index-url ${PYTORCH_PIP_DOWNLOAD_URL}
+    pip install ${PYTORCH_PIP_PREFIX} torchtext==0.18.0 --index-url ${PYTORCH_PIP_DOWNLOAD_URL}
 fi
 
 python  ./test/smoke_tests/smoke_tests.py
