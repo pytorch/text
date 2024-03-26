d7b799ad816297e5ee7a860dc96f8e023a53ada4
Nicolas Hug <contact@nicolas-hug.com>
2024-03-26 14:50:30 +0000

    Add removeable warnings indicating this is the last release

--- torchtext/vocab/__init__.py
+++ torchtext/vocab/__init__.py
@@ -1,3 +1,8 @@
+import warnings
+import torchtext
+if torchtext._WARN:
+    warnings.warn(torchtext._TORCHTEXT_DEPRECATION_MSG)
+
 from .vectors import CharNGram, FastText, GloVe, pretrained_aliases, Vectors
 from .vocab import Vocab
 from .vocab_factory import build_vocab_from_iterator, vocab
