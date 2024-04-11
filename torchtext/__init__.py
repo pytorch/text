a897aa927f94909ac3e644ea7ab015f97c2f66d0
Andrey Talman <atalman@fb.com>
2024-04-11 14:12:53 -0400

    [Release 0.18] Add removeable warnings indicating this is the last release (#2248) (#2257)
    * Add removeable warnings indicating this is the last release (#2248)
    
    * make pytorch dep a lower bound (#2256)
    
    * Fix CNNDM dataset tests (#2246)
    
    ---------
    
    Co-authored-by: Nicolas Hug <contact@nicolas-hug.com>

--- torchtext/__init__.py
+++ torchtext/__init__.py
@@ -2,14 +2,24 @@ import os
 
 from torch.hub import _get_torch_home
 
+_WARN = True
+_TORCHTEXT_DEPRECATION_MSG = (
+    "\n/!\ IMPORTANT WARNING ABOUT TORCHTEXT STATUS /!\ \n"
+    "Torchtext is deprecated and the last released version will be 0.18 (this one). "
+    "You can silence this warning by calling the following at the beginnign of your scripts: "
+    "`import torchtext; torchtext.disable_torchtext_deprecation_warning()`"
+)
+
+def disable_torchtext_deprecation_warning():
+    global _WARN
+    _WARN = False
+
 # the following import has to happen first in order to load the torchtext C++ library
 from torchtext import _extension  # noqa: F401
 
 _TEXT_BUCKET = "https://download.pytorch.org/models/text/"
 
 _CACHE_DIR = os.path.expanduser(os.path.join(_get_torch_home(), "text"))
-
-from . import data, datasets, prototype, functional, models, nn, transforms, utils, vocab, experimental
 
 try:
     from .version import __version__, git_version  # noqa: F401
